557cfb379c39c0ff053d8d731a124eb9a83db6c8
David Luposchainsky <dluposchainsky@gmail.com>
2018-11-08 10:28:06 +0100

    update

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,9 @@ import           Data.Colour.RGBSpace
 import           Data.Colour.RGBSpace.HSV
 import Data.Semigroup
 
+import Lib
+
+
 
 picWidth, picHeight :: Int
 picWidth = 600
@@ -32,10 +35,10 @@ hsva h s v a = setSourceRGBA channelRed channelGreen channelBlue a
 sketch :: Render ()
 sketch = do
     background
-    for_ (haskellLogo 30 30) (\closedPath -> do
+    for_ (move (Vec2 30 30) haskellLogo) (\closedPath -> do
         hsva 230 0.7 0.5 1
-        renderClosedPath closedPath
-        setLineWidth 5
+        renderPolygon closedPath
+        setLineWidth 3
         stroke)
     scanlines
 
@@ -54,8 +57,8 @@ scanlines = do
             newPath
             angledLineDraw x y (pi / 4) 1000
             closePath
-    for_ (haskellLogo 30 30) (\closedPath -> do
-        renderClosedPath closedPath
+    for_ (move (Vec2 30 30) haskellLogo) (\polygon -> do
+        renderPolygon polygon
         clip
         for_ [-1000,-995..1000] (\x -> do
             diagonalLine x 0
@@ -65,20 +68,19 @@ scanlines = do
 
 
 
-data Vec2 = Vec2 Double Double
 
-renderClosedPath :: [Vec2] -> Render ()
-renderClosedPath [] = pure ()
-renderClosedPath (Vec2 x y : vecs) = do
+renderPolygon :: Polygon -> Render ()
+renderPolygon (Polygon []) = pure ()
+renderPolygon (Polygon (Vec2 x y : vecs)) = do
     newPath
     moveTo x y
     for_ vecs (\(Vec2 x' y') -> lineTo x' y')
     closePath
 
-haskellLogo xOffset yOffset = (fmap . fmap) move [left, lambda, upper, lower]
+haskellLogo :: [Polygon]
+haskellLogo = [left, lambda, upper, lower]
   where
-    move (Vec2 x y) = Vec2 (x + xOffset) (y + yOffset)
-    left   = [Vec2 0 340.15625, Vec2 113.386719 170.078125, Vec2 0 0, Vec2 85.039062 0, Vec2 198.425781 170.078125, Vec2 85.039062 340.15625, Vec2 0 340.15625]
-    lambda = [Vec2 113.386719 340.15625, Vec2 226.773438 170.078125, Vec2 113.386719 0, Vec2 198.425781 0, Vec2 425.195312 340.15625, Vec2 340.15625 340.15625, Vec2 269.292969 233.859375, Vec2 198.425781 340.15625, Vec2 113.386719 340.15625]
-    upper  = [Vec2 387.402344 240.945312, Vec2 349.609375 184.253906, Vec2 481.890625 184.25, Vec2 481.890625 240.945312, Vec2 387.402344 240.945312]
-    lower  = [Vec2 330.710938 155.90625, Vec2 292.914062 99.214844, Vec2 481.890625 99.210938, Vec2 481.890625 155.90625, Vec2 330.710938 155.90625]
+    left   = Polygon [Vec2 0 340.15625, Vec2 113.386719 170.078125, Vec2 0 0, Vec2 85.039062 0, Vec2 198.425781 170.078125, Vec2 85.039062 340.15625, Vec2 0 340.15625]
+    lambda = Polygon [Vec2 113.386719 340.15625, Vec2 226.773438 170.078125, Vec2 113.386719 0, Vec2 198.425781 0, Vec2 425.195312 340.15625, Vec2 340.15625 340.15625, Vec2 269.292969 233.859375, Vec2 198.425781 340.15625, Vec2 113.386719 340.15625]
+    upper  = Polygon [Vec2 387.402344 240.945312, Vec2 349.609375 184.253906, Vec2 481.890625 184.25, Vec2 481.890625 240.945312, Vec2 387.402344 240.945312]
+    lower  = Polygon [Vec2 330.710938 155.90625, Vec2 292.914062 99.214844, Vec2 481.890625 99.210938, Vec2 481.890625 155.90625, Vec2 330.710938 155.90625]
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,90 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+
+
+import Data.Maybe
+
+
+
+data Vec2 = Vec2 !Double !Double deriving (Eq, Ord, Show)
+newtype Polygon = Polygon [Vec2] deriving (Eq, Ord, Show)
+data Circle = Circle Vec2 !Double deriving (Eq, Ord, Show)
+data Line = Line Vec2 Vec2 deriving (Eq, Ord, Show)
+
+
+class Move stuff where
+    move :: Vec2 -> stuff -> stuff
+
+instance Move Vec2 where
+    move = addVec2
+
+instance Move Polygon where
+    move offset (Polygon points) = Polygon (map (move offset) points)
+
+instance Move Circle where
+    move offset (Circle center r) = Circle (move offset center) r
+
+instance Move geo => Move [geo] where
+    move offset = fmap (move offset)
+
+instance Move geo => Move (Maybe geo) where
+    move offset = fmap (move offset)
+
+addVec2 :: Vec2 -> Vec2 -> Vec2
+addVec2 (Vec2 x1 y1) (Vec2 x2 y2) = Vec2 (x1+x2) (y1+y2)
+
+negateVec2 :: Vec2 -> Vec2
+negateVec2 (Vec2 x y) = Vec2 (-x) (-y)
+
+subtractVec2 :: Vec2 -> Vec2 -> Vec2
+subtractVec2 v1 v2 = addVec2 v1 (negateVec2 v2)
+
+dotProduct :: Vec2 -> Vec2 -> Double
+dotProduct (Vec2 x1 y1) (Vec2 x2 y2) = x1*x2 + y1*y2
+
+norm :: Vec2 -> Double
+norm v = sqrt (dotProduct v v)
+
+
+data LLIntersection = Intersection | VirtualIntersection
+    deriving (Eq, Ord, Show)
+
+intersectionLL :: Line -> Line -> (Vec2, LLIntersection)
+intersectionLL (Line (Vec2 x1 y1) (Vec2 x2 y2)) (Line (Vec2 x3 y3) (Vec2 x4 y4))
+    | t < 0 || t > 1 = (p, VirtualIntersection)
+    | otherwise      = (p, Intersection)
+  where
+    p = Vec2 (x1 + t * (x2-x1)) (y1 + t * (y2-y1))
+    t = ((x1-x3) * (y3-y4) - (y1-y3) * (x3-x4))
+        / -------------------------------------
+        ((x1-x2) * (y3-y4) - (y1-y3) * (x3-x4))
+
+intersectionLC :: Line -> Circle -> Maybe (Vec2, Vec2)
+intersectionLC (Line (Vec2 x1 y1) (Vec2 x2 y2)) (Circle _ r) = undefined
+  where
+    -- Taken from MathWorld,
+    -- http://mathworld.wolfram.com/Circle-LineIntersection.html
+    dx = x2 - x1
+    dy = y2 - y1
+    dr = sqrt (dx^2 + dy^2)
+    dd = x1*y2 - x2*y1
+    sgnStar x = if x < 0 then -1 else 1
+    delta = r^2 * dr^2 - dd^2
+
+polygonEdges :: Polygon -> [Line]
+polygonEdges (Polygon ps) = zipWith Line ps (drop 1 (cycle ps))
+
+-- Ray-casting algorithm.
+pointInPolygon :: Vec2 -> Polygon -> Bool
+pointInPolygon p poly = odd (length intersections)
+  where
+    -- The test ray comes from outside the polygon from the left, and ends at
+    -- the point to be tested.
+    testRay = Line (Vec2 (leftmostPolyX - 1) pointY) p
+      where
+        leftmostPolyX = minimum (edges >>= \(Line (Vec2 x1 _) (Vec2 x2 _)) -> [x1,x2])
+        Vec2 _ pointY = p
+
+    intersections = filter ((== Intersection) . snd . intersectionLL testRay) edges
+
+    edges = polygonEdges poly
