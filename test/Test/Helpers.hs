72feee3d0de802207f9ef220ac77ebc04a8b722c
David Luposchainsky <dluposchainsky@gmail.com>
2018-12-04 15:36:32 +0100

    Move approximate equality class to helpers

--- test/Test/Helpers.hs
+++ test/Test/Helpers.hs
@@ -1,6 +1,15 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
-module Test.Helpers where
+module Test.Helpers (
+      Seed(..)
+    , gaussianVecs
+    , GaussianVec(..)
+    , LotsOfGaussianPoints(..)
+
+    , EqApprox(..)
+    , Tolerance(..)
+    , (~==)
+) where
 
 
 
@@ -69,3 +78,49 @@ gaussianVecs (Seed seed)
               in Vec2 x y : go rest
         go _ = error "Can’t happen, input is infinite"
     in go (randomRs (0, 1) (mkStdGen seed))
+newtype Tolerance = Tolerance Double
+
+class EqApprox a where
+    approxEqualTolerance :: Tolerance -> a -> a -> Bool
+
+infix 4 ~==
+(~==) :: EqApprox a => a -> a -> Bool
+(~==) = approxEqualTolerance (Tolerance 1e-10)
+
+instance EqApprox Double where
+    approxEqualTolerance (Tolerance tolerance) reference value
+      = abs (reference - value) <= tolerance
+
+instance EqApprox Vec2 where
+    approxEqualTolerance (Tolerance tolerance) v1 v2
+      = let Distance distance = norm (v2 -. v1)
+        in distance <= tolerance
+
+instance EqApprox Distance where
+    approxEqualTolerance tol (Distance x) (Distance y) = approxEqualTolerance tol x y
+
+instance EqApprox Area where
+    approxEqualTolerance tol (Area x) (Area y) = approxEqualTolerance tol x y
+
+instance EqApprox Angle where
+    approxEqualTolerance tol (Angle x) (Angle y)
+      = let Angle x' = rad x
+            Angle y' = rad y
+            -- If x and y are just around 0°/360° within the tolerance interval,
+            -- the angles above will be apart by 2π, so we check the π rotated
+            -- version here as well, avoiding the instability around 0.
+            Angle x'' = rad (x+pi)
+            Angle y'' = rad (y+pi)
+        in approxEqualTolerance tol x' y' || approxEqualTolerance tol x'' y''
+
+instance EqApprox Transformation where
+    approxEqualTolerance tol
+        (Transformation a1 b1 c1 d1 e1 f1)
+        (Transformation a2 b2 c2 d2 e2 f2)
+      = all (\(x,y) -> approxEqualTolerance tol x y)
+            [ (a1, a2)
+            , (b1, b2)
+            , (c1, c2)
+            , (d1, d2)
+            , (e1, e2)
+            , (f1, f2) ]
--- test/Test/Properties.hs
+++ test/Test/Properties.hs
@@ -15,7 +15,7 @@ import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck
 
-import Test.Helpers ()
+import Test.Helpers
 
 
 
@@ -30,53 +30,6 @@ tests = testGroup "Properties"
     , polygonInstancesTest
     , transformationTest
      ]
-
-newtype Tolerance = Tolerance Double
-
-class EqApprox a where
-    approxEqualTolerance :: Tolerance -> a -> a -> Bool
-
-infix 4 ~==
-(~==) :: EqApprox a => a -> a -> Bool
-(~==) = approxEqualTolerance (Tolerance 1e-10)
-
-instance EqApprox Double where
-    approxEqualTolerance (Tolerance tolerance) reference value
-      = abs (reference - value) <= tolerance
-
-instance EqApprox Vec2 where
-    approxEqualTolerance (Tolerance tolerance) v1 v2
-      = let Distance distance = norm (v2 -. v1)
-        in distance <= tolerance
-
-instance EqApprox Distance where
-    approxEqualTolerance tol (Distance x) (Distance y) = approxEqualTolerance tol x y
-
-instance EqApprox Area where
-    approxEqualTolerance tol (Area x) (Area y) = approxEqualTolerance tol x y
-
-instance EqApprox Angle where
-    approxEqualTolerance tol (Angle x) (Angle y)
-      = let Angle x' = rad x
-            Angle y' = rad y
-            -- If x and y are just around 0°/360° within the tolerance interval,
-            -- the angles above will be apart by 2π, so we check the π rotated
-            -- version here as well, avoiding the instability around 0.
-            Angle x'' = rad (x+pi)
-            Angle y'' = rad (y+pi)
-        in approxEqualTolerance tol x' y' || approxEqualTolerance tol x'' y''
-
-instance EqApprox Transformation where
-    approxEqualTolerance tol
-        (Transformation a1 b1 c1 d1 e1 f1)
-        (Transformation a2 b2 c2 d2 e2 f2)
-      = all (\(x,y) -> approxEqualTolerance tol x y)
-            [ (a1, a2)
-            , (b1, b2)
-            , (c1, c2)
-            , (d1, d2)
-            , (e1, e2)
-            , (f1, f2) ]
 
 angleBetweenTest :: TestTree
 angleBetweenTest = testProperty "Angle between two lines"
