ada2922e02629b69cb7ef99e6f8b9c9254758a84
Franz Thoma <franz.thoma@tngtech.com>
2021-12-23 15:01:57 +0100

    Import Voronoi & make it compile

---
+++ app/haskell_logo_voronoi/Voronoi.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE RecordWildCards #-}
+module Voronoi where
+
+import           Data.List (foldl')
+
+import           Geometry
+
+data VoronoiFace = VF { center :: Vec2, face :: Polygon }
+    deriving (Eq, Show)
+
+data Voronoi = Voronoi { bounds :: Polygon, faces :: [VoronoiFace] }
+    deriving (Eq, Show)
+
+voronoi :: [Vec2] -> Double -> Double -> Voronoi
+voronoi points w h = foldl' addPoint (emptyVoronoi w h) points
+
+emptyVoronoi :: Double -> Double -> Voronoi
+emptyVoronoi w h = Voronoi initialPolygon []
+  where
+    initialPolygon = Polygon [ Vec2 0 0, Vec2 w 0, Vec2 w h, Vec2 0 h ]
+
+addPoint :: Voronoi -> Vec2 -> Voronoi
+addPoint Voronoi{..} p = Voronoi bounds (newFace : faces')
+  where
+    newFace :: VoronoiFace
+    newFace = foldl' (\nf f -> updateFace nf (center f)) (VF p bounds) faces
+
+    faces' :: [VoronoiFace]
+    faces' = fmap (\f -> updateFace f (center newFace)) faces
+
+updateFace :: VoronoiFace -> Vec2 -> VoronoiFace
+updateFace f p = VF (center f) (clipFace (face f) (Line q d))
+  where
+    q = let Vec2 px py = p
+            Vec2 fx fy = center f
+        in  Vec2 ((px + fx) / 2) ((py + fy) / 2)
+    d = let Vec2 qx qy = q
+            Vec2 dx dy = p -. center f
+        in  Vec2 (qx - dy) (qy + dx)
+
+-- | Keep everything that's *left* of the line
+clipFace :: Polygon -> Line -> Polygon
+clipFace polygon line = let p:_ = cutPolygon line polygon in p --TODO
