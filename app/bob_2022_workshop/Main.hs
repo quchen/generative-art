c2b7f660a5f6d624642a14b978b2cd7a5053f4e8
David Luposchainsky <dluposchainsky@gmail.com>
2022-01-04 14:06:39 +0100

    Add Hello World (plain Cairo)

---
+++ app/bob_2022_workshop/Main.hs
@@ -0,0 +1,21 @@
+module Main (main) where
+
+import Draw
+import qualified Graphics.Rendering.Cairo as Cairo
+import Steps.A.HelloWorld
+
+-- | Draw a 'Render' to PNG and SVG files.
+drawToFiles
+    :: FilePath -- ^ Output location, will store .svg and .png versions
+    -> Int -- ^ Width
+    -> Int -- ^ Height
+    -> (Int -> Int -> Cairo.Render a) -- ^ Picture, parameterized by width/height
+    -> IO ()
+drawToFiles filename w h drawing = do
+    _ <- withSurface PNG (filename ++ ".png") w h (\surface -> Cairo.renderWith surface (drawing w h))
+    _ <- withSurface SVG (filename ++ ".svg") w h (\surface -> Cairo.renderWith surface (drawing w h))
+    pure ()
+
+main :: IO ()
+main = do
+    drawToFiles "bob2022/helloworld" 800 600 (Steps.A.HelloWorld.hello)
---
+++ app/bob_2022_workshop/Steps/A/HelloWorld.hs
@@ -0,0 +1,111 @@
+module Steps.A.HelloWorld (hello) where
+
+import Graphics.Rendering.Cairo hiding (x,y)
+
+hello :: Int -> Int -> Render ()
+hello _ _ = do
+    translate 10 10
+    letterH
+    translate 40 0
+    letterE
+    translate 40 0
+    letterL1
+    translate 40 0
+    letterL2
+    translate 40 0
+    letterO
+
+    identityMatrix
+    translate 10 100
+    bobkonf2022
+
+
+letterH :: Render ()
+letterH = do
+    newPath
+    moveTo 0 0
+    lineTo 10 0
+    lineTo 10 20
+    lineTo 20 20
+    lineTo 20 0
+    lineTo 30 0
+    lineTo 30 50
+    lineTo 20 50
+    lineTo 20 30
+    lineTo 10 30
+    lineTo 10 50
+    lineTo 0 50
+    closePath
+    stroke
+
+letterE :: Render ()
+letterE = do
+    newPath
+    moveTo 0 0
+    relLineTo 30 0
+    relLineTo 0 10
+    relLineTo (-20) 0
+    relLineTo 0 10
+    relLineTo 15 0
+    relLineTo 0 10
+    relLineTo (-15) 0
+    relLineTo 0 10
+    relLineTo 20 0
+    relLineTo 0 10
+    relLineTo (-30) 0
+    closePath
+    setSourceRGB 0.368417 0.506779 0.709798
+    fillPreserve
+    setSourceRGB 0 0 0
+    strokePreserve
+
+letterL1 :: Render ()
+letterL1 = do
+    newPath
+    moveTo 0 0
+    sequence_ [ relLineTo dx dy | (dx,dy) <- [(10, 0), (0,40), (20,0), (0,10),(-30,0)] ]
+    closePath
+    setDash [3,5] 0
+    setLineCap LineCapRound
+    stroke
+
+letterL2 :: Render ()
+letterL2 = do
+    setLineWidth 3
+    setSourceRGB 0 0 0
+    letterL1
+
+letterO :: Render ()
+letterO = do
+    newPath
+    setSourceRGB 0.880722 0.611041 0.142051
+    setLineWidth 1
+    setDash [] 0
+    moveTo 0 20
+    curveTo 0 0 40 0 40 20
+    relLineTo 0 10
+    curveTo 40 50 0 50 0 30
+    closePath
+    stroke
+    prettyBezier 0 20 0 0 40 0 40 20
+    prettyBezier 40 30 40 50 0 50 0 30
+  where
+    circle r = do
+        (x,y) <- getCurrentPoint
+        newPath
+        arc x y r 0 (2*pi)
+        closePath
+    prettyBezier x1 y1 x2 y2 x3 y3 x4 y4 = do
+        setSourceRGBA 0 0 0 0.2
+        moveTo x1 y1 >> circle 2 >> fill
+        moveTo x1 y1 >> lineTo x2 y2 >> stroke
+        moveTo x2 y2 >> circle 2 >> fill
+        moveTo x3 y3 >> circle 2 >> fill
+        moveTo x3 y3 >> lineTo x4 y4 >> stroke
+        moveTo x4 y4 >> circle 2 >> fill
+
+bobkonf2022 :: Render ()
+bobkonf2022 = do
+    setFontSize 40
+    setSourceRGB 0.368417 0.506779 0.709798
+    showText "BobKonf 2022"
