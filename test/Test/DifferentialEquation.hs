c0bddef360f18b3ad2c762f7c406e2e5ee268b94
David Luposchainsky <dluposchainsky@gmail.com>
2022-01-07 20:37:43 +0100

    Refactor two-body problem painting

--- test/Test/DifferentialEquation.hs
+++ test/Test/DifferentialEquation.hs
@@ -20,8 +20,8 @@ tests = testGroup "Differential equations"
 twoBodyProblem :: TestTree
 twoBodyProblem = testCase "Two-body problem" (renderAllFormats 800 800 "test/out/differential_equations/1_two_body_problem" (renderTwoBodyProblem 800 800))
 
-planetTrajectory :: [(Double, (Vec2, Vec2))]
-planetTrajectory = rungeKuttaAdaptiveStep f y0 t0 dt0 tol
+planetPhaseDiagram :: [(Double, (Vec2, Vec2))]
+planetPhaseDiagram = rungeKuttaAdaptiveStep f y0 t0 dt0 tol
   where
     f :: Double -> (Vec2, Vec2) -> (Vec2, Vec2)
     f _t (x, v)
@@ -40,47 +40,40 @@ planetTrajectory = rungeKuttaAdaptiveStep f y0 t0 dt0 tol
 
 renderTwoBodyProblem :: Int -> Int -> Render ()
 renderTwoBodyProblem w h = do
-    let phaseDiagram = takeWhile (\(t,_) -> t < 1000) planetTrajectory
+    let phaseDiagram' = takeWhile (\(t,_) -> t < 1000) planetPhaseDiagram
 
-        trajectoryBoundingBox = boundingBox [x | (_t, (x,_v)) <- phaseDiagram]
-                             <> boundingBox (Vec2 0 0) -- Don’t forget about the sun :-)
-        canvasBoundingBox = boundingBox (Vec2 0 0, Vec2 (fromIntegral w) (fromIntegral h))
-        scaleToCanvas = transformBoundingBox trajectoryBoundingBox canvasBoundingBox MaintainAspectRatio
-        addSomeMargin = translateT (Vec2 10 10) <> scaleT 0.98 0.98
-        transformNicely = Geometry.transform (addSomeMargin <> scaleToCanvas)
-        trajectory = [(t, transformNicely x) | (t, (x,_v)) <- phaseDiagram]
+        transformNicely
+          = let trajectoryBoundingBox = boundingBox [x | (_t, (x,_v)) <- phaseDiagram']
+                                    <> boundingBox (Vec2 0 0) -- Don’t forget about the sun :-)
+                canvasBoundingBox = boundingBox (Vec2 0 0, Vec2 (fromIntegral w) (fromIntegral h))
+                scaleToCanvas = transformBoundingBox trajectoryBoundingBox canvasBoundingBox MaintainAspectRatio
+                addSomeMargin = translateT (Vec2 10 10) <> scaleT 0.98 0.98
+            in Geometry.transform (addSomeMargin <> scaleToCanvas)
+        planetTrajectory = [(t, transformNicely x) | (t, (x,_v)) <- phaseDiagram']
+        sun = transformNicely (Vec2 0 0)
 
-    -- Paint sun
-    do save
-       newPath
-       let Vec2 xSun ySun = transformNicely (Vec2 0 0)
-       arc xSun ySun 7 0 (2*pi)
-       closePath
-       setSourceRGB 0.880722 0.611041 0.142051
-       fillPreserve
-       setSourceRGB 0 0 0
-       setLineWidth 1
-       stroke
-       restore
+    let paintSun = restoreStateAfter $ do
+            circleSketch sun (Distance 16)
+            setLineWidth 2
+            mmaColor 1 1
+            fillPreserve
+            setSourceRGB 0 0 0
+            stroke
 
-    -- Paint trajectory
-    do save
-       newPath
-       setLineWidth 1
-       for_ trajectory $ \(_t, Vec2 x y) -> lineTo x y
-       stroke
-       restore
+        paintTrajectory = restoreStateAfter $ do
+            setLineWidth 1.5
+            for_ planetTrajectory $ \(_t, Vec2 x y) -> lineTo x y
+            stroke
 
-    -- Paint planet
-    do save
-       newPath
-       let (_t, Vec2 x y) = head trajectory
-       arc x y 4 0 (2*pi)
-       closePath
-       setSourceRGB 0.922526 0.385626 0.209179
-       fillPreserve
-       setSourceRGB 0 0 0
-       setLineWidth 1
-       setLineCap LineCapRound
-       stroke
-       restore
+        paintPlanet = restoreStateAfter $ do
+            let (_t0, planet) = head planetTrajectory
+            circleSketch planet (Distance 8)
+            mmaColor 3 1
+            fillPreserve
+            setSourceRGB 0 0 0
+            setLineWidth 2
+            stroke
+
+    paintSun
+    paintTrajectory
+    paintPlanet
