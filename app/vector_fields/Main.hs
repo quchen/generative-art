7363128ab573341609922b93548db85cfa98ce45
Franz Thoma <franz.thoma@tngtech.com>
2022-01-07 23:48:45 +0100

    Y compression

--- app/vector_fields/Main.hs
+++ app/vector_fields/Main.hs
@@ -3,12 +3,13 @@ module Main where
 import Control.Applicative (liftA2)
 import Control.Monad (replicateM)
 import Data.Foldable (for_)
-import Graphics.Rendering.Cairo as Cairo hiding (x, y)
+import qualified Graphics.Rendering.Cairo as Cairo
 import Numeric.Noise.Perlin
 import System.Random.MWC
 
 import Draw
 import Geometry
+import Graphics.Rendering.Cairo (Render, liftIO)
 
 
 picWidth, picHeight :: Int
@@ -19,10 +20,10 @@ noiseScale :: Double
 noiseScale = 251
 
 seed :: Int
-seed = 123456
+seed = 123457
 
 main :: IO ()
-main = withSurface PNG "out.png" picWidth picHeight $ \surface -> renderWith surface $ do
+main = withSurface PNG "out.png" picWidth picHeight $ \surface -> Cairo.renderWith surface $ do
     restoreStateAfter $ do
         Cairo.setSourceRGB 1 1 1
         Cairo.rectangle 0 0 (fromIntegral picWidth) (fromIntegral picHeight)
@@ -41,16 +42,16 @@ uniformlyDistributedPoints gen count = liftIO $ replicateM count randomPoint
 
 drawVectorField :: (Vec2 -> Vec2) -> Render ()
 drawVectorField f = restoreStateAfter $ do
-    setSourceRGB 0 0 0
+    Cairo.setSourceRGB 0 0 0
     for_ [0,40..picWidth] $ \x -> for_ [0,40..picHeight] $ \y -> do
         let point = Vec2 (fromIntegral x) (fromIntegral y)
             end = point +. 20 *. f point
         arrowSketch (Line point end) def
-        stroke
+        Cairo.stroke
 
 drawFieldLines :: (Vec2 -> Vec2) -> [Vec2] -> Render ()
 drawFieldLines f ps = restoreStateAfter $ do
-    setSourceRGB 0 0 0
+    Cairo.setSourceRGB 0 0 0
     for_ ps $ \p -> do
         moveToVec p
         for_ (gradientDescent f p) lineToVec
@@ -71,7 +72,7 @@ gradientField2 perturbation v@(Vec2 x y) =
 gradientField3 :: Perlin -> Vec2 -> Vec2
 gradientField3 perturbation p@(Vec2 x y) =
     let perturbationStrength = 0.5 * (1 + tanh (4 * (x / fromIntegral picWidth - 0.6)))
-        noise (Vec2 x' y') = noiseValue perturbation (x' + 49156616, y' + 46216981, 321685163213)
+        noise (Vec2 x' y') = noiseValue perturbation (x' + 49156616, 2 * y' + 46216981, 321685163213)
         grad f v = 100 *. Vec2 (f (v +. Vec2 0.01 0) - f v) (f (v +. Vec2 0 0.01) - f v)
         Vec2 dx dy = noiseScale * perturbationStrength *. grad noise p
     in  Vec2 (1.5 - perturbationStrength) 0 +. Vec2 dy (-dx)
