8d94d141a8d5f4e64c29ea33780c195e57e673a6
Franz Thoma <franz.thoma@tngtech.com>
2022-01-08 14:08:47 +0100

    Look for a seed without clipping, add frames

--- app/vector_fields/Main.hs
+++ app/vector_fields/Main.hs
@@ -6,6 +6,7 @@ import Data.Foldable (for_)
 import qualified Graphics.Rendering.Cairo as Cairo
 import Numeric.Noise.Perlin
 import System.Random.MWC
+import Text.Printf
 
 import Draw
 import Geometry
@@ -23,28 +24,33 @@ noiseScale = 251
 seed :: Int
 seed = 519496
 
+frames :: [Double]
+frames = [0]
+
 main :: IO ()
-main = withSurface PNG "out.png" scaledWidth scaledHeight $ \surface -> Cairo.renderWith surface $ do
-    Cairo.scale scaleFactor scaleFactor
-    restoreStateAfter $ do
-        Cairo.setSourceRGB 1 1 1
-        Cairo.rectangle 0 0 (fromIntegral picWidth) (fromIntegral picHeight)
-        Cairo.fill
-    don't $ drawScalarField scalarField
-    restoreStateAfter $ don't $ do
-        Cairo.setSourceRGB 1 0 0
-        drawVectorField ((0.2 *.) <$> gradientField)
-    restoreStateAfter $ don't $ do
-        Cairo.setSourceRGB 0 0 1
-        drawVectorField compositeField
-    gen <- liftIO create
-    ps <- uniformlyDistributedPoints gen 10000
-    thicknesses <- liftIO $ replicateM 1000 (uniformR (0.5, 1.5) gen)
-    for_ (zip ps (cycle thicknesses)) $ \(p, thickness) -> restoreStateAfter $ do
-        Cairo.setLineWidth thickness
-        drawFieldLine (take 20 (fieldLine compositeField p))
+main = for_ frames $ \t -> do
+    let filename = "out.png" --"out/" ++ show seed ++ "_t" ++ printf "%08.4f" t ++ ".png"
+    withSurface PNG filename scaledWidth scaledHeight $ \surface -> Cairo.renderWith surface $ do
+        Cairo.scale scaleFactor scaleFactor
+        restoreStateAfter $ do
+            Cairo.setSourceRGB 1 1 1
+            Cairo.rectangle 0 0 (fromIntegral picWidth) (fromIntegral picHeight)
+            Cairo.fill
+        don't $ drawScalarField (scalarField t)
+        restoreStateAfter $ don't $ do
+            Cairo.setSourceRGB 1 0 0
+            drawVectorField ((0.2 *.) <$> gradientField t)
+        restoreStateAfter $ don't $ do
+            Cairo.setSourceRGB 0 0 1
+            drawVectorField (compositeField t)
+        gen <- liftIO create
+        ps <- uniformlyDistributedPoints gen 10000
+        thicknesses <- liftIO $ replicateM 1000 (uniformR (0.5, 1.5) gen)
+        for_ (zip ps (cycle thicknesses)) $ \(p, thickness) -> restoreStateAfter $ do
+            Cairo.setLineWidth thickness
+            drawFieldLine (take 20 (fieldLine (compositeField t) p))
   where
-    scaleFactor = 0.5
+    scaleFactor = 4
     scaledWidth = round (fromIntegral picWidth * scaleFactor)
     scaledHeight = round (fromIntegral picHeight * scaleFactor)
 
@@ -54,6 +60,18 @@ uniformlyDistributedPoints gen count = liftIO $ replicateM count randomPoint
   where
     randomPoint = liftA2 Vec2 (randomCoordinate picWidth) (randomCoordinate picHeight)
     randomCoordinate mx = fmap fromIntegral (uniformR (0, mx) gen :: IO Int)
+
+detectClipping :: (Vec2 -> Double) -> Render ()
+detectClipping f = restoreStateAfter $
+    for_ [ Vec2 (fromIntegral x) (fromIntegral y) | x <- [0,5..picWidth], y <- [0,5..picHeight] ] $ \p@(Vec2 x y) -> do
+        let v = f p
+            v' | v >= 1 = 1
+               | v <= -1 = -1
+               | otherwise = 0.5 * v
+
+        hsva 0 0 (0.5 + 0.5 * v') 1
+        Cairo.rectangle x y 5 5
+        Cairo.fill
 
 drawScalarField :: (Vec2 -> Double) -> Render ()
 drawScalarField f = restoreStateAfter $
@@ -76,22 +94,22 @@ drawFieldLine ps = restoreStateAfter $ do
     bezierCurveSketch (bezierSmoothenOpen ps)
     Cairo.stroke
 
-scalarField :: Vec2 -> Double
-scalarField (Vec2 x y) = noise2d noise1 (Vec2 x (2*y))
+scalarField :: Double -> Vec2 -> Double
+scalarField = \t (Vec2 x y) -> noise2d noise1 (Vec2 (x - t) (2*y))
   where
-    noise1 = perlin seed 1 (1/noiseScale) 0.5
-    noise2d nf (Vec2 x y) = noiseValue nf (x + 49156616, y + 46216381, 321685163213)
+    noise1 = perlin seed 1 (1/noiseScale) 0.001
+    noise2d nf (Vec2 x y) = noiseValue nf (x + 49121616, y + 46216381, 321685163213)
 
-gradientField :: Vec2 -> Vec2
-gradientField p@(Vec2 x y) =
+gradientField :: Double -> Vec2 -> Vec2
+gradientField t p@(Vec2 x y) =
     let grad f v = 100 *. Vec2 (f (v +. Vec2 0.01 0) - f v) (f (v +. Vec2 0 0.01) - f v)
-    in  noiseScale *. grad scalarField p
+    in  noiseScale *. grad (scalarField t) p
 
-rotationField :: Vec2 -> Vec2
-rotationField = rotate (deg 90) . gradientField
+rotationField :: Double -> Vec2 -> Vec2
+rotationField t = rotate (deg 90) . gradientField t
 
-compositeField :: Vec2 -> Vec2
-compositeField p@(Vec2 x y) = Vec2 1 0 +. 0.8 * perturbationStrength *. rotationField p
+compositeField :: Double -> Vec2 -> Vec2
+compositeField t p@(Vec2 x y) = Vec2 1 0 +. 0.8 * perturbationStrength *. rotationField t p
   where
     perturbationStrength = 0.5 * (1 + tanh (4 * (x / fromIntegral picWidth - 0.6))) * exp (-3 * (y / fromIntegral picHeight - 0.5)^2)
 
