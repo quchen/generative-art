02b8af23910e9a0f0cdd0cd8312bad427058935a
David Luposchainsky <dluposchainsky@gmail.com>
2022-01-12 16:46:50 +0100

    Documentation

--- test/Test/ArcLengthParameterization.hs
+++ test/Test/ArcLengthParameterization.hs
@@ -21,31 +21,45 @@ tests = testGroup "Arc length parameterization"
     ]
 
 singleCurveTest :: TestTree
-singleCurveTest = testCase "Single curve" (renderAllFormats 320 240 "docs/arc_length_parameterization/1_single_curve" (singleCurveRender 320 240))
+singleCurveTest = testCase "Single curve" (renderAllFormats 320 170 "docs/arc_length_parameterization/1_single_curve" (singleCurveRender 320 170))
 
 singleCurveRender :: Int -> Int -> Render ()
-singleCurveRender w h = do
-    let curveRaw = Bezier (Vec2 0 0) (Vec2 0.6 0.8) (Vec2 1.4 0.6) (Vec2 1 0)
-        fitToBox = G.transform (transformBoundingBox curveRaw (Vec2 10 10, Vec2 (fromIntegral w-10) (fromIntegral h-10)) FitAllIgnoreAspect)
-        curve = fitToBox curveRaw
-        evenlySpaced = bezierSubdivideS 10 curve
-        unevenlySpaced = bezierSubdivideT 10 curve
+singleCurveRender _w _h = do
+    let curve = let curveRaw = G.transform (G.rotate (deg (-30))) (Bezier (Vec2 0 0) (Vec2 1 5) (Vec2 2.5 (-1)) (Vec2 3 3))
+                    fitToBox = G.transform (transformBoundingBox curveRaw (Vec2 0 0, Vec2 300 100) FitAllMaintainAspect )
+                in fitToBox curveRaw
+        evenlySpaced = bezierSubdivideS 16 curve
+        unevenlySpaced = bezierSubdivideT 16 curve
 
     setLineWidth 1
+
+    Cairo.translate 10 10
     cairoScope $ do
         mmaColor 1 1
         bezierCurveSketch [curve]
         stroke
+    cairoScope $ for_ (zip [1..] evenlySpaced) $ \(i, p) -> do
+        newPath
+        circleSketch p (Distance 3)
+        mmaColor 0 1
+        stroke
+        cairoScope $ do
+            moveToVec (p +. Vec2 0 10)
+            setFontSize 6
+            showText (show i)
+
+    Cairo.translate 0 50
+
     cairoScope $ do
-        for_ (zip unevenlySpaced evenlySpaced) $ \(unevenly, evenly) -> do
-            cairoScope $ do
-                circleSketch evenly (Distance 3)
-                mmaColor 0 1
-                stroke
-            cairoScope $ do
-                mmaColor 2 0.1
-                circleSketch unevenly (Distance 3)
-                fillPreserve
-                mmaColor 2 0.4
-                stroke
-    pure ()
+        mmaColor 1 1
+        bezierCurveSketch [curve]
+        stroke
+    cairoScope $ for_ (zip [1..] unevenlySpaced) $ \(i, p) -> do
+        newPath
+        circleSketch p (Distance 3)
+        mmaColor 3 1
+        stroke
+        cairoScope $ do
+            moveToVec (p +. Vec2 0 10)
+            setFontSize 6
+            showText (show i)
