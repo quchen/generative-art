724f23f9a541c2e1d6c3b971957d6f2e6ad23be5
David Luposchainsky <dluposchainsky@gmail.com>
2022-01-13 18:10:06 +0100

    Make Bezier subdivision doc prettier

--- test/Test/Bezier.hs
+++ test/Test/Bezier.hs
@@ -20,45 +20,37 @@ tests = testGroup "Arc length parameterization"
     ]
 
 singleCurveTest :: TestTree
-singleCurveTest = testCase "Single curve" (renderAllFormats 320 170 "docs/bezier/1_single_curve" (singleCurveRender 320 170))
+singleCurveTest = testCase "Single curve" (renderAllFormats 300 150 "docs/bezier/1_single_curve" (singleCurveRender 300 150))
 
 singleCurveRender :: Int -> Int -> Render ()
 singleCurveRender _w _h = do
     let curve = let curveRaw = G.transform (G.rotate (deg (-30))) (Bezier (Vec2 0 0) (Vec2 1 5) (Vec2 2.5 (-1)) (Vec2 3 3))
-                    fitToBox = G.transform (transformBoundingBox curveRaw (Vec2 0 0, Vec2 300 100) FitAllMaintainAspect )
+                    fitToBox = G.transform (transformBoundingBox curveRaw (Vec2 10 10, Vec2 290 90) FitAllIgnoreAspect)
                 in fitToBox curveRaw
         evenlySpaced = bezierSubdivideS 16 curve
         unevenlySpaced = bezierSubdivideT 16 curve
 
+        offsetBelow :: Transform geo => geo -> geo
+        offsetBelow = G.transform (G.translate (Vec2 0 50))
+
     setLineWidth 1
 
-    Cairo.translate 10 10
     cairoScope $ do
         mmaColor 1 1
         bezierCurveSketch [curve]
         stroke
-    cairoScope $ for_ (zip [1..] evenlySpaced) $ \(i, p) -> do
-        newPath
-        circleSketch p (Distance 3)
-        mmaColor 0 1
+        bezierCurveSketch [offsetBelow curve]
         stroke
-        cairoScope $ do
-            moveToVec (p +. Vec2 0 10)
-            setFontSize 6
-            showText (show i)
 
-    Cairo.translate 0 50
-
-    cairoScope $ do
-        mmaColor 1 1
-        bezierCurveSketch [curve]
-        stroke
-    cairoScope $ for_ (zip [1..] unevenlySpaced) $ \(i, p) -> do
-        newPath
-        circleSketch p (Distance 3)
-        mmaColor 3 1
-        stroke
-        cairoScope $ do
-            moveToVec (p +. Vec2 0 10)
-            setFontSize 6
-            showText (show i)
+    for_ (zip evenlySpaced unevenlySpaced) $ \(e, u') -> do
+        let u = offsetBelow u'
+        let circle p = newPath >> circleSketch p (Distance 3) >> stroke
+            connect p q = do
+                let shrink factor = resizeLineSymmetric (\(Distance d) -> Distance (factor*d))
+                    line = shrink 0.8 (Line p q)
+                lineSketch line
+                setDash [1,1] 0
+                stroke
+        cairoScope (mmaColor 0 1 >> circle e)
+        cairoScope (mmaColor 3 1 >> circle u)
+        cairoScope (setSourceRGBA 0 0 0 0.1 >> connect e u)
