be10830c0b53533d6fb6fcd49dd26444d411d0dd
David Luposchainsky <dluposchainsky@gmail.com>
2022-01-15 17:05:31 +0100

    Add VectorSpace instances for 3- to 5-tuples

--- src/Algebra/VectorSpace.hs
+++ src/Algebra/VectorSpace.hs
@@ -46,6 +46,24 @@ instance (VectorSpace v1, VectorSpace v2) => VectorSpace (v1, v2) where
     a *. (u1, v1) = (a*.u1, a*.v1)
     zero = (zero, zero)
 
+instance (VectorSpace v1, VectorSpace v2, VectorSpace v3) => VectorSpace (v1, v2, v3) where
+    (u1, v1, w1) +. (u2, v2, w2) = (u1+.u2, v1+.v2, w1+.w2)
+    (u1, v1, w1) -. (u2, v2, w2) = (u1-.u2, v1-.v2, w1-.w2)
+    a *. (u1, v1, w1) = (a*.u1, a*.v1, a*.w1)
+    zero = (zero, zero, zero)
+
+instance (VectorSpace v1, VectorSpace v2, VectorSpace v3, VectorSpace v4) => VectorSpace (v1, v2, v3, v4) where
+    (u1, v1, w1, x1) +. (u2, v2, w2, x2) = (u1+.u2, v1+.v2, w1+.w2, x1+.x2)
+    (u1, v1, w1, x1) -. (u2, v2, w2, x2) = (u1-.u2, v1-.v2, w1-.w2, x1-.x2)
+    a *. (u1, v1, w1, x1) = (a*.u1, a*.v1, a*.w1, a*.x1)
+    zero = (zero, zero, zero, zero)
+
+instance (VectorSpace v1, VectorSpace v2, VectorSpace v3, VectorSpace v4, VectorSpace v5) => VectorSpace (v1, v2, v3, v4, v5) where
+    (u1, v1, w1, x1, y1) +. (u2, v2, w2, x2, y2) = (u1+.u2, v1+.v2, w1+.w2, x1+.x2, y1+.y2)
+    (u1, v1, w1, x1, y1) -. (u2, v2, w2, x2, y2) = (u1-.u2, v1-.v2, w1-.w2, x1-.x2, y1-.y2)
+    a *. (u1, v1, w1, x1, y1) = (a*.u1, a*.v1, a*.w1, a*.x1, a*.y1)
+    zero = (zero, zero, zero, zero, zero)
+
 instance VectorSpace Double where
     a +. b = a+b
     a *. b = a*b
