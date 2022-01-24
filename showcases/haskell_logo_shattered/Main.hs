4ef3ec06d7690895ac2bfbc26c2eee7698199248
David Luposchainsky <dluposchainsky@gmail.com>
2022-01-24 17:45:51 +0100

    Housekeeping

--- showcases/haskell_logo_shattered/Main.hs
+++ showcases/haskell_logo_shattered/Main.hs
@@ -2,83 +2,74 @@ module Main (main) where
 
 
 
-import Data.Foldable
-import Graphics.Rendering.Cairo  as Cairo hiding (x, y)
-import qualified System.Random.MWC as MWC
-import Control.Monad.ST
-import qualified Data.Vector as V
+import           Control.Monad.ST
+import           Data.Foldable
+import           Data.Traversable
+import qualified Data.Vector              as V
+import           Graphics.Rendering.Cairo as Cairo hiding (x, y)
+import qualified System.Random.MWC        as MWC
 
 import Draw
-import Geometry as G
+import Geometry        as G
 import Geometry.Shapes
 
 
 
-picWidth, picHeight :: Num a => a
-picWidth = 1000
-picHeight = 720
-
-haskellLogo' :: [Polygon]
-haskellLogo' = G.transform (G.scale 340) haskellLogo
-
 main :: IO ()
-main = withSurfaceAuto "out/haskell_logo_shattered.svg" picWidth picHeight renderDrawing
+main = do
+    withSurfaceAuto "out/haskell_logo_shattered.svg" picWidth picHeight (\surface -> renderWith surface renderDrawing)
+    withSurfaceAuto "out/haskell_logo_shattered.png" picWidth picHeight $ \surface -> renderWith surface $ do
+        cairoScope $ do
+            setColor white
+            paint
+        renderDrawing
   where
-    renderDrawing surface = renderWith surface $ do
-        Cairo.scale 2 2
-        let recurse polygon = minMaxAreaRatio (polygon : haskellLogo') >= 1/64
+    picWidth = 1000
+    picHeight = 720
+    shattered = runST $ do
+        let recurse polygon = minMaxAreaRatio (polygon : haskellLogo) >= 1/128
             acceptCut polygons = minMaxAreaRatio polygons >= 1/3
-            shattered = runST $ do
-                gen <- MWC.initialize (V.fromList [])
-                runShatterProcess gen recurse acceptCut haskellLogo'
+        gen <- MWC.initialize (V.fromList [1])
+        cutUpLogoParts <- for haskellLogo (shatterProcess gen recurse acceptCut)
+        for (concat cutUpLogoParts) $ \polygon -> do
+            angle <- fmap deg (MWC.uniformRM (-15, 15) gen)
+            pure (G.transform (G.rotateAround (polygonCenter polygon) angle) polygon)
 
+    renderDrawing = do
         gen <- liftIO $ MWC.initialize (V.fromList [])
-        drawing gen shattered
+        let fitToCanvas = G.transform (transformBoundingBox shattered (Vec2 10 10, Vec2 (fromIntegral picWidth-10) (fromIntegral picHeight-10)) FitAllMaintainAspect)
+        drawing gen (fitToCanvas shattered)
 
-shatterProcessS
+shatterProcess
     :: MWC.GenST s
     -> (Polygon -> Bool)   -- ^ Recursively subdivide the current polygon?
     -> ([Polygon] -> Bool) -- ^ Accept the cut result, or retry with a different random cut line?
     -> Polygon             -- ^ Initial polygon, cut only if the recursion predicate applies
     -> ST s [Polygon]
-shatterProcessS gen recurse acceptCut polygon
-    | recurse polygon = do
-        cutPieces <- randomCutS gen acceptCut polygon
-        let triangulated = concatMap triangulate cutPieces
-        if acceptCut triangulated
-            then fmap concat (traverse (shatterProcessS gen recurse acceptCut) triangulated)
-            else shatterProcessS gen recurse acceptCut polygon
-    | otherwise = pure [polygon]
-
-runShatterProcess
-    :: MWC.GenST s
-    -> (Polygon -> Bool)
-    -> ([Polygon] -> Bool)
-    -> [Polygon]
-    -> ST s [Polygon]
-runShatterProcess gen recurse acceptCut initialPolygon
-  = fmap concat (traverse (shatterProcessS gen recurse acceptCut) initialPolygon)
+shatterProcess _ recurse _ polygon
+    | not (recurse polygon) = pure [polygon]
+shatterProcess gen recurse acceptCut polygon = do
+    cutPieces <- randomCut gen polygon
+    let triangulated = concatMap triangulate cutPieces
+    if acceptCut triangulated
+        then do
+            subcuts <- traverse (shatterProcess gen recurse acceptCut) triangulated
+            pure (concat subcuts)
+        else shatterProcess gen recurse acceptCut polygon
 
 polygonCenter :: Polygon -> Vec2
-polygonCenter (Polygon corners) = foldl' (+.) (Vec2 0 0) corners /. fromIntegral (length corners)
+polygonCenter (Polygon corners) = foldl' (+.) zero corners /. fromIntegral (length corners)
 
-randomCutS
+randomCut
     :: MWC.Gen s
-    -> ([Polygon] -> Bool) -- ^ Accept the cut result, or retry with a different random cut line?
-    -> Polygon             -- ^ Initial polygon, cut only if the recursion predicate applies
+    -> Polygon -- ^ Initial polygon, cut only if the recursion predicate applies
     -> ST s [Polygon]
-randomCutS gen acceptCut polygon = findGoodCut
-  where
-    BoundingBox vMin vMax = boundingBox polygon
-
-    findGoodCut = do
-        p <- MWC.uniformRM (vMin, vMax) gen
-        angle <- fmap deg (MWC.uniformRM (0, 360) gen)
-        let scissors = angledLine p angle (Distance 1)
-            cutResult = cutPolygon scissors polygon
-        if acceptCut cutResult
-            then pure cutResult
-            else findGoodCut
+randomCut gen polygon = do
+    let BoundingBox vMin vMax = boundingBox polygon
+    p <- MWC.uniformRM (vMin, vMax) gen
+    angle <- MWC.uniformM gen
+    let scissors = angledLine p angle (Distance 1)
+    pure (cutPolygon scissors polygon)
 
 -- | Calculate the min/max ratio of the areas of a list of polygons. Useful to
 -- build cutoff predicates with, e.g.
@@ -93,18 +84,16 @@ minMaxAreaRatio cutResult
         Area maxA = maximum cutResultAreas
     in minA / maxA
 
-drawing :: Foldable t => MWC.GenIO -> t Polygon -> Render ()
+drawing :: MWC.GenIO -> [Polygon] -> Render ()
 drawing gen shattered = do
-    Cairo.translate 10 10
     setLineWidth 0.5
     cairoScope $ for_ shattered $ \polygon -> do
         let hue = 30
         saturation <- liftIO $ MWC.uniformRM (0.2, 0.7) gen
         let value = 1
         let alpha = 1
-        angle <- liftIO $ fmap deg (MWC.uniformRM (-15, 15) gen)
         setColor (hsva hue saturation value alpha)
-        polygonSketch (G.transform (G.rotateAround (polygonCenter polygon) angle) polygon)
+        polygonSketch polygon
         fillPreserve
         setColor black
         stroke
