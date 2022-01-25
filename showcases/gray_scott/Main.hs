8f6b63bb23373aaf61e3036001d57a6a32e7e24c
Franz Thoma <franz.thoma@tngtech.com>
2022-01-25 13:32:25 +0100

    Some optimizations

--- showcases/gray_scott/Main.hs
+++ showcases/gray_scott/Main.hs
@@ -4,14 +4,15 @@ module Main where
 import qualified Data.Map.Strict as M
 import qualified Graphics.Rendering.Cairo as Cairo
 import Data.Maybe (fromMaybe)
+import System.Random.MWC (create)
 
 import Draw
 import Geometry
-import Graphics.Rendering.Cairo (RectangleInt(y))
+import Sampling
 
 picWidth, picHeight :: Num a => a
-picWidth = 50
-picHeight = 50
+picWidth = 100
+picHeight = 100
 
 main :: IO ()
 main = withSurfaceAuto "out/gray_scott.png" picWidth picHeight renderDrawing
@@ -22,6 +23,13 @@ main = withSurfaceAuto "out/gray_scott.png" picWidth picHeight renderDrawing
 
 drawing :: Cairo.Render ()
 drawing = do
+    gen <- Cairo.liftIO create
+    seeds <- Cairo.liftIO $ poissonDisc PoissonDisc
+        { width = picWidth
+        , height = picHeight
+        , k = 4
+        , radius = 100
+        , .. }
     let grayScottProcess = grayScott GS
             { feedRateU = 0.04
             , killRateV = 0.1
@@ -29,15 +37,12 @@ drawing = do
             , diffusionRateV = 0.1
             , width = picWidth
             , height = picHeight }
-        origin = Vec2 (picWidth/2) (picHeight/2)
-        p1 = Vec2 (picWidth/2.9) (picHeight/2.8)
-        p2 = Vec2 (picWidth/2.6) (picHeight/1.2)
         initialState = M.fromList
-            [ (p, (u, 1-u))
+            [ (p, (1 - v, v))
             | x <- [0..picWidth]
             , y <- [0..picHeight]
             , let p = Vec2 x y
-            , let u = 1 - exp (- 0.5 * normSquare (p -. origin)) - exp (- normSquare (p -. p1)) - exp (- normSquare (p -. p2))
+            , let v = sum ((\q -> exp (- 0.5 * normSquare (p -. q))) <$> seeds)
             ]
         finalState = foldr ($) initialState (replicate 400 grayScottProcess)
     for_ [ Vec2 x y | x <- [0, 1..picWidth], y <- [0, 1..picHeight] ] $ \p@(Vec2 x y) -> do
@@ -55,16 +60,16 @@ data GrayScott = GS
     }
 
 (!) :: M.Map Vec2 (Double, Double) -> Vec2 -> (Double, Double)
-(!) grid p = fromMaybe (1, 0) (grid M.!? p)
+(!) grid (Vec2 x y) = fromMaybe (1, 0) (grid M.!? p)
+  where p = Vec2 (fromIntegral (round x `mod` picWidth)) (fromIntegral (round y `mod` picHeight))
 
 grayScott :: GrayScott -> M.Map Vec2 (Double, Double) -> M.Map Vec2 (Double, Double)
-grayScott GS{..} grid = foldl' (\g p -> M.insert p (grayScottStep p) g) grid [ Vec2 x y | x <- [0..width], y <- [0..height] ]
+grayScott GS{..} grid = M.mapWithKey grayScottStep grid
   where
-    grayScottStep p = (u0 + deltaU, v0 + deltaV)
+    grayScottStep p (u0, v0) = (u0 + deltaU, v0 + deltaV)
       where
         u q = fst (grid ! q)
         v q = snd (grid ! q)
-        (u0, v0) = grid ! p
         deltaU = diffusionRateU * laplace u p - u0 * v0^2 + feedRateU * (1 - u0)
         deltaV = diffusionRateV * laplace v p + u0 * v0^2 - killRateV * v0
-        laplace f (Vec2 x y) = (f (Vec2 (x+1) y) + f (Vec2 (x-1) y) - 2 * f (Vec2 x y)) + (f (Vec2 x (y+1)) + f (Vec2 x (y-1)) - 2 * f (Vec2 x y))
+        laplace f (Vec2 x y) = f (Vec2 (x+1) y) + f (Vec2 (x-1) y) + f (Vec2 x (y+1)) + f (Vec2 x (y-1)) - 4 * f (Vec2 x y)
