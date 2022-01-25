de017d20b3b7814419266fbcb9cc67be47f1d322
Franz Thoma <franz.thoma@tngtech.com>
2022-01-25 12:02:24 +0100

    Better initial value

--- showcases/gray_scott/Main.hs
+++ showcases/gray_scott/Main.hs
@@ -23,23 +23,23 @@ main = withSurfaceAuto "out/gray_scott.png" picWidth picHeight renderDrawing
 drawing :: Cairo.Render ()
 drawing = do
     let grayScottProcess = grayScott GS
-            { feedRateU = 0.06
-            , killRateV = 0.09
+            { feedRateU = 0.04
+            , killRateV = 0.1
             , diffusionRateU = 0.2
             , diffusionRateV = 0.1
             , width = picWidth
             , height = picHeight }
+        origin = Vec2 (picWidth/2) (picHeight/2)
+        p1 = Vec2 (picWidth/2.9) (picHeight/2.8)
+        p2 = Vec2 (picWidth/2.6) (picHeight/1.2)
         initialState = M.fromList
-            [ (Vec2 (picWidth/2) (picHeight/2), (0, 1))
-            , (Vec2 (picWidth/2 + 1) (picHeight/2), (0, 1))
-            , (Vec2 (picWidth/2 + 1) (picHeight/2 + 1), (0, 1))
-            , (Vec2 (picWidth/2) (picHeight/2 + 1), (0, 1))
-            , (Vec2 (picWidth/2 + 10) (picHeight/2 + 1), (0, 1))
-            , (Vec2 (picWidth/2 + 11) (picHeight/2 + 1), (0, 1))
-            , (Vec2 (picWidth/2 + 11) (picHeight/2 + 2), (0, 1))
-            , (Vec2 (picWidth/2 + 10) (picHeight/2 + 2), (0, 1))
+            [ (p, (u, 1-u))
+            | x <- [0..picWidth]
+            , y <- [0..picHeight]
+            , let p = Vec2 x y
+            , let u = 1 - exp (- 0.5 * normSquare (p -. origin)) - exp (- normSquare (p -. p1)) - exp (- normSquare (p -. p2))
             ]
-        finalState = foldr ($) initialState (replicate 100 grayScottProcess)
+        finalState = foldr ($) initialState (replicate 400 grayScottProcess)
     for_ [ Vec2 x y | x <- [0, 1..picWidth], y <- [0, 1..picHeight] ] $ \p@(Vec2 x y) -> do
         Cairo.rectangle x y 1 1
         setColor (hsv 0 0 (fst (finalState ! p)))
