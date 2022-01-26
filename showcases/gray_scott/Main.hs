36ac32b9a4bbcefb44ca067a55b1fe3f33966da4
Franz Thoma <franz.thoma@tngtech.com>
2022-01-26 18:47:51 +0100

    Fix some problems

--- showcases/gray_scott/Main.hs
+++ showcases/gray_scott/Main.hs
@@ -2,17 +2,14 @@
 module Main where
 
 import qualified Data.Map.Strict as M
-import Control.Comonad
 import System.Random.MWC (create)
 import Text.Printf (printf)
 import qualified Codec.Picture as P
-import qualified Data.Vector.Storable as V
 
 import Delaunay.Internal
 import Draw
 import Geometry hiding (Grid)
 import Sampling
-import Zipper
 
 picWidth, picHeight :: Num a => a
 picWidth = 100
@@ -25,12 +22,10 @@ main = do
         height = picHeight
         k = 4
 
+    seeds <- poissonDisc PoissonDisc { radius = 50, .. }
     vertices <- poissonDisc PoissonDisc { radius = 1, .. }
     let lattice = bowyerWatson (BoundingBox (Vec2 0 0) (Vec2 picWidth picHeight)) vertices
-
-    seeds <- poissonDisc PoissonDisc { radius = 100, .. }
-
-    let generator p = sum ((\q -> exp (- 0.5 * normSquare (p -. q))) <$> seeds)
+        generator p = sum ((\q -> exp (- 0.5 * normSquare (p -. q))) <$> seeds)
         initialState = M.mapWithKey (\p ps -> ((1 - generator p, generator p), toList ps)) (vertexGraph lattice)
         grayScottProcess = grayScott GS
             { feedRateU = 0.029
@@ -39,7 +34,7 @@ main = do
             , diffusionRateV = 0.1
             , width = picWidth
             , height = picHeight }
-        frames = take 1000 (iterate (grayScottProcess . grayScottProcess . grayScottProcess . grayScottProcess . grayScottProcess) initialState)
+        frames = take 5 (iterate (grayScottProcess . grayScottProcess . grayScottProcess . grayScottProcess . grayScottProcess) initialState)
     for_ (zip [0 :: Int ..] frames) $ \(index, grid) -> do
         let file = printf "out/gray_scott_%06i.png" index
         P.writePng file (renderImage grid)
@@ -47,6 +42,7 @@ main = do
 renderImage :: Grid -> P.Image P.Pixel8
 renderImage grid = P.generateImage pixel picWidth picHeight
   where
+
     pixel x y = round $ 255 * (sum pointsWithinRadius / fromIntegral (length pointsWithinRadius))
       where
         p = Vec2 (fromIntegral x) (fromIntegral y)
@@ -73,4 +69,4 @@ grayScott GS{..} grid = M.mapWithKey grayScottStep grid
         deltaU = diffusionRateU * laplace u - u0 * v0^2 + feedRateU * (1 - u0)
         deltaV = diffusionRateV * laplace v + u0 * v0^2 - (feedRateU + killRateV) * v0
         laplace f = sum (delta f <$> neighbours) / fromIntegral (length neighbours)
-        delta f q = (f q - f p) / norm (q -. p)
+        delta f q = (f q - f p) / normSquare (q -. p)
