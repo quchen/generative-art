c3865c8ed32f5f1f24249486780177bfa3b920ef
Franz Thoma <franz.thoma@tngtech.com>
2022-01-30 17:01:29 +0100

    Experimental new colors based on reaction density and gradient

--- showcases/gray_scott/Main.hs
+++ showcases/gray_scott/Main.hs
@@ -4,7 +4,9 @@ module Main where
 import System.Random.MWC (create)
 import Text.Printf (printf)
 import qualified Codec.Picture as P
+import qualified Codec.Picture.Extra as P
 import qualified Data.Vector.Storable as V
+import qualified Data.Vector.Unboxed as U
 
 import Draw
 import Geometry
@@ -12,8 +14,8 @@ import Plane
 import Sampling
 
 picWidth, picHeight :: Num a => a
-picWidth = 960
-picHeight = 540
+picWidth = 480
+picHeight = 270
 
 main :: IO ()
 main = do
@@ -38,7 +40,7 @@ main = do
             [ row
             | y <- [0..picHeight - 1]
             , let row =
-                    [ (u, v)
+                    [ (u, v, 0, 0)
                     | x <- [0..picWidth - 1]
                     , let p = Vec2 x y
                     , let u = 1 - sum ((\q -> exp (- 0.05 * diffusionRate * normSquare (p -. q))) <$> seeds)
@@ -49,18 +51,46 @@ main = do
         frames = take 100 (iterate (grayScott 5 params) initialState)
 
     for_ (zip [0 :: Int ..] frames) $ \(index, grid) -> do
-        let file = printf "out/gray_scott_%06i.png" index
-        P.writePng file (renderImage grid)
+        P.writePng (printf "out/gray_scott_%06i.png" index) (renderImageGradient grid)
+        P.writePng (printf "out/gray_scott_%06i_experimental.png" index) $ P.beside
+            [ P.below
+                [ fromGrayscale $ renderImageGrayscale (mapPlane (\(u, _, _, _) -> 1-u) grid)
+                , fromGrayscale $ renderImageGrayscale (mapPlane (\(_, v, _, _) -> v) grid)
+                ]
+            , P.below
+                [ fromGrayscale $ renderImageGrayscale (mapPlane (\(_, _, du, _) -> 500*du) grid)
+                , fromGrayscale $ renderImageGrayscale (mapPlane (\(_, _, _, dv) -> 500*dv) grid)
+                ]
+            , P.below
+                [ fromGrayscale $ renderImageGrayscale (mapPlane (\(u, v, _, _) -> 10*u*v*v) grid)
+                , renderImageGradient' (\(u, v, du, dv) -> du *. (500, 0, 250) +. dv *. (500, 0, 0) +. gradient (22*u*v*v)) grid
+                ]
+            ]
 
-renderImage :: Grid -> P.Image P.PixelRGB8
-renderImage grid = P.Image picWidth picHeight $ V.concatMap renderPixel $ V.convert $ items $ mapPlane snd grid
-  where renderPixel v = let (r, g, b) = gradient (2.4*v) in V.fromList [r, g, b]
+fromGrayscale :: P.Image P.Pixel8 -> P.Image P.PixelRGB8
+fromGrayscale (P.Image w h pixels) = P.Image w h (V.concatMap (\px -> V.fromList [px, px, px]) pixels)
 
-gradient :: Double -> (P.Pixel8, P.Pixel8, P.Pixel8)
+renderImageGrayscale :: Plane Double -> P.Image P.Pixel8
+renderImageGrayscale grid = P.Image picWidth picHeight (V.convert (items (mapPlane renderPixel grid)))
+  where renderPixel = round . max 0 . min 255 . (* 255)
+
+renderImageGradient' :: ((Double, Double, Double, Double) -> (Double, Double, Double)) -> Grid -> P.Image P.PixelRGB8
+renderImageGradient' f grid = P.Image picWidth picHeight (V.convert $ U.concatMap renderPixel (items grid))
+  where
+    renderPixel uv = let (r, g, b) = f uv in U.fromList [pixel8 r, pixel8 g, pixel8 b]
+    pixel8 = round . clamp 0 255 . (* 255)
+
+renderImageGradient :: Grid -> P.Image P.PixelRGB8
+renderImageGradient grid = P.Image picWidth picHeight $ V.concatMap renderPixel $ V.convert $ items $ mapPlane (\(_, v, _, _) -> v) grid
+  where
+    renderPixel v = let (r, g, b) = gradient (2.4*v) in V.fromList [pixel8 r, pixel8 g, pixel8 b]
+    pixel8 = round . clamp 0 255 . (* 255)
+
+gradient :: Double -> (Double, Double, Double)
 gradient v
-    | v < 0.4 = pixelRGB $ interpolate (2.5*v) color1 color0
-    | v < 1.0 = pixelRGB $ interpolate (1.667*(v-0.4)) color2 color1
-    | otherwise = pixelRGB $ interpolate (v-1) color3 color2
+    | v < 0.4 = interpolate (2.5*v) color1 color0
+    | v < 1.0 = interpolate (1.667*(v-0.4)) color2 color1
+    | otherwise = interpolate (v-1) color3 color2
   where
     color0, color1, color2, color3 :: (Double, Double, Double)
     color0 = (0, 0, 0.1)
@@ -68,13 +98,11 @@ gradient v
     color2 = (0.1, 0.9, 0.1)
     color3 = (255, 0, 0)
     interpolate a c1 c2 = a *. c1 +. (1-a) *. c2
-    pixel8 = round . clamp 0 255 . (* 255)
-    pixelRGB (r, g, b) = (pixel8 r, pixel8 g, pixel8 b)
 
 clamp :: (Ord a, Num a) => a -> a -> a -> a
 clamp lower upper = max lower . min upper
 
-type Grid = Plane (Double, Double)
+type Grid = Plane (Double, Double, Double, Double)
 
 data GrayScott = GS
     { feedRateU :: Double
@@ -89,12 +117,12 @@ data GrayScott = GS
 grayScott :: Int -> GrayScott -> Grid -> Grid
 grayScott steps GS{..} = repeatF steps (mapNeighbours grayScottStep)
   where
-    grayScottStep (uv11, uv12, uv13, uv21, uv22, uv23, uv31, uv32, uv33) = uv22 +. step *. (deltaU, deltaV)
+    grayScottStep (uv11, uv12, uv13, uv21, uv22, uv23, uv31, uv32, uv33) = (u0, v0, deltaU, deltaV) +. step *. (deltaU, deltaV, 0, 0)
       where
-        (u0, v0) = uv22
+        (u0, v0, _, _) = uv22
         deltaU = diffusionRateU * laplaceU - u0 * v0^2 + feedRateU * (1 - u0)
         deltaV = diffusionRateV * laplaceV + u0 * v0^2 - (feedRateU + killRateV) * v0
-        (laplaceU, laplaceV) = (uv11 +. 2*.uv12 +. uv13 +. 2*.uv21 -. 12*.uv22 +. 2*. uv23 +. uv31 +. 2*.uv32 +. uv33) /. 4
+        (laplaceU, laplaceV, _, _) = (uv11 +. 2*.uv12 +. uv13 +. 2*.uv21 -. 12*.uv22 +. 2*. uv23 +. uv31 +. 2*.uv32 +. uv33) /. 4
 
     repeatF :: Int -> (a -> a) -> a -> a
     repeatF 0 _ = id
