9f379cee07c70ca93a67001ae00ccc26cce3ad7b
Franz Thoma <franz.thoma@tngtech.com>
2022-01-30 23:11:34 +0100

    Cleanup

--- showcases/gray_scott/Main.hs
+++ showcases/gray_scott/Main.hs
@@ -4,7 +4,6 @@ module Main where
 import System.Random.MWC (create)
 import Text.Printf (printf)
 import qualified Codec.Picture as P
-import qualified Codec.Picture.Extra as P
 import qualified Data.Vector.Storable as V
 import qualified Data.Vector.Unboxed as U
 
@@ -14,8 +13,8 @@ import Plane
 import Sampling
 
 picWidth, picHeight :: Num a => a
-picWidth = 480
-picHeight = 270
+picWidth = 960
+picHeight = 540
 
 main :: IO ()
 main = do
@@ -50,54 +49,34 @@ main = do
 
         frames = take 100 (iterate (grayScott 5 params) initialState)
 
-    for_ (zip [0 :: Int ..] frames) $ \(index, grid) -> do
-        P.writePng (printf "out/gray_scott_%06i.png" index) (renderImageGradient grid)
-        P.writePng (printf "out/gray_scott_%06i_experimental.png" index) $ P.beside
-            [ P.below
-                [ fromGrayscale $ renderImageGrayscale (mapPlane (\(u, _, _, _) -> 1-u) grid)
-                , fromGrayscale $ renderImageGrayscale (mapPlane (\(_, v, _, _) -> v) grid)
-                , renderImageGradient' (\(_, _, du, dv) -> tanh (200*(du+dv)) *. (-1, 0, 1)) grid
-                ]
-            , P.below
-                [ renderImageGradient' (\(_, _, du, _) -> tanh (400*du) *. (-1, 0, 1)) grid
-                , renderImageGradient' (\(_, _, _, dv) -> tanh (400*dv) *. (1, 0, -1)) grid
-                , renderImageGradient' (\(_, _, du, dv) -> tanh (200*(du-dv)) *. (-1, 0, 1)) grid
-                ]
-            , P.below
-                [ fromGrayscale $ renderImageGrayscale (mapPlane (\(u, v, _, _) -> 10*u*v*v) grid)
-                , renderImageGradient' (\(u, v, du, dv) -> tanh (400 * max 0 du) *. (0, 0, 0.5) +. tanh (-400 * min 0 dv) *. (0.7, 0, -0.1) +. tanh (400 * max 0 dv) *. (0.4, 0.1, 0) +. gradient (22*u*v*v)) grid
-                , renderImageGradient' (\(u, v, du, dv) -> tanh (200 * du) *. (0.75, 0, 0.5) -. tanh (200 * dv) *. (1, 0, 0) +. gradient (22*u*v*v)) grid
-                ]
-            ]
-
-fromGrayscale :: P.Image P.Pixel8 -> P.Image P.PixelRGB8
-fromGrayscale (P.Image w h pixels) = P.Image w h (V.concatMap (\px -> V.fromList [px, px, px]) pixels)
+    for_ (zip [0 :: Int ..] frames) $ \(index, grid) ->
+        P.writePng (printf "out/gray_scott_%06i.png" index) (renderImageColor (colorFront +. colorTrail +. colorReaction) grid)
 
 renderImageGrayscale :: Plane Double -> P.Image P.Pixel8
 renderImageGrayscale grid = P.Image picWidth picHeight (V.convert (items (mapPlane renderPixel grid)))
   where renderPixel = round . max 0 . min 255 . (* 255)
 
-renderImageGradient' :: ((Double, Double, Double, Double) -> (Double, Double, Double)) -> Grid -> P.Image P.PixelRGB8
-renderImageGradient' f grid = P.Image picWidth picHeight (V.convert $ U.concatMap renderPixel (items grid))
+renderImageColor :: ((Double, Double, Double, Double) -> (Double, Double, Double)) -> Grid -> P.Image P.PixelRGB8
+renderImageColor f grid = P.Image picWidth picHeight (V.convert $ U.concatMap renderPixel (items grid))
   where
     renderPixel uv = let (r, g, b) = f uv in U.fromList [pixel8 r, pixel8 g, pixel8 b]
     pixel8 = round . clamp 0 255 . (* 255)
 
-renderImageGradient :: Grid -> P.Image P.PixelRGB8
-renderImageGradient grid = P.Image picWidth picHeight $ V.concatMap renderPixel $ V.convert $ items $ mapPlane (\(_, v, _, _) -> v) grid
-  where
-    renderPixel v = let (r, g, b) = gradient (2.4*v) in V.fromList [pixel8 r, pixel8 g, pixel8 b]
-    pixel8 = round . clamp 0 255 . (* 255)
+colorFront :: (Double, Double, Double, Double) -> (Double, Double, Double)
+colorFront (_, _, _, dv)= tanh (400 * max 0 dv) *. (0.4, 0.1, 0)
 
-gradient :: Double -> (Double, Double, Double)
-gradient v
-    | v < 0.4 = interpolate (2.5*v) color1 color0
-    | v < 1.0 = interpolate (1.667*(v-0.4)) color2 color1
-    | otherwise = interpolate (v-1) color3 color2
+colorTrail :: (Double, Double, Double, Double) -> (Double, Double, Double)
+colorTrail (_, _, du, dv) = tanh (400 * max 0 du) *. (0, 0, 0.5) +. tanh (-400 * min 0 dv) *. (0.7, 0, -0.1)
+
+colorReaction :: (Double, Double, Double, Double) -> (Double, Double, Double)
+colorReaction (u, v, _, _) = case 25 * u * v * v of
+    x | x < 0.5   -> interpolate (2*x) color1 color0
+      | x < 1.0   -> interpolate (2*(x-0.5)) color2 color1
+      | otherwise -> interpolate (x-1) color3 color2
   where
     color0, color1, color2, color3 :: (Double, Double, Double)
     color0 = (0, 0, 0.1)
-    color1 = 0.5 *. (0.1, 0.25, 0.5)
+    color1 = 0.7 *. (0.1, 0.25, 0.5)
     color2 = (0.1, 0.9, 0.1)
     color3 = (255, 0, 0)
     interpolate a c1 c2 = a *. c1 +. (1-a) *. c2
