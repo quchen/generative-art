b743a8f75dfa4c538b924d04875e0e4ed09d8b36
David Luposchainsky <dluposchainsky@gmail.com>
2022-01-31 20:45:11 +0100

    Use DrawState type for safety

--- src/Geometry/SvgPathParser.hs
+++ src/Geometry/SvgPathParser.hs
@@ -43,57 +43,65 @@ move = MP.label "move (mM)" $ do
         Absolute -> const v
         Relative -> (+. v)
 
-lineXY :: Ord err => MP.Parsec err Text (State (Vec2, Vec2) Line)
+data DrawState = DrawState
+    { _startOfTrajectory :: Vec2
+    , _currentPoint :: Vec2
+    } deriving (Eq, Ord, Show)
+
+
+
+lineXY :: Ord err => MP.Parsec err Text (State DrawState Line)
 lineXY = do
     absRel <- Absolute <$ char_ 'L' <|> Relative <$ char_ 'l'
     p <- vec2
     pure $ do
-        (start, current) <- get
+        DrawState start current <- get
         let new = case absRel of
                 Absolute -> p
                 Relative -> current +. p
-        put (start, new)
+        put (DrawState start new)
         pure (Line current new)
 
-lineH :: Ord err => MP.Parsec err Text (State (Vec2, Vec2) Line)
+lineH :: Ord err => MP.Parsec err Text (State DrawState Line)
 lineH = do
     absRel <- Absolute <$ char_ 'H' <|> Relative <$ char_ 'h'
     x' <- double
     pure $ do
-        (start, current@(Vec2 x y)) <- get
+        DrawState start current@(Vec2 x y) <- get
         let new = case absRel of
                 Absolute -> Vec2 x' y
                 Relative -> Vec2 (x+x') y
-        put (start, new)
+        put (DrawState start new)
         pure (Line current new)
 
-lineV :: Ord err => MP.Parsec err Text (State (Vec2, Vec2) Line)
+lineV :: Ord err => MP.Parsec err Text (State DrawState Line)
 lineV = do
     absRel <- Absolute <$ char_ 'V' <|> Relative <$ char_ 'v'
     y' <- double
     pure $ do
-        (start, current@(Vec2 x y)) <- get
+        DrawState start current@(Vec2 x y) <- get
         let new = case absRel of
                 Absolute -> Vec2 x y'
                 Relative -> Vec2 x (y+y')
-        put (start, new)
+        put (DrawState start new)
         pure (Line current new)
 
-line :: Ord err => MP.Parsec err Text (State (Vec2, Vec2) Line)
+line :: Ord err => MP.Parsec err Text (State DrawState Line)
 line = MP.label "line (lLhHvV)" $ lineXY <|> lineH <|> lineV
 
-bezier :: Ord err => MP.Parsec err Text (State (Vec2, Vec2) Bezier)
+bezier :: Ord err => MP.Parsec err Text (State DrawState Bezier)
 bezier = MP.label "cubical bezier (cC)" $ do
     absRel <- Absolute <$ char_ 'C' <|> Relative <$ char_ 'c'
     helper1 <- vec2
     helper2 <- vec2
     end <- vec2
     pure $ do
-        (start, current) <- get
+        DrawState start current <- get
         let [h1', h2', end'] = case absRel of
                 Absolute -> [helper1, helper2, end]
                 Relative -> map (+. start) [helper1, helper2, end]
-        put (start, end')
+        put (DrawState start end')
+
         pure (Bezier current h1' h2' end')
 
 bezierCubic :: MP.Parsec Text Text a
@@ -106,12 +114,12 @@ ellipticalArc = MP.label "" $ do
     _cubicChar <- asum (map char_ "aA")
     MP.customFailure ("Elliptical arc curves are not supported by the parser")
 
-closePath :: Ord err => MP.Parsec err Text (State (Vec2, Vec2) Line)
+closePath :: Ord err => MP.Parsec err Text (State DrawState Line)
 closePath = MP.label "close path (zZ)" $ do
     char_ 'Z' <|> char_ 'z'
     pure $ do
-        (start, current) <- get
-        put (start, start) *> pure (Line current start)
+        DrawState start current <- get
+        put (DrawState start start) *> pure (Line current start)
 
 instance MP.ShowErrorComponent Text where
     showErrorComponent = show
@@ -125,8 +133,8 @@ parse input = case MP.parse (MPC.space *> many parseSinglePathInstruction <* MP.
 
 parseSinglePathInstruction :: MP.Parsec Text Text
     (
-        Vec2 -> Vec2,                    -- Move to beginning
-        [State (Vec2, Vec2) (Either Line Bezier)] -- Draw path
+        Vec2 -> Vec2,                          -- Move to beginning
+        [State DrawState (Either Line Bezier)] -- Draw path
     )
 parseSinglePathInstruction = do
     modifyStart <- move
@@ -147,14 +155,14 @@ parseSinglePathInstruction = do
 interpretSingleDrawingInstruction
     :: Traversable t
     => Vec2
-    -> (Vec2 -> Vec2, t (State (Vec2, Vec2) pathSegment))
-    -> (t pathSegment, (Vec2, Vec2))
-interpretSingleDrawingInstruction origin (moveToStart, steps) = runState (sequence steps) (moveToStart origin, moveToStart origin)
+    -> (Vec2 -> Vec2, t (State DrawState pathSegment))
+    -> (t pathSegment, DrawState)
+interpretSingleDrawingInstruction origin (moveToStart, steps) = runState (sequence steps) (DrawState (moveToStart origin) (moveToStart origin))
 
-interpretAllDrawingInstructions :: [(Vec2 -> Vec2, [State (Vec2, Vec2) pathSegment])] -> [[pathSegment]]
-interpretAllDrawingInstructions = go (zero, zero)
+interpretAllDrawingInstructions :: [(Vec2 -> Vec2, [State DrawState pathSegment])] -> [[pathSegment]]
+interpretAllDrawingInstructions = go (DrawState zero zero)
   where
     go _ [] = []
-    go (_, startPoint) (i:is) =
+    go (DrawState _ startPoint) (i:is) =
         let (path, state') = interpretSingleDrawingInstruction startPoint i
         in path : go state' is
