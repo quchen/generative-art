66d507858a471969eb9f38255d2b473d15a258bf
David Luposchainsky <dluposchainsky@gmail.com>
2022-02-02 20:53:39 +0100

    Wow, this is wrong, but very pretty! I should learn to reproduce it…

--- test/Test/Geometry/Processes/ApollonianGasket.hs
+++ test/Test/Geometry/Processes/ApollonianGasket.hs
@@ -16,6 +16,7 @@ import Test.TastyAll
 tests :: TestTree
 tests = testGroup "Apollonian gasket"
     [ threeCircles
+    , manyCircles
     ]
 
 threeCircles :: TestTree
@@ -53,6 +54,38 @@ threeCircles = testCase "Manually defined" $ renderAllFormats 300 300 "out/manua
             ++ [topL, topR, topB]
             ++ [rightT, rightL, rightB]
             ++ [leftT, leftR, leftB]
+
+    for_ (zip [0..] apoCircles) $ \(i, apo) -> cairoScope $ do
+        setLineWidth 1
+        setColor (mathematica97 i)
+        apoCircleSketch apo
+        stroke
+
+manyCircles :: TestTree
+manyCircles = testCase "Automatically built" $ renderAllFormats 300 300 "out/automatically_built" $ do
+    let startL = toApoCircle $ Circle (Vec2 100 100) 50
+        startR = toApoCircle $ Circle (Vec2 200 100) 50
+        startB = toApoCircle $ Circle (G.transform (rotateAround (Vec2 100 100) (deg 60)) (Vec2 200 100)) 50
+
+        large = newCircle (-) (-) startL startR startB
+
+        top = newCircle (+) (+) large startL startR
+        bl = newCircle (+) (-) large startL startB
+        br = newCircle (+) (+) large startR startB
+
+        recurse [] = []
+        recurse ((c1, c2, c3) : rest) =
+            let new@(ApoCircle _ k) = newCircle (+) (+) c1 c2 c3
+            in if k > 10
+                then recurse rest
+                else new : recurse ((c1, c2, new) : (c1, c3, new) : (c2, c3, new) : rest)
+
+
+        apoCircleSketch c =
+            let Circle center radius = toCircle c
+            in circleSketch center (abs radius)
+
+        apoCircles = large : top : bl : br : recurse [(large, top, bl), (large, top, br), (top, bl, br), (large, bl, br)]
 
     for_ (zip [0..] apoCircles) $ \(i, apo) -> cairoScope $ do
         setLineWidth 1
