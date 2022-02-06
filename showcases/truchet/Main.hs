30ad259f7db83ddc7a9cfdf13e8136dfb612c477
Franz Thoma <franz.thoma@tngtech.com>
2022-02-06 14:03:13 +0100

    Some fixes to tile declaration

--- showcases/truchet/Main.hs
+++ showcases/truchet/Main.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE DeriveFunctor #-}
 module Main where
 
-import qualified Data.Vector as V
 import qualified Data.Map.Strict as M
 import Data.Traversable (for)
+import qualified Data.Vector as V
 import qualified Graphics.Rendering.Cairo as Cairo
 import System.Random.MWC (uniformRM, GenIO, initialize)
 
@@ -35,9 +35,7 @@ main = do
     withSurfaceAuto file scaledWidth scaledHeight $ \surface -> Cairo.renderWith surface $ do
         Cairo.scale scaleFactor scaleFactor
         cairoScope (setColor backgroundColor >> Cairo.paint)
-        hexagonalCoordinateSystem cellSize 30 >> Cairo.newPath
-        for_ (M.toList tiling) $ \(hex, tile) -> do
-            drawTile hex tile
+        for_ (M.toList tiling) $ \(hex, tile) -> drawTile hex tile
 
 colorScheme :: Int -> Color Double
 colorScheme = mathematica97
@@ -49,7 +47,6 @@ plane :: [Hex]
 plane = hexagonsInRange 15 origin
   where origin = fromVec2 cellSize (Vec2 (picWidth/2) (picHeight/2))
 
-
 newtype Tile a = Tile (M.Map (Direction, Int) (Direction, a)) deriving (Eq, Ord, Show, Functor)
 
 mkTile :: [((Direction, Direction), Int)] -> Tile ()
@@ -60,17 +57,19 @@ mkTile = Tile . go M.empty
     go m (((d1, d2), n) : xs) = foldl' (addArc d1 d2) (go m xs) [1..n]
     addArc :: Direction -> Direction -> M.Map (Direction, Int) (Direction, ()) -> Int -> M.Map (Direction, Int) (Direction, ())
     addArc d1 d2 m i = M.insert (d1, arcIndex d1 d2 i) (d2, ()) . M.insert (d2, arcIndex d2 d1 i) (d1, ()) $ m
-    arcIndex d1 d2 i = if cyclic d1 d2 then 4-i else i
-    cyclic d1 d2 = (6 + fromEnum d1 - fromEnum d2) `mod` 6 <= 3 && d1 < L
+    arcIndex d1 d2 i = if cyclic d1 d2 then i else 4-i
+
+cyclic :: Direction -> Direction -> Bool
+cyclic d1 d2
+    | d1 == reverseDirection d2 = d1 < d2
+    | otherwise = (6 + fromEnum d1 - fromEnum d2) `mod` 6 <= 3
 
 unTile :: Tile a -> [(Direction, Int, Direction, a)]
 unTile (Tile xs) =
     [ (d1, i, d2, a)
-    | ((d1,i), (d2, a)) <- M.toList xs
-    , anticyclic d1 d2
+    | ((d1, i), (d2, a)) <- M.toList xs
+    , cyclic d1 d2
     ]
-  where
-    anticyclic d1 d2 = (6 + fromEnum d1 - fromEnum d2) `mod` 6 >= 3
 
 tiles1 :: V.Vector (Tile ())
 tiles1 = V.fromList $ allRotations =<<
@@ -86,14 +85,14 @@ tiles3 = V.fromList $ allRotations =<<
 
 tiles4 :: V.Vector (Tile ())
 tiles4 = V.fromList $ allRotations =<<
-    [ mkTile [((R, L), k), ((UL, UR), l), ((DL, DR), m)] | k <- [0..3], l <- [0..2], m <- [0..3], k+m <= 5, k+l+m == 7 ]
+    [ mkTile [((L, R), k), ((DL, DR), l), ((UL, UR), m)] | k <- [0..3], l <- [0..2], m <- [0..3], k+m <= 5, k+l+m == 7 ]
 
 tiles5 :: V.Vector (Tile ())
 tiles5 = V.fromList $ allRotations =<<
-    [ mkTile [((R, L), k), ((UL, UR), l), ((L, DL), m), ((DL, DR), n), ((DR, R), m)] | k <- [0..3], l <- [2..3], m <- [0..3], n <- [0..3], if k == 0 then l == 3 else l == 2, m+n <= 3, k+m <= 3, k+n >= 4, k+n <= 5 ]
+    [ mkTile [((L, R), k), ((DL, DR), l), ((L, UL), m), ((UL, UR), n), ((UR, R), m)] | k <- [0..3], l <- [2..3], m <- [0..3], n <- [0..3], if k == 0 then l == 3 else l == 2, m+n <= 3, k+m <= 3, k+n >= 4, k+n <= 5 ]
 
 tiles :: V.Vector (Tile ())
-tiles = V.concat [tiles1, tiles2, tiles3, tiles4, tiles5]
+tiles = V.concat [ tiles1, tiles2, tiles3, tiles4, tiles5 ]
 
 allRotations :: Tile a -> [Tile a]
 allRotations tile = [ rotateTile i tile | i <- [0..6] ]
@@ -116,15 +115,19 @@ randomTile = \gen -> do
     pure (tiles V.! rnd)
   where countTiles = V.length tiles
 
-drawTile :: Hex -> Tile () -> Cairo.Render ()
-drawTile hex tile = for_ (unTile tile) $ drawArc hex
+reverseDirection :: Direction -> Direction
+reverseDirection d = toEnum ((fromEnum d + 3) `mod` 6)
 
-drawArc :: Hex -> (Direction, Int, Direction, ()) -> Cairo.Render ()
-drawArc hex (d1, n, d2, _) = cairoScope $ do
-    sketchArc (fromIntegral n) d1 d2
+drawTile :: Hex -> Tile () -> Cairo.Render ()
+drawTile hex tile = for_ (unTile tile) $ \(d1, i, d2, _) -> drawArc hex(d1, i, d2, i `mod` 2)
+
+drawArc :: Hex -> (Direction, Int, Direction, Int) -> Cairo.Render ()
+drawArc hex (d1, n, d2, c) = cairoScope $ do
+    let i = if cyclic d1 d2 then n else 4-n
+    sketchArc (fromIntegral i) d1 d2
     Cairo.setLineWidth (3/16 * cellSize)
     Cairo.setLineCap Cairo.LineCapRound
-    setColor (colorScheme (n `mod` 2))
+    setColor (colorScheme c)
     Cairo.stroke
   where
     center = toVec2 cellSize hex
@@ -133,12 +136,9 @@ drawArc hex (d1, n, d2, _) = cairoScope $ do
     corner d d' = (center +. nextCenter d +. nextCenter d') /. 3
     [down, lowerLeft, upperLeft, up, upperRight, lowerRight] = [ transform (rotate alpha) (Vec2 0 cellSize) | alpha <- deg <$> [0, 60 .. 300] ]
 
-    sketchArc i L  R  = moveToVec ((0.5 - 0.25 * i) *. up         +. side L)  >> lineToVec ((0.5 - 0.25 * i) *. up         +. side R)
     sketchArc i UL DR = moveToVec ((0.5 - 0.25 * i) *. upperRight +. side UL) >> lineToVec ((0.5 - 0.25 * i) *. upperRight +. side DR)
     sketchArc i UR DL = moveToVec ((0.5 - 0.25 * i) *. lowerRight +. side DL) >> lineToVec ((0.5 - 0.25 * i) *. lowerRight +. side UR)
     sketchArc i R  L  = moveToVec ((0.5 - 0.25 * i) *. down       +. side L)  >> lineToVec ((0.5 - 0.25 * i) *. down       +. side R)
-    sketchArc i DR UL = moveToVec ((0.5 - 0.25 * i) *. lowerLeft  +. side UL) >> lineToVec ((0.5 - 0.25 * i) *. lowerLeft  +. side DR)
-    sketchArc i DL UR = moveToVec ((0.5 - 0.25 * i) *. upperLeft  +. side DL) >> lineToVec ((0.5 - 0.25 * i) *. upperLeft  +. side UR)
 
     sketchArc i L  UR = arcSketch (nextCenter UL) ((1 + 0.25 * i) * cellSize) (deg 30)  (deg 90)
     sketchArc i UL R  = arcSketch (nextCenter UR) ((1 + 0.25 * i) * cellSize) (deg 90)  (deg 150)
