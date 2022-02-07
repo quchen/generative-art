044aec076117aa22716368f6c52628417543f0cf
Franz Thoma <franz.thoma@tngtech.com>
2022-02-07 21:56:08 +0100

    Colorized isoareas

--- showcases/iso_perlin/Main.hs
+++ showcases/iso_perlin/Main.hs
@@ -1,11 +1,13 @@
 module Main where
 
 import Data.Maybe (fromMaybe)
+import qualified Data.Vector as V
 import qualified Graphics.Rendering.Cairo as Cairo
 import Math.Noise (Perlin (..), perlin, getValue)
 
 import Draw
 import Geometry
+import Debug.Trace
 
 
 
@@ -14,11 +16,11 @@ picWidth = 2560
 picHeight = 1440
 
 noiseScale :: Double
-noiseScale = 251
+noiseScale = 250
 
 -- Randomness seed for reproducible results
 seed :: Int
-seed = 519496
+seed = 511151
 
 main :: IO ()
 main = withSurfaceAuto "out/iso-perlin.png" scaledWidth scaledHeight $ \surface -> Cairo.renderWith surface $ do
@@ -27,24 +29,32 @@ main = withSurfaceAuto "out/iso-perlin.png" scaledWidth scaledHeight $ \surface 
         Cairo.setSourceRGB 1 1 1
         Cairo.paint
 
-    let grid = Grid { _range = (Vec2 0 0, Vec2 picWidth picHeight), _maxIndex = (picWidth `div` 10, picHeight `div` 10)}
+    let cellSize = 10
+        grid = Grid
+            { _range = (Vec2 (-cellSize) (-cellSize), Vec2 (picWidth + cellSize) (picHeight + cellSize))
+            , _maxIndex = (round (picWidth / cellSize + 2), round (picHeight / cellSize + 2))
+            }
         isoLine = isoLines grid scalarField
 
-    for_ [-1,-0.9..1] $ \v ->
-        drawIsoLine (isoLine v)
+    for_ [-0.5,-0.4..0.5] $ \v ->
+        drawIsoLine v (isoLine v)
   where
     scaleFactor = 0.39
     scaledWidth = round (picWidth * scaleFactor)
     scaledHeight = round (picHeight * scaleFactor)
 
-drawIsoLine :: [[Vec2]] -> Cairo.Render ()
-drawIsoLine pss = for_ pss $ \(p:ps) -> do
-    moveToVec p
-    for_ ps lineToVec
-    Cairo.stroke
+drawIsoLine :: Double -> [[Vec2]] -> Cairo.Render ()
+drawIsoLine v ls = for_ ls $ \l -> do
+    let smoothLine = V.toList $ bezierSmoothenLoop (V.fromList $ traceShowId l)
+    bezierCurveSketch smoothLine
+    Cairo.closePath
+    setColor (inferno (v+0.5))
+    Cairo.fill
 
 scalarField :: Vec2 -> Double
-scalarField = noise2d . transform (scale' 1 2)
+scalarField (Vec2 x y)
+    | x < 0 || x > picWidth || y < 0 || y > picHeight = -1/0
+    | otherwise = noise2d
   where
     noise = perlin { perlinFrequency = 1/noiseScale, perlinOctaves = 1, perlinSeed = seed }
-    noise2d (Vec2 x y) = fromMaybe 0 $ getValue noise (x, y, 0)
+    noise2d = fromMaybe 0 $ getValue noise (x, y, 0)
