97e6700cb77da924dc399d15423335e225acc970
David Luposchainsky <dluposchainsky@gmail.com>
2022-02-07 23:12:56 +0100

    Housekeeping: voronoi_3d

--- showcases/voronoi_3d/Main.hs
+++ showcases/voronoi_3d/Main.hs
@@ -1,20 +1,24 @@
 {-# LANGUAGE RecordWildCards #-}
 module Main (main) where
 
-import Data.List (sortOn)
-import Data.Maybe (fromMaybe)
-import Data.Ord (comparing)
-import Data.Vector (fromList)
-import Math.Noise (Perlin (..), getValue, perlin)
-import Prelude hiding ((**))
-import System.Random.MWC (initialize)
-import qualified Graphics.Rendering.Cairo as Cairo
+
+
+import           Data.List
+import           Data.Maybe
+import           Data.Ord
+import qualified Data.Vector              as V
+import           Graphics.Rendering.Cairo as C
+import           Math.Noise               (Perlin (..), getValue, perlin)
+import           Prelude                  hiding ((**))
+import           System.Random.MWC
 
 import Draw
-import Geometry
+import Geometry                     as G
 import Geometry.Algorithms.Delaunay
 import Geometry.Algorithms.Sampling
 import Geometry.Algorithms.Voronoi
+
+
 
 picWidth, picHeight :: Num a => a
 picWidth = 2560
@@ -30,7 +34,7 @@ main = do
         scaledWidth = round (scaleFactor * picWidth)
         scaledHeight = round (scaleFactor * picHeight)
 
-    gen <- initialize (fromList [12, 984, 498, 498, 626, 15, 165])
+    gen <- initialize (V.fromList [12, 984, 498, 498, 626, 15, 165])
     let -- constructed so that we have roughly `count` points
         adaptiveRadius = 1440 * sqrt (0.75 / count)
         samplingProps = PoissonDisc { width = 1440, height = 1440, radius = adaptiveRadius, k = 4, ..}
@@ -42,18 +46,18 @@ main = do
         origin = Vec2 (picWidth/2) (picHeight/2)
         voronoiCells = sortOn ((\(Polygon ps) -> minimum (yCoordinate <$> ps)) . fst) $
             ( \c ->
-                ( transform
-                    (  translate (Vec2 0 (picHeight/5))
-                    <> scaleAround' origin 1 0.35
-                    <> rotateAround origin (deg 45)
-                    <> translate (Vec2 560 0 )
-                    <> scaleAround (seed c) 0.9 )
+                ( G.transform
+                    (  G.translate (Vec2 0 (picHeight/5))
+                    <> G.scaleAround' origin 1 0.35
+                    <> G.rotateAround origin (deg 45)
+                    <> G.translate (Vec2 560 0 )
+                    <> G.scaleAround (seed c) 0.9 )
                     (region c)
                 , props c) )
             <$> cells voronoiWithProps
 
-    withSurfaceAuto file scaledWidth scaledHeight $ \surface -> Cairo.renderWith surface $ do
-        cairoScope (setColor (magma 0.05) >> Cairo.paint)
+    withSurfaceAuto file scaledWidth scaledHeight $ \surface -> renderWith surface $ do
+        cairoScope (setColor (magma 0.05) >> paint)
         for_ voronoiCells $ uncurry drawCell
 
 randomHeight :: Vec2 -> Double
@@ -69,34 +73,34 @@ randomColor = \p -> inferno (0.6 + 0.35 * noise2d p)
     noise = perlin { perlinOctaves = 5, perlinFrequency = 0.001, perlinPersistence = 0.65, perlinSeed = 1980166 }
     noise2d (Vec2 x y) = fromMaybe 0 $ getValue noise (x, y, 0)
 
-drawCell :: Polygon -> (Color Double, Double) -> Cairo.Render ()
+drawCell :: Polygon -> (Color Double, Double) -> Render ()
 drawCell (Polygon []) _ = pure ()
 drawCell poly@(Polygon ps) (color, height) = cairoScope $ do
     let lineColor = blend 0.95 color white
         sideColor = blend 0.1 (color `withOpacity` 0.8) (black `withOpacity` 0.3)
         topColor = blend 0.7 (color `withOpacity` 0.8) (black `withOpacity` 0.3)
 
-    Cairo.setLineJoin Cairo.LineJoinBevel
+    C.setLineJoin C.LineJoinBevel
 
     for_ (zip normalizedPoints (drop 1 (cycle normalizedPoints))) $ \(p, q) -> cairoScope $ do
         moveToVec p
         lineToVec q
-        lineToVec (transform (translate (Vec2 0 (-height))) q)
-        lineToVec (transform (translate (Vec2 0 (-height))) p)
+        lineToVec (G.transform (G.translate (Vec2 0 (-height))) q)
+        lineToVec (G.transform (G.translate (Vec2 0 (-height))) p)
         setColor (dissolve 0.8 sideColor)
-        Cairo.fillPreserve
+        fillPreserve
         setColor lineColor
-        Cairo.setLineWidth 2
-        Cairo.stroke
+        setLineWidth 2
+        stroke
 
     cairoScope $ do
-        Cairo.translate 0 (-height)
+        C.translate 0 (-height)
         polygonSketch poly
         setColor topColor
-        Cairo.fillPreserve
+        fillPreserve
         setColor lineColor
-        Cairo.setLineWidth 2
-        Cairo.stroke
+        setLineWidth 2
+        stroke
 
   where
     leftmost = minimumBy (comparing xCoordinate) ps
