34c14fc6477040db0759021c56b2894a2e94fcf4
David Luposchainsky <dluposchainsky@gmail.com>
2022-02-10 22:42:39 +0100

    Rebase fallout

--- showcases/cycloids/Main.hs
+++ showcases/cycloids/Main.hs
@@ -1,11 +1,10 @@
-{-# LANGUAGE RecordWildCards #-}
 module Main where
 
 import Control.Monad (when)
-import qualified Graphics.Rendering.Cairo as Cairo
+import Graphics.Rendering.Cairo as C
 
 import Draw
-import Geometry
+import Geometry as G
 
 
 picHeight, picWidth :: Num a => a
@@ -16,9 +15,9 @@ scaleFactor :: Double
 scaleFactor = 1
 
 main :: IO ()
-main = withSurfaceAuto "out/cycloids.png" scaledWidth scaledHeight $ \surface -> Cairo.renderWith surface $ do
-    Cairo.scale scaleFactor scaleFactor
-    cairoScope (setColor colorBackground2 >> Cairo.paint)
+main = withSurfaceAuto "out/cycloids.png" scaledWidth scaledHeight $ \surface -> C.renderWith surface $ do
+    C.scale scaleFactor scaleFactor
+    cairoScope (setColor colorBackground2 >> C.paint)
 
     let curve k t = hypotrochoid Trochoid
             { baseRadius = t+800
@@ -28,26 +27,26 @@ main = withSurfaceAuto "out/cycloids.png" scaledWidth scaledHeight $ \surface ->
 
     for_ [0..16] $ \k -> do
         cairoScope $ do
-            Cairo.scale 0.25 0.25
-            Cairo.rectangle 240 240 2080 2080
+            C.scale 0.25 0.25
+            rectangle 240 240 2080 2080
             cairoScope $ do
                 setColor colorForeground2
-                Cairo.setLineWidth 20
-                Cairo.strokePreserve
+                setLineWidth 20
+                strokePreserve
             cairoScope $ do
                 setColor colorBackground1
-                Cairo.fillPreserve
-            Cairo.clip
+                fillPreserve
+            clip
 
-            Cairo.translate (picWidth / 2) (picHeight / 2)
+            C.translate (picWidth / 2) (picHeight / 2)
             moveToVec (curve (fromIntegral k) 0)
             for_ [0, 0.2 .. 5700] $ lineToVec . curve (fromIntegral k)
 
             setColor colorForeground1
-            Cairo.stroke
+            stroke
 
-        Cairo.translate (picWidth/4) 0
-        when (k `mod` 4 == 3) $ Cairo.translate (-picWidth) (picHeight / 4)
+        C.translate (picWidth/4) 0
+        when (k `mod` 4 == 3) $ C.translate (-picWidth) (picHeight / 4)
 
   where
     scaledWidth = round (scaleFactor * picWidth)
@@ -63,17 +62,17 @@ data Trochoid = Trochoid
 epitrochoid :: Trochoid -> Double -> Vec2
 epitrochoid Trochoid{..} t = order0 -. pencilRadius *. order1
   where
-    order0 = polar (rad t) (Distance (baseRadius + rotorRadius))
-    order1 = polar (rad $ (baseRadius + rotorRadius) / rotorRadius * t) (Distance 1)
+    order0 = polar (rad t) (baseRadius + rotorRadius)
+    order1 = polar (rad $ (baseRadius + rotorRadius) / rotorRadius * t) 1
 
 hypotrochoid :: Trochoid -> Double -> Vec2
 hypotrochoid Trochoid{..} t =  order0 +. pencilRadius *. order1
   where
-    order0 = polar (rad t) (Distance $ baseRadius - rotorRadius)
-    order1 = polar (rad $ (rotorRadius - baseRadius) / rotorRadius * t) (Distance 1)
+    order0 = polar (rad t) (baseRadius - rotorRadius)
+    order1 = polar (rad $ (rotorRadius - baseRadius) / rotorRadius * t) 1
 
 colorBackground1, colorForeground1, colorBackground2, colorForeground2 :: Color Double
-colorBackground1 = parseRGBHex "#002b36"
-colorBackground2 = parseRGBHex "#073642"
-colorForeground1 = parseRGBHex "#586e75"
-colorForeground2 = parseRGBHex "#657b83"
+colorBackground1 = parseRgbHex "#002b36"
+colorBackground2 = parseRgbHex "#073642"
+colorForeground1 = parseRgbHex "#586e75"
+colorForeground2 = parseRgbHex "#657b83"
