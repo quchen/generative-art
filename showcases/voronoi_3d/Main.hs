b95a1a2f6a2a535b85dd1cb64903b3fc6ec8fc3a
Franz Thoma <franz.thoma@tngtech.com>
2022-02-16 20:14:32 +0100

    Refactor for easier demo

--- showcases/voronoi_3d/Main.hs
+++ showcases/voronoi_3d/Main.hs
@@ -29,36 +29,35 @@ scaleFactor = 1
 
 main :: IO ()
 main = do
-    let file = "out/voronoi_3d.svg"
+    let file = "out/voronoi_3d.png"
         count = 500
         scaledWidth = round (scaleFactor * picWidth)
         scaledHeight = round (scaleFactor * picHeight)
 
     gen <- initialize (V.fromList [12, 984, 498, 498, 626, 15, 165])
+    points <- poissonDiscDistribution count gen
+
+    let voronoi = toVoronoi (lloydRelaxation $ lloydRelaxation $ lloydRelaxation $ lloydRelaxation $ bowyerWatson extents points)
+        voronoiWithProps = mapWithSeed (\p () -> (randomColor p, randomHeight p)) voronoi
+        voronoiCells = sortOn (backToFront . region) $ cells voronoiWithProps
+
+    withSurfaceAuto file scaledWidth scaledHeight $ \surface -> renderWith surface $ do
+        cairoScope (setColor (magma 0.05) >> paint)
+        for_ voronoiCells drawColumn
+  where
+    extents = BoundingBox (Vec2 0 0) (Vec2 1440 1440)
+    backToFront (Polygon ps) = minimum (yCoordinate <$> ps)
+
+poissonDiscDistribution :: Int -> GenIO -> IO [Vec2]
+poissonDiscDistribution count gen = do
     let -- constructed so that we have roughly `count` points
-        adaptiveRadius = 1440 * sqrt (0.75 / count)
+        adaptiveRadius = 1440 * sqrt (0.75 / fromIntegral count)
         samplingProps = PoissonDisc { width = 1440, height = 1440, radius = adaptiveRadius, k = 4, ..}
 
     points <- poissonDisc samplingProps
     print (length points)
-    let voronoi = toVoronoi (lloydRelaxation $ lloydRelaxation $ lloydRelaxation $ lloydRelaxation $ bowyerWatson (BoundingBox (Vec2 0 0) (Vec2 1440 1440)) points)
-        voronoiWithProps = mapWithSeed (\p () -> (randomColor p, randomHeight p)) voronoi
-        origin = Vec2 (picWidth/2) (picHeight/2)
-        voronoiCells = sortOn ((\(Polygon ps) -> minimum (yCoordinate <$> ps)) . fst) $
-            ( \c ->
-                ( G.transform
-                    (  G.translate (Vec2 0 (picHeight/5))
-                    <> G.scaleAround' origin 1 0.35
-                    <> G.rotateAround origin (deg 45)
-                    <> G.translate (Vec2 560 0 )
-                    <> G.scaleAround (seed c) 0.9 )
-                    (region c)
-                , props c) )
-            <$> cells voronoiWithProps
+    pure points
 
-    withSurfaceAuto file scaledWidth scaledHeight $ \surface -> renderWith surface $ do
-        cairoScope (setColor (magma 0.05) >> paint)
-        for_ voronoiCells $ uncurry drawCell
 
 randomHeight :: Vec2 -> Double
 randomHeight = \p -> 300 + 400 * noise2d p + 200 * exp(- 0.000005 * normSquare (p -. origin))
@@ -73,9 +72,8 @@ randomColor = \p -> inferno (0.6 + 0.35 * noise2d p)
     noise = perlin { perlinOctaves = 5, perlinFrequency = 0.001, perlinPersistence = 0.65, perlinSeed = 1980166 }
     noise2d (Vec2 x y) = fromMaybe 0 $ getValue noise (x, y, 0)
 
-drawCell :: Polygon -> (Color Double, Double) -> Render ()
-drawCell (Polygon []) _ = pure ()
-drawCell poly@(Polygon ps) (color, height) = cairoScope $ do
+drawColumn :: VoronoiCell (Color Double, Double) -> Render ()
+drawColumn Cell{..} = cairoScope $ do
     let lineColor = blend 0.95 color white
         sideColor = blend 0.1 (color `withOpacity` 0.8) (black `withOpacity` 0.3)
         topColor = blend 0.7 (color `withOpacity` 0.8) (black `withOpacity` 0.3)
@@ -101,10 +99,20 @@ drawCell poly@(Polygon ps) (color, height) = cairoScope $ do
         setColor lineColor
         setLineWidth 2
         stroke
-
   where
+    poly@(Polygon ps) = G.transform (isometricPerspective <> G.scaleAround seed 0.9) region
+    (color, height) = props
     leftmost = minimumBy (comparing xCoordinate) ps
     normalizedPoints = rotateUntil (== leftmost) ps
+
+isometricPerspective :: Transformation
+isometricPerspective =
+       G.translate (Vec2 0 (picHeight/5))
+    <> G.scaleAround' origin 1 0.35
+    <> G.rotateAround origin (deg 45)
+    <> G.translate (Vec2 560 0)
+  where
+    origin = Vec2 (picWidth/2) (picHeight/2)
 
 rotateUntil :: (a -> Bool) -> [a] -> [a]
 rotateUntil p xs = zipWith
