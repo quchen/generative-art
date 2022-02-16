f414febc484307e0c94c07b4c42b7b57ba325b2d
Franz Thoma <franz.thoma@tngtech.com>
2022-02-16 20:14:32 +0100

    Demo for MuniHac workshop

--- showcases/voronoi_3d/Main.hs
+++ showcases/voronoi_3d/Main.hs
@@ -1,21 +1,23 @@
-module Main (main) where
+{-# LANGUAGE RecordWildCards #-}
+module Main (main, main0, main1, main2, main3) where
 
 
 
-import           Data.List
-import           Data.Maybe
-import           Data.Ord
-import qualified Data.Vector              as V
-import           Graphics.Rendering.Cairo as C hiding (height, width, x, y)
-import           Math.Noise               (Perlin (..), getValue, perlin)
-import           Prelude                  hiding ((**))
-import           System.Random.MWC
+import Data.List ( sortOn )
+import Data.Maybe ( fromMaybe )
+import Data.Ord ( comparing )
+import qualified Data.Vector as V
+import Graphics.Rendering.Cairo as C
+import Math.Noise (Perlin (..), getValue, perlin)
+import System.Random.MWC ( initialize, uniformRM )
 
 import Draw
-import Geometry                     as G
+import Geometry as G
 import Geometry.Algorithms.Delaunay
 import Geometry.Algorithms.Sampling
 import Geometry.Algorithms.Voronoi
+import Control.Monad (replicateM)
+import Control.Applicative (Applicative(liftA2))
 
 
 
@@ -23,45 +25,129 @@ picWidth, picHeight :: Num a => a
 picWidth = 2560
 picHeight = 1440
 
-scaleFactor :: Double
-scaleFactor = 1
+file :: FilePath
+file = "out/voronoi_3d.png"
 
 main :: IO ()
-main = do
-    let file = "out/voronoi_3d.svg"
-        count = 500
-        scaledWidth = round (scaleFactor * picWidth)
-        scaledHeight = round (scaleFactor * picHeight)
+main = main3
 
+main0 :: IO ()
+main0 = do
+    points <- uniformDistribution 500
+    render file 1440 1440 $ do
+        cairoScope (setColor white >> paint)
+        for_ points $ \point -> cairoScope $ do
+            sketch (Circle point 5)
+            setColor black
+            fill
+
+uniformDistribution :: Int -> IO [Vec2]
+uniformDistribution count = do
+    gen <- initialize (V.fromList [12, 984, 498, 498, 626, 15, 165])
+    replicateM count $ liftA2 Vec2
+        (uniformRM (0, 1440) gen)
+        (uniformRM (0, 1440) gen)
+
+--------------------------------------------------------------------------------
+
+main1 :: IO ()
+main1 = do
+    points <- uniformDistribution 500
+
+    let voronoi = toVoronoi (bowyerWatson extents points)
+        voronoiCells = _voronoiCells voronoi
+
+    render file 1440 1440 $ do
+        cairoScope (setColor white >> paint)
+        for_ voronoiCells $ \cell -> cairoScope $ do
+            sketch (Circle (_voronoiSeed cell) 5)
+            setColor black
+            fill
+        for_ voronoiCells drawCell
+  where
+    extents = BoundingBox (Vec2 0 0) (Vec2 1440 1440)
+
+poissonDiscDistribution :: Int -> IO [Vec2]
+poissonDiscDistribution count = do
     gen <- initialize (V.fromList [12, 984, 498, 498, 626, 15, 165])
     let -- constructed so that we have roughly `count` points
-        adaptiveRadius = 1440 * sqrt (0.75 / count)
+        adaptiveRadius = 1440 * sqrt (0.75 / fromIntegral count)
         samplingProps = PoissonDiscParams
-            { _poissonShape = boundingBox [zero, Vec2 1440 1440]
+            { _poissonShape = boundingBox [Vec2 0 0, Vec2 1440 1440]
             , _poissonRadius = adaptiveRadius
-            , _poissonK      = 4
+            , _poissonK = 4
             }
 
     points <- poissonDisc gen samplingProps
     print (length points)
-    let voronoi = toVoronoi (lloydRelaxation 4 (bowyerWatson (BoundingBox (Vec2 0 0) (Vec2 1440 1440)) points))
-        voronoiWithProps = mapWithMetadata (\p _ _ -> (randomColor p, randomHeight p)) voronoi
-        origin = Vec2 (picWidth/2) (picHeight/2)
-        voronoiCells = sortOn ((\(Polygon ps) -> minimum (yCoordinate <$> ps)) . fst) $
-            ( \c ->
-                ( G.transform
-                    (  G.translate (Vec2 0 (picHeight/5))
-                    <> G.scaleAround' origin 1 0.35
-                    <> G.rotateAround origin (deg 45)
-                    <> G.translate (Vec2 560 0 )
-                    <> G.scaleAround (_voronoiSeed c) 0.9 )
-                    (_voronoiRegion c)
-                , _voronoiProps c) )
-            <$> _voronoiCells voronoiWithProps
+    pure points
 
-    render file scaledWidth scaledHeight $ do
+drawCell :: VoronoiCell a -> Render ()
+drawCell VoronoiCell{..} = cairoScope $ do
+    C.setLineJoin C.LineJoinBevel
+    sketch poly
+    setColor black
+    setLineWidth 2
+    stroke
+  where
+    poly = _voronoiRegion
+
+--------------------------------------------------------------------------------
+
+main2 :: IO ()
+main2 = do
+    points <- poissonDiscDistribution 500
+
+    let voronoi = toVoronoi (lloydRelaxation 4 $ bowyerWatson extents points)
+        voronoiWithProps = mapWithMetadata (\p _ _ -> randomColor p) voronoi
+        voronoiCells = _voronoiCells voronoiWithProps
+
+    render file 1440 1440 $ do
         cairoScope (setColor (magma 0.05) >> paint)
-        for_ voronoiCells $ uncurry drawCell
+        for_ voronoiCells drawCellColor
+  where
+    extents = BoundingBox (Vec2 0 0) (Vec2 1440 1440)
+
+randomColor :: Vec2 -> Color Double
+randomColor = \p -> inferno (0.6 + 0.35 * noise2d p)
+  where
+    noise = perlin { perlinOctaves = 5, perlinFrequency = 0.001, perlinPersistence = 0.65, perlinSeed = 1980166 }
+    noise2d (Vec2 x y) = fromMaybe 0 $ getValue noise (x, y, 0)
+
+drawCellColor :: VoronoiCell (Color Double) -> Render ()
+drawCellColor VoronoiCell{..} = cairoScope $ do
+    let lineColor = blend 0.95 color white
+        cellColor = blend 0.60 color black
+
+    C.setLineJoin C.LineJoinBevel
+
+    sketch poly
+    setColor cellColor
+    fillPreserve
+    setColor lineColor
+    setLineWidth 2
+    stroke
+
+  where
+    poly = _voronoiRegion
+    color = _voronoiProps
+
+--------------------------------------------------------------------------------
+
+main3 :: IO ()
+main3 = do
+    points <- poissonDiscDistribution 500
+
+    let voronoi = toVoronoi (lloydRelaxation 4 $ bowyerWatson extents points)
+        voronoiWithProps = mapWithMetadata (\p _ _ -> (randomColor p, randomHeight p)) voronoi
+        voronoiCells = sortOn (backToFront . _voronoiRegion) $ _voronoiCells voronoiWithProps
+
+    render file 2560 1440 $ do
+        cairoScope (setColor (magma 0.05) >> paint)
+        for_ voronoiCells drawColumn
+  where
+    extents = BoundingBox (Vec2 0 0) (Vec2 1440 1440)
+    backToFront (Polygon ps) = minimum (yCoordinate <$> ps)
 
 randomHeight :: Vec2 -> Double
 randomHeight = \p -> 300 + 400 * noise2d p + 200 * exp(- 0.000005 * normSquare (p -. origin))
@@ -70,18 +156,11 @@ randomHeight = \p -> 300 + 400 * noise2d p + 200 * exp(- 0.000005 * normSquare (
     noise2d (Vec2 x y) = fromMaybe 0 $ getValue noise (x, y, 0)
     origin = Vec2 720 720
 
-randomColor :: Vec2 -> Color Double
-randomColor = \p -> inferno (0.6 + 0.35 * noise2d p)
-  where
-    noise = perlin { perlinOctaves = 5, perlinFrequency = 0.001, perlinPersistence = 0.65, perlinSeed = 1980166 }
-    noise2d (Vec2 x y) = fromMaybe 0 $ getValue noise (x, y, 0)
-
-drawCell :: Polygon -> (Color Double, Double) -> Render ()
-drawCell (Polygon []) _ = pure ()
-drawCell poly@(Polygon ps) (color, height) = cairoScope $ do
+drawColumn :: VoronoiCell (Color Double, Double) -> Render ()
+drawColumn VoronoiCell{..} = cairoScope $ do
     let lineColor = blend 0.95 color white
         sideColor = blend 0.1 (color `withOpacity` 0.8) (black `withOpacity` 0.3)
-        topColor = blend 0.7 (color `withOpacity` 0.8) (black `withOpacity` 0.3)
+        cellColor = blend 0.7 (color `withOpacity` 0.8) (black `withOpacity` 0.3)
 
     C.setLineJoin C.LineJoinBevel
 
@@ -99,15 +178,25 @@ drawCell poly@(Polygon ps) (color, height) = cairoScope $ do
     cairoScope $ do
         C.translate 0 (-height)
         sketch poly
-        setColor topColor
+        setColor cellColor
         fillPreserve
         setColor lineColor
         setLineWidth 2
         stroke
-
   where
+    poly@(Polygon ps) = G.transform (isometricPerspective <> G.scaleAround _voronoiSeed 0.9) _voronoiRegion
+    (color, height) = _voronoiProps
     leftmost = minimumBy (comparing xCoordinate) ps
     normalizedPoints = rotateUntil (== leftmost) ps
+
+isometricPerspective :: Transformation
+isometricPerspective =
+       G.translate (Vec2 0 (picHeight/5))
+    <> G.scaleAround' origin 1 0.35
+    <> G.rotateAround origin (deg 45)
+    <> G.translate (Vec2 560 0)
+  where
+    origin = Vec2 (picWidth/2) (picHeight/2)
 
 rotateUntil :: (a -> Bool) -> [a] -> [a]
 rotateUntil p xs = zipWith
