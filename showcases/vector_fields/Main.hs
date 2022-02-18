edd2cd7dd9067f8f01f4117b8e96c0643afa698d
David Luposchainsky <dluposchainsky@gmail.com>
2022-02-18 22:24:20 +0100

    Rework vector fields

--- showcases/vector_fields/Main.hs
+++ showcases/vector_fields/Main.hs
@@ -2,83 +2,85 @@ module Main (main) where
 
 
 
-import           Data.Maybe
-import qualified Data.Vector              as V
-import qualified Graphics.Rendering.Cairo as C
-import           Math.Noise               (Perlin (..), getValue, perlin)
+import           Control.Monad
+import qualified Graphics.Rendering.Cairo        as C
 import           System.Random.MWC
+import           System.Random.MWC.Distributions
 
 import           Draw
-import           Geometry                      as G
-import           Geometry.Algorithms.Sampling
-import qualified Geometry.Processes.FlowField  as ODE
+import           Geometry                        as G
+import           Geometry.Algorithms.PerlinNoise
+import qualified Geometry.Processes.FlowField    as ODE
 import           Graphics.Rendering.Cairo
 import           Numerics.DifferentialEquation
+import           Numerics.Functions
 import           Numerics.VectorAnalysis
 
 
 
 picWidth, picHeight :: Num a => a
-picWidth = 2560
-picHeight = 1440
+picWidth = 600
+picHeight = 400
 
+-- Higher values yield lower-frequency noise
 noiseScale :: Double
-noiseScale = 251
+noiseScale = 0.5 * min picWidth picHeight
 
 noiseSeed :: Int
 noiseSeed = 519496
 
 main :: IO ()
-main = withSurfaceAuto "out/vector_fields.svg" scaledWidth scaledHeight $ \surface -> C.renderWith surface $ do
-    C.scale scaleFactor scaleFactor
+main = withSurfaceAuto "out/vector_fields.png" picWidth picHeight $ \surface -> C.renderWith surface $ do
     cairoScope $ do
         setColor white
         C.paint
 
     gen <- liftIO create
-    startPoints <- liftIO $ G.transform (G.translate (Vec2 (-500) 0)) <$> poissonDisc PoissonDisc
-        { width = picWidth + 500
-        , height = picHeight
-        , radius = 18
-        , k = 4
-        , gen = gen }
+    let startPoints = [Vec2 0 y | y <- [-50..picHeight+50]]
 
     cairoScope $ for_ startPoints $ \startPoint -> do
-        thickness <- liftIO (uniformR (0.5, 1.5) gen)
-        let trajectory = takeWhile (\(t, _) -> t <= 200) (fieldLine compositeField startPoint)
-        drawFieldLine thickness trajectory
-  where
-    scaleFactor = 0.39
-    scaledWidth = round (picWidth * scaleFactor)
-    scaledHeight = round (picHeight * scaleFactor)
+        thickness <- liftIO (uniformR (0.1, 1.0) gen)
+        setLineWidth thickness
+        colorValue <- liftIO $ do
+            let Vec2 _ y = startPoint
+            normal (y/picHeight) 0.3 gen
+        setColor (rocket colorValue)
+        let trajectory =
+                takeWhile
+                    (\(t, pos) -> t <= 1600 && pos `insideBoundingBox` (Vec2 (-50) (-50), Vec2 (picWidth+50) (picHeight+50)))
+                    (fieldLine velocityField startPoint)
+        drawFieldLine trajectory
 
-drawFieldLine :: Double -> [(Double, Vec2)] -> Render ()
-drawFieldLine _ [] = pure ()
-drawFieldLine thickness ps = cairoScope $ do
-    let ps' = V.toList (bezierSmoothen (V.fromList (snd <$> ps)))
-        time = fst <$> ps
-    for_ (zip time ps') $ \(t, p) -> do
-        setLineCap LineCapRound
-        setLineWidth (thickness * (t/100))
-        bezierSegmentSketch p
+drawFieldLine :: [(Double, Vec2)] -> Render ()
+drawFieldLine ps = cairoScope $ do
+    let polyLine = map snd ps
+        simplified = simplifyTrajectoryRadial 3 polyLine
+    when (not (null (drop 2 simplified))) $ do
+        bezierSketch (bezierSmoothen simplified)
         stroke
 
-scalarField :: Vec2 -> Double
-scalarField = noise2d . G.transform (G.scale' 1 2)
+-- 2D vector potential, which in 2D is umm well a scalar potential.
+vectorPotential :: Vec2 -> Double
+vectorPotential p = noiseScale *. perlinNoise params p
   where
-    noise = perlin { perlinFrequency = 1/noiseScale, perlinOctaves = 1, perlinSeed = noiseSeed }
-    noise2d (Vec2 x y) = fromMaybe 0 $ getValue noise (x, y, 0)
-
-gradientField :: Vec2 -> Vec2
-gradientField p = noiseScale *. grad scalarField p
+    params = PerlinParameters
+        { _perlinFrequency   = 3/noiseScale
+        , _perlinLacunarity  = 2
+        , _perlinOctaves     = 1
+        , _perlinPersistence = 0.5
+        , _perlinSeed        = noiseSeed
+        }
 
 rotationField :: Vec2 -> Vec2
-rotationField = G.transform (G.rotate (deg 90)) . gradientField
+rotationField = curlZ vectorPotential
 
-compositeField :: Vec2 -> Vec2
-compositeField p@(Vec2 x y) = Vec2 1 0 +. 0.8 * perturbationStrength *. rotationField p
+velocityField :: Vec2 -> Vec2
+velocityField p@(Vec2 x y) = Vec2 1 0 +. perturbationStrength *. rotationField p
   where
-    perturbationStrength = 0.7 * (1 + tanh (4 * (x / picWidth - 0.6))) * exp (-3 * (y / picHeight - 0.5)^2)
+    perturbationStrength =
+        1.4
+        * logisticRamp (0.6*picWidth) (picWidth/6) x
+        * gaussianFalloff (0.5*picHeight) (0.4*picHeight) y
 
 fieldLine
     :: (Vec2 -> Vec2)
@@ -89,5 +91,5 @@ fieldLine f p0 = rungeKuttaAdaptiveStep (ODE.fieldLine f) p0 t0 dt0 tolNorm tol
     t0 = 0
     dt0 = 1
     -- Decrease exponent for more accurate results
-    tol = 1e-1
+    tol = 1e-2
     tolNorm = norm
