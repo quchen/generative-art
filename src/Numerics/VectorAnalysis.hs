dad0c7507603b88b28c90d1825106d6bf6a66c27
David Luposchainsky <dluposchainsky@gmail.com>
2022-02-18 22:24:20 +0100

    Add curlZ function

--- src/Numerics/VectorAnalysis.hs
+++ src/Numerics/VectorAnalysis.hs
@@ -4,12 +4,14 @@ module Numerics.VectorAnalysis (
       grad
     , divergence
     , curl
+    , curlZ
     , laplace
 
     -- * Operators with configurable step width
     , gradH
     , divH
     , curlH
+    , curlZH
     , laplaceH
 ) where
 
@@ -19,65 +21,46 @@ import Geometry.Core
 standardH :: Double
 standardH = 1e-3
 
--- | Gradient with a predefined sampling distance.
+-- | Gradient with predefined sampling distance.
 --
 -- \[
--- \begin{align}
--- \mathrm{grad} :\hspace{0.3em}
---     &\left(\mathbb R^2 \rightarrow \mathbb R\right)
---            \rightarrow \left(\mathbb R^2 \rightarrow \mathbb R^2\right) \\
---     &f \mapsto \begin{pmatrix}
---             \partial_x f \\
---             \partial_y f
---         \end{pmatrix}
--- \end{align}
+-- \text{grad}(f) = \begin{pmatrix}
+--         \partial_x f \\
+--         \partial_y f
+--     \end{pmatrix}
 -- \]
 --
 -- The fire-and-forget version of 'gradH'.
 grad :: (Vec2 -> Double) -> Vec2 -> Vec2
 grad = gradH standardH
 
--- | Divergence with a predefined sampling distance. Named to avoid clashing with
+-- | Divergence with predefined sampling distance. Named to avoid clashing with
 -- integer 'div'ision.
 --
 -- \[
--- \begin{align}
--- \mathrm{div} :\hspace{0.3em}
---     &\left(\mathbb R^2 \rightarrow \mathbb R^2\right)
---            \rightarrow \left(\mathbb R^2 \rightarrow \mathbb R\right) \\
---     &f \mapsto \partial_xf_x + \partial_y f_y
--- \end{align}
+-- \text{div}(f) = \partial_xf_x + \partial_y f_y
 -- \]
 --
 -- The fire-and-forget version of 'divH'.
 divergence :: (Vec2 -> Vec2) -> Vec2 -> Double
 divergence = divH standardH
 
--- | Curl with a predefined sampling distance. Note that in two dimensions, the
--- curl is simply a number (or, more accurately, a 1-form).
+-- | Curl with predefined sampling distance. Note that in two dimensions, the curl
+-- is simply a number. For a different 2D adaptation of the \(\text{curl}\)
+-- operator, see 'curlZ'.
 --
 -- \[
--- \begin{align}
--- \mathrm{curl} :\hspace{0.3em}
---     &\left(\mathbb R^2 \rightarrow \mathbb R^2\right)
---            \rightarrow \left(\mathbb R^2 \rightarrow \mathbb R\right) \\
---     &f \mapsto \partial_x f_y - \partial_y f_x
--- \end{align}
+-- \text{curl}(f) = \partial_x f_y - \partial_y f_x
 -- \]
 --
 -- The fire-and-forget version of 'curlH'.
 curl :: (Vec2 -> Vec2) -> Vec2 -> Double
 curl = curlH standardH
 
--- | Laplacian with a predefined sampling distance.
+-- | Laplacian with predefined sampling distance.
 --
 -- \[
--- \begin{align}
--- \mathrm{\nabla^2} :\hspace{0.3em}
---     &\left(\mathbb R^2 \rightarrow \mathbb R\right)
---            \rightarrow \left(\mathbb R^2 \rightarrow \mathbb R\right) \\
---     &f \mapsto \partial^2_x f + \partial^2_y f
--- \end{align}
+-- \nabla^2f = \partial^2_x f + \partial^2_y f
 -- \]
 --
 -- The fire-and-forget version of 'laplaceH'.
@@ -105,8 +88,6 @@ divH h f = \v@(Vec2 x y) ->
     in (dx+dy)/h
 
 -- | Curl with customizable sampling distance. The configurable version of 'curl'.
--- Note that in two dimensions, the curl is simply a number (or, more accurately, a
--- 1-form).
 curlH
     :: Double -- ^ \(h\) as in \(\frac{f(x+h)-f(x)}h\)
     -> (Vec2 -> Vec2)
@@ -123,3 +104,36 @@ laplaceH
     -> (Vec2 -> Double)
     -> (Vec2 -> Double)
 laplaceH h = divH h . gradH h
+
+-- | Curl of a purely-z-component 3D vector field, which is another common way
+-- (than 'curl') to implement a two-dimensional version of \(\text{curl}\):
+--
+-- \[
+-- \text{curl}_z(\psi)
+--     = \left[\text{curl}_{3D} \begin{pmatrix}0\\0\\\psi\end{pmatrix}\right]_{\text{2D part}}
+--     = \begin{pmatrix}\partial_y\psi\\-\partial_x\psi\end{pmatrix}
+-- \]
+--
+-- Because of this, the result is always divergence-free, because \(\forall f. \text{div}(\text{curl}(f))=0\).
+--
+-- Using 'curlZ' to create 'divergence'-free flow fields is similar to using 'grad'
+-- to create 'curl'-free force fields. The argument for 'curlZ' is known as the
+-- vector or stream potential in fluid dynamics, with the resulting vector field
+-- being known as a
+-- [stream function](https://en.wikipedia.org/wiki/Stream_function).
+--
+-- The fire-and-forget version of 'curlZH'.
+curlZ :: (Vec2 -> Double) -> Vec2 -> Vec2
+curlZ f = curlZH standardH f
+
+-- | Curl of the z component of a 3D vector field, with customizable sampling
+-- distance. The configurable version of 'curlZ'.
+curlZH
+    :: Double -- ^ \(h\) as in \(\frac{f(x+h)-f(x)}h\)
+    -> (Vec2 -> Double)
+    -> Vec2
+    -> Vec2
+curlZH h f = \x ->
+    let f_x = f x
+    in Vec2 (f (x +. Vec2 0 h) - f_x) (- f (x +. Vec2 h 0) + f_x) /. h
+        -- NB: this is just 90° rotated grad
