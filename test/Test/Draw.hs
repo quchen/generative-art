e1cc94a2b8461837b97776428bf140165e931b3b
David Luposchainsky <dluposchainsky@gmail.com>
2022-02-22 17:55:17 +0100

    Add visual ellipse test

--- test/Test/Draw.hs
+++ test/Test/Draw.hs
@@ -2,7 +2,12 @@ module Test.Draw (tests) where
 
 
 
+import Data.Foldable
+import Graphics.Rendering.Cairo as C
+
 import Draw
+import Geometry               as G
+import Numerics.Interpolation
 
 import Test.TastyAll
 
@@ -12,4 +17,91 @@ tests :: TestTree
 tests = testGroup "Draw"
     [ testProperty "fromCairoMatrix/toCairoMatrix are inverses" $ \trafo ->
         (fromCairoMatrix . toCairoMatrix) trafo ~=== trafo
+    , testGroup "Ellipses"
+        [ scaleEllipseTest
+        ]
     ]
+
+scaleEllipseTest :: TestTree
+scaleEllipseTest = testVisual "Scale" 300 300 "docs/geometry/ellipses" $ \(w,h) -> do
+    let center = zero
+        radius = w/6*0.9
+        ellipse = toEllipse (Circle center radius)
+
+        grid i j = C.translate (i*w/3 + w/6) (j*h/3 + w/6)
+
+    setLineWidth 1
+
+    let actions =
+            [ for_ (take 10 [0..]) $ \i -> cairoScope $ do
+                let scaleFactor = linearInterpolateID (0,9) (0.1, 1) i
+                grid 0 0
+                sketch (G.transform (G.scaleAround center scaleFactor) ellipse)
+                stroke
+
+            , do
+                grid 1 0
+                for_ (take 10 [0..]) $ \i -> cairoScope $ do
+                    let scaleFactor = linearInterpolateID (0,9) (0.1, 1) i
+                    sketch (G.transform (G.scaleAround' center scaleFactor 1) ellipse)
+                    stroke
+
+            , do
+                grid 2 0
+                for_ [0..9] $ \i -> cairoScope $ do
+                    let scaleFactor1 = linearInterpolate (0, 9) (1, 0.1) (fromIntegral i)
+                        scaleFactor2 = linearInterpolate (0, 9) (0.1, 1) (fromIntegral i)
+                    sketch (G.transform (G.scaleAround' center scaleFactor1 scaleFactor2) ellipse)
+                    stroke
+
+            , do
+                grid 0 1
+                for_ (take 10 [0..]) $ \i -> do
+                    let scaleX = scaleAround' center (linearInterpolateID (0,9) (0.5,1) (fromIntegral i)) 1
+                        scaleY = scaleAround' center 1 (linearInterpolateID (0,9) (0.1,1) (fromIntegral i))
+                    sketch (G.transform (scaleX <> scaleY) ellipse)
+                    stroke
+
+            , do
+                grid 1 1
+                for_ (take 10 [0..]) $ \i -> cairoScope $ do
+                    let angle = deg (linearInterpolateID (0,9) (0, 90) i)
+                    sketch (G.transform (G.rotateAround center angle <> scaleAround' center 1 0.5) ellipse)
+                    stroke
+
+            , do
+                grid 2 1
+                for_ (take 19 [0..]) $ \i -> cairoScope $ do
+                    let angle = deg (linearInterpolateID (0,19) (0, 180) i)
+                    sketch (G.transform (G.rotateAround center angle <> scaleAround' center 1 0.5) ellipse)
+                    stroke
+
+            , do
+                grid 0 2
+                for_ (take 9 [0..]) $ \i -> cairoScope $ do
+                    let scaleFactor = linearInterpolateID (0,9) (1,0.1) i
+                        angle = deg (linearInterpolateID (0,9) (90,0) i)
+                    sketch (G.transform (G.rotateAround center angle <> G.scaleAround' center 1 scaleFactor) ellipse)
+                    stroke
+
+            , do
+                grid 1 2
+                for_ (take 9 [0..]) $ \i -> do
+                    let scaleFactor = linearInterpolateID (0,9) (1,0.1) i
+                        angle = deg (linearInterpolateID (0,9) (0,90) i)
+                    sketch (G.transform (scaleAround center scaleFactor <> G.rotateAround center angle <> scaleAround' center 1 scaleFactor) ellipse)
+                    stroke
+
+            , do
+                grid 2 2
+                for_ (take 9 [0..]) $ \i -> do
+                    let BoundingBox topLeft _ = boundingBox ellipse
+                        scaleFactor = linearInterpolateID (0,9) (1,0.1) i
+                        angle = deg (linearInterpolateID (0,9) (0,90) i)
+                    sketch (G.transform (rotateAround center angle <> scaleAround (0.5 *. (topLeft -. center)) scaleFactor) ellipse)
+                    stroke
+            ]
+
+    for_ (zip [0..] actions) $ \(i, action) -> do
+        setColor (mathematica97 i)
+        cairoScope action
