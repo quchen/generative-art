4f2f88e9b63be29f9eb63fa9827d235be72049f2
Franz Thoma <franz.thoma@tngtech.com>
2022-03-08 21:34:12 +0100

    Reset to starting point

--- app/bob_2022_workshop/Main.hs
+++ app/bob_2022_workshop/Main.hs
@@ -1,22 +1,22 @@
 module Main (main) where
 
-import Draw
+import qualified Data.Vector as V
 import qualified Graphics.Rendering.Cairo as Cairo
-import Steps.A.HelloWorld
-import Steps.B.Squares
-import Steps.C.FlowFields
-import Steps.D.Shatter
+import System.Random.MWC
+
+import Geometry
+import Draw
 
 -- | Draw a 'Render' to PNG and SVG files.
 drawToFiles
     :: FilePath -- ^ Output location, will store .svg and .png versions
     -> Int -- ^ Width
     -> Int -- ^ Height
-    -> (Int -> Int -> Cairo.Render a) -- ^ Picture, parameterized by width/height
+    -> Cairo.Render a -- ^ Picture, parameterized by width/height
     -> IO ()
 drawToFiles filename w h drawing = do
-    _ <- withSurface PNG (filename ++ ".png") w h (\surface -> Cairo.renderWith surface (drawing w h))
-    _ <- withSurface SVG (filename ++ ".svg") w h (\surface -> Cairo.renderWith surface (drawing w h))
+    _ <- withSurface PNG (filename ++ ".png") w h (\surface -> Cairo.renderWith surface drawing)
+    _ <- withSurface SVG (filename ++ ".svg") w h (\surface -> Cairo.renderWith surface drawing)
     pure ()
 
 -- | You can generate the output files using either:
@@ -27,16 +27,6 @@ drawToFiles filename w h drawing = do
 -- `ghcid --command='stack ghci --main-is generative-art:bob-2022-workshop' --test=main -W`
 -- (fast compilation, slower execution time, requires `ghcid` installation)
 main :: IO ()
-main = do
-    drawToFiles "bob2022/a_helloworld" 300 110 Steps.A.HelloWorld.hello
-
-    drawToFiles "bob2022/b_squares" 400 400 Steps.B.Squares.squares
-
-    drawToFiles "bob2022/c1_simpleVectorField"        400 400 Steps.C.FlowFields.drawSimpleVectorField
-    drawToFiles "bob2022/c2_fieldLines"               400 400 Steps.C.FlowFields.drawFieldLines
-    drawToFiles "bob2022/c3_fieldLinesWithRandomness" 400 400 Steps.C.FlowFields.drawFieldLinesWithRandomness
-
-    drawToFiles "bob2022/d1_square"           400 400 Steps.D.Shatter.drawSquare
-    drawToFiles "bob2022/d2_square_cut"       400 400 Steps.D.Shatter.drawSquareCut
-    drawToFiles "bob2022/d3_square_shards"    400 400 Steps.D.Shatter.drawSquareShards
-    drawToFiles "bob2022/d4_square_shattered" 400 400 Steps.D.Shatter.drawSquareShattered
+main = drawToFiles "bob2022/shatter" 1000 1000 $ do
+    setColor white
+    Cairo.paint
