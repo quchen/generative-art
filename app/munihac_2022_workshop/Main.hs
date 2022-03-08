e69cbb41cc88b096698d3e81ee519b430cf200f6
Franz Thoma <franz.thoma@tngtech.com>
2022-03-08 22:12:43 +0100

    Revert "Full implementation of minimal example"
    This reverts commit 828532c61891f1f57db61ef4add7b30b7f6abc92.

--- app/munihac_2022_workshop/Main.hs
+++ app/munihac_2022_workshop/Main.hs
@@ -29,37 +29,3 @@ main :: IO ()
 main = drawToFiles "munihac2022/shatter" 1000 1000 $ do
     setColor white
     Cairo.paint
-    Cairo.setLineWidth 5
-
-    let rectangle = Polygon
-            [ Vec2 100 100
-            , Vec2 100 900
-            , Vec2 900 900
-            , Vec2 900 100
-            ]
-    gen <- Cairo.liftIO $ initialize (V.fromList [1])
-    shards <- shatter 10 gen [rectangle]
-
-    for_ shards $ \shard -> do
-        angle <- Cairo.liftIO $ deg <$> uniformRM (-10, 10) gen
-        displacement <- Cairo.liftIO $ uniformRM (Vec2 (-20) (-20), Vec2 20 20) gen
-        sketch (transform (rotateAround (polygonCentroid shard) angle <> translate displacement) shard)
-        colorCoord <- Cairo.liftIO $ uniformRM (0, 100) gen
-        let color = mathematica97 colorCoord
-        setColor (color `withOpacity` 0.5)
-        Cairo.fillPreserve
-        setColor color
-        Cairo.stroke
-
-randomCut :: GenIO -> Polygon -> Cairo.Render [Polygon]
-randomCut gen poly = Cairo.liftIO $ do
-    let BoundingBox pMin pMax = boundingBox poly
-    p <- uniformRM (pMin, pMax) gen
-    angle <- uniformM gen
-    pure (cutPolygon (angledLine p angle 1) poly)
-
-shatter :: Int -> GenIO -> [Polygon] -> Cairo.Render [Polygon]
-shatter 0 _ polys = pure polys
-shatter n gen polys = do
-    shards <- traverse (randomCut gen) polys
-    shatter (n-1) gen (concat shards)
