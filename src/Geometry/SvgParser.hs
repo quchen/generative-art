2e419ff350e6d72d70bec3d41b2e57f09b314024
David Luposchainsky <dluposchainsky@gmail.com>
2022-03-22 22:44:01 +0100

    [SVG parser] Make all circles (annoying) ellipses (also annoying, but at least transformable)

--- src/Geometry/SvgParser.hs
+++ src/Geometry/SvgParser.hs
@@ -16,22 +16,25 @@ import qualified Geometry.SvgParser.SimpleShapes as SimpleShapes
 
 data SvgElement
     = SvgLine Line
-    | SvgCircle Circle
     | SvgEllipse Ellipse
     | SvgPath [[Either Line Bezier]]
 
 instance HasBoundingBox SvgElement where
     boundingBox (SvgLine x) = boundingBox x
-    boundingBox (SvgCircle x) = boundingBox x
     boundingBox (SvgEllipse x) = boundingBox x
     boundingBox (SvgPath x) = boundingBox x
+
+instance Transform SvgElement where
+    transform t (SvgLine x) = SvgLine (transform t x)
+    transform t (SvgEllipse x) = SvgEllipse (transform t x)
+    transform t (SvgPath x) = SvgPath (transform t x)
 
 parse :: Text -> Either Text SvgElement
 parse input = case PathParser.parse input of
     Right path -> Right (SvgPath path)
     Left pathErr -> case SimpleShapes.parse input of
         Right (SimpleShapes.SvgLine line) -> Right (SvgLine line)
-        Right (SimpleShapes.SvgCircle circle) -> Right (SvgCircle circle)
+        Right (SimpleShapes.SvgCircle circle) -> Right (SvgEllipse (toEllipse circle))
         Right (SimpleShapes.SvgEllipse ellipse) -> Right (SvgEllipse ellipse)
         Left shapeErr -> Left (errorMsg input pathErr shapeErr)
 
