230a2133008dfce0b5c9c04a12e612eeb4ebab59
David Luposchainsky <dluposchainsky@gmail.com>
2022-03-23 22:40:44 +0100

    [Showcase: gasket] Scale gasket to A4 paper

--- penplotting/ApollonianGasket/Main.hs
+++ penplotting/ApollonianGasket/Main.hs
@@ -2,6 +2,8 @@ module Main (main) where
 
 
 
+import           Data.Coerce
+import           Data.Default.Class
 import           Data.Foldable
 import qualified Data.Text.Lazy.IO                   as T
 import           Data.Tree
@@ -11,16 +13,30 @@ import           Geometry.Processes.ApollonianGasket
 
 
 
-scaleFactor :: Double
-scaleFactor = 0.5
+minRadius :: Double
+minRadius = 0.5
 
 gen0L, gen0R, gen0B :: Circle
-gen0L = Circle (Vec2 (100*scaleFactor) (100*scaleFactor)) (50*scaleFactor)
-gen0R = Circle (Vec2 (200*scaleFactor) (100*scaleFactor)) (50*scaleFactor)
-gen0B = Circle (G.transform (G.rotateAround (Vec2 (100*scaleFactor) (100*scaleFactor)) (deg 60)) (Vec2 (200*scaleFactor) (100*scaleFactor))) (50*scaleFactor)
+gen0L = Circle (Vec2 100 100) 50
+gen0R = Circle (Vec2 200 100) 50
+gen0B = Circle (G.transform (G.rotateAround (Vec2 100 100) (deg 60)) (Vec2 200 100)) 50
+
+unsafelyTransform :: Transformation -> Tree Circle -> Tree Circle
+unsafelyTransform t = coerce . (fmap (transform t) :: Tree UnsafeTransformCircle -> Tree UnsafeTransformCircle) . coerce
 
 gasket :: Tree Circle
-gasket = createGasket 1 gen0L gen0R gen0B
+gasket = createGasket minRadius gen0L gen0R gen0B
+
+pageWidth, pageHeight, margin :: Double
+pageWidth = 210
+pageHeight = 291
+margin = 10
+
+gasketScaled :: Tree Circle
+gasketScaled = unsafelyTransform (G.transformBoundingBox (foldMap boundingBox gasket) (Vec2 margin margin, Vec2 pageWidth pageHeight -. Vec2 margin margin) def) gasket
+
+plotterSettings :: PlottingSettings
+plotterSettings = PlottingSettings (Just (foldMap boundingBox gasketScaled)) (Just 1000)
 
 main :: IO ()
-main = T.putStrLn (renderGCode (toGCode (toList gasket)))
+main = T.putStrLn (renderGCode (addHeaderFooter plotterSettings (toGCode (toList gasketScaled))))
