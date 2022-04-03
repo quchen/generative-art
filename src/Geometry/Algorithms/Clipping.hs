4063bbd323befe09245cc2e250b6b10753cae46a
David Luposchainsky <dluposchainsky@gmail.com>
2022-04-03 12:16:44 +0200

    Refactor hatching algorihtm

--- src/Geometry/Algorithms/Clipping.hs
+++ src/Geometry/Algorithms/Clipping.hs
@@ -27,35 +27,28 @@ import Geometry.Core
 -- do shading, hence the name.
 --
 -- >>> :{
--- haddockRender "Geometry/Algorithms/Clipping.hs/hatchd_polygon.svg" 100 100 $ do
+-- haddockRender "Geometry/Algorithms/Clipping.hs/hatched_polygon.svg" 100 100 $ do
 --     let polygon = Polygon [Vec2 10 10, Vec2 90 10, Vec2 90 90, Vec2 10 90]
 --     let shading = hatch polygon (deg 30) 10
 --     sketch polygon
+--     stroke
 --     for_ shading sketch
+--     setColor (mathematica97 1)
 --     stroke
 -- :}
--- docs/haddock/Geometry/Algorithms/Clipping.hs/hatchd_polygon.svg
+-- docs/haddock/Geometry/Algorithms/Clipping.hs/hatched_polygon.svg
 --
--- <<docs/haddock/Geometry/Algorithms/Clipping.hs/hatchd_polygon.svg>>
+-- <<docs/haddock/Geometry/Algorithms/Clipping.hs/hatched_polygon.svg>>
 hatch
     :: Polygon
     -> Angle -- ^ Direction in which the lines will point. @'deg' 0@ is parallel to the x axis.
     -> Double -- ^ Distance between shading lines
     -> [Line]
 hatch polygon angle hatchInterval = do
-
-    -- We first fill the whole bounding box perimeter with lines evenly spaced
-    -- horizontal lines. Afterwards we rotate them around the bounding box center
-    -- by the right angle, and use those for clipping.
-    let bb = boundingBox polygon
-        bbCenter = boundingBoxCenter bb
-        bbCircleR = let (bbW, bbH) = boundingBoxSize bb in norm (Vec2 bbW bbH) / 2
-    scissorsHorizontal <- do
-        let graceInterval = 1
-        shiftAmount <- takeWhile
-            (<= bbCircleR+graceInterval)
-            (iterate (+hatchInterval) (-bbCircleR-graceInterval))
-        pure (transform (translate (Vec2 0 shiftAmount)) (angledLine bbCenter (deg 0) 100))
-    let scissors = transform (rotateAround bbCenter angle) scissorsHorizontal
-
-    [line | (line, LineInsidePolygon) <- clipPolygonWithLine polygon scissors]
+    let polygonAligned = transform (rotate (negateV angle)) polygon
+    horizontalScissors <- do
+        let BoundingBox (Vec2 xLo yLo) (Vec2 xHi yHi) = boundingBox polygonAligned
+        y <- takeWhile (< yHi) (tail (iterate (+ hatchInterval) yLo))
+        pure (Line (Vec2 xLo y) (Vec2 xHi y))
+    horizontalHatches <- [line | (line, LineInsidePolygon) <- clipPolygonWithLine polygonAligned horizontalScissors]
+    pure (transform (rotate angle) horizontalHatches)
