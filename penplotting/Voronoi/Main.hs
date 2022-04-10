e76192395c47e9ec6cb8d366a0a65c5c9b1b4924
Franz Thoma <franz.thoma@tngtech.com>
2022-04-10 16:25:37 +0200

    Tweak parameters for something visually pleasing

--- penplotting/Voronoi/Main.hs
+++ penplotting/Voronoi/Main.hs
@@ -18,32 +18,31 @@ import           Graphics.Rendering.Cairo     as C
 
 picWidth, picHeight :: Num a => a
 picWidth = 1000
-picHeight = 720
+picHeight = 1000
 
 main :: IO ()
 main = do
-    gen <- initialize (V.fromList [124])
-    let count = 100
+    gen <- initialize (V.fromList [1234])
+    let center = Vec2 (picWidth / 2) (picHeight / 2)
+        count = 200
         -- constructed so that we have roughly `count` points
         adaptiveRadius = sqrt (0.75 * picWidth * picHeight / fromIntegral count)
         samplingProps = PoissonDiscParams
-            { _poissonShape = boundingBox [zero, Vec2 picWidth picHeight]
+            { _poissonShape = boundingBox (Vec2 100 100, Vec2 (picWidth - 100) (picHeight - 100))
             , _poissonRadius = adaptiveRadius
             , _poissonK = 4
             }
     points <- poissonDisc gen samplingProps
     print (length points)
-    let delaunay = lloydRelaxation 10 $ bowyerWatson (BoundingBox (Vec2 0 0) (Vec2 picWidth picHeight)) points
+    let delaunay = lloydRelaxation 8 $ bowyerWatson (boundingBox (Vec2 0 0, Vec2 picWidth picHeight)) points
         voronoi = toVoronoi delaunay
+        polygonInRange (Polygon xs) = all (\p -> norm (center -. p) < 400) xs
 
     render "out/voronoi.png" picWidth picHeight $ do
         setColor grey
         C.paint
-        for_ (getPolygons delaunay) (drawPoly black)
-        for_ (_voronoiCells voronoi) drawCell
-
-drawCell :: VoronoiCell () -> Render ()
-drawCell VoronoiCell{..} = drawPoly white _voronoiRegion
+        for_ (filter polygonInRange $ getPolygons delaunay) (drawPoly black)
+        for_ (filter polygonInRange $ _voronoiRegion <$> _voronoiCells voronoi) (drawPoly white)
 
 drawPoly :: Color Double -> Polygon -> Render ()
 drawPoly _ (Polygon []) = pure ()
--- src/Geometry/Algorithms/Voronoi.hs
+++ src/Geometry/Algorithms/Voronoi.hs
@@ -39,7 +39,7 @@ module Geometry.Algorithms.Voronoi (
     , mkVoronoi
     , addPoint
 
-    -- * Internal
+    -- * Internal>
     , updateCell
 ) where
 
