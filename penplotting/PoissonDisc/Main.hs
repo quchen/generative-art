d5f5134326a5f8dd46278fad9e5c101860286bcf
Franz Thoma <franz.thoma@tngtech.com>
2022-04-10 23:03:35 +0200

    Disc size based on simplex noise

--- penplotting/PoissonDisc/Main.hs
+++ penplotting/PoissonDisc/Main.hs
@@ -25,14 +25,13 @@ main :: IO ()
 main = do
 
     gen <- initialize (V.fromList [1237])
-    let center = Vec2 (picWidth / 2) (picHeight / 2)
-        bb = boundingBox (Vec2 50 50, Vec2 (picWidth - 50) (picHeight - 50))
-        count = 100
-        -- constructed so that we have roughly `count` points
-        adaptiveRadius = sqrt (0.75 * picWidth * picHeight / fromIntegral count)
+    noise <- simplex2 def { _simplexFrequency = 1/150 , _simplexOctaves = 2 } gen
+
+    let bb = boundingBox (Vec2 50 50, Vec2 (picWidth - 50) (picHeight - 50))
+        r0 = 20
         samplingProps = PoissonDiscParams
             { _poissonShape = bb
-            , _poissonRadius = \p -> adaptiveRadius / (1 + 0.01 * norm (p -. center))
+            , _poissonRadius = \p -> r0 * (1 + 0.7 * noise p)
             , _poissonK = 8
             }
     samples <- poissonDisc gen samplingProps
@@ -45,7 +44,7 @@ main = do
 
 drawSample :: (Vec2, Vec2, Double) -> Render ()
 drawSample (sample, parent, radius) = do
-    sketch (Line parent sample)
-    C.stroke
+    --sketch (Line parent sample)
+    --C.stroke
     sketch (Circle sample (radius/2))
     C.stroke
