5636e8b610b13c55bed4679f5f2637abd9aca8cd
David Luposchainsky <dluposchainsky@gmail.com>
2022-04-10 13:05:24 +0200

    Housekeeping

--- test/testsuite/Test/Draw/Plotting.hs
+++ test/testsuite/Test/Draw/Plotting.hs
@@ -4,18 +4,6 @@ module Test.Draw.Plotting (tests) where
 
 
 
-import           Control.Monad
-import           Data.Colour.Names
-import           Data.Default.Class
-import           Data.List
-import qualified Data.Vector              as V
-import           Graphics.Rendering.Cairo as C
-import           Text.Printf
-
-import Draw.Plotting
-import Geometry               as G
-import Numerics.Interpolation
-
 import Test.TastyAll
 
 
