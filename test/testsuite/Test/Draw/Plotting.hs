4474bd8e54cec0988a85329dd348f2ca71e7dfdb
David Luposchainsky <dluposchainsky@gmail.com>
2022-04-12 19:05:28 +0200

    Add first small GCode test (plotting length of a line)

--- test/testsuite/Test/Draw/Plotting.hs
+++ test/testsuite/Test/Draw/Plotting.hs
@@ -6,9 +6,20 @@ module Test.Draw.Plotting (tests) where
 
 import Test.TastyAll
 
+import Draw.Plotting
+import Geometry      as G
+
 
 
 tests :: TestTree
-tests = testGroup "GCode penplottingPlotting"
-    [
+tests = testGroup "Penplotting GCode"
+    [ testGroup "Plotting distance"
+        [ test_plottingDistanceOfLine
+        ]
     ]
+
+test_plottingDistanceOfLine :: TestTree
+test_plottingDistanceOfLine = testProperty "Line" $ \start end ->
+    let line = Line start end
+        (_gcode, (PlottingState {_drawingDistance = drawnDistance}, _)) = runPlotRaw def (plot line)
+    in lineLength line ~=== drawnDistance
