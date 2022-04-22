abf7aee78c3594aca4d7c3e288df1799863ce848
Franz Thoma <franz.thoma@tngtech.com>
2022-04-22 15:34:19 +0200

    Housekeeping

--- penplotting/Hatched/Main.hs
+++ penplotting/Hatched/Main.hs
@@ -6,7 +6,6 @@ import Draw
 import Geometry
 import qualified Geometry.Shapes as Shapes (haskellLogo)
 import Data.List (sortOn)
-import qualified Data.Text.Lazy.IO as T
 
 -- DIN A4
 _picWidth, picHeight :: Double
@@ -18,12 +17,15 @@ haskellLogo = transform (translate (Vec2 15 10) <> scale (picHeight - 20) <> mir
 
 main :: IO ()
 main = do
+    let settings = def { _feedrate = 30000, _zTravelHeight = 1 }
+    writeGCodeFile "hatching-pen-pressure.g" $ runPlot settings penPressure
+
+penPressure :: Plot ()
+penPressure = do
     let hatches = fmap (\poly -> hatch poly zero 1) haskellLogo
         hatchesWithPressure = concat $ zipWith (\p hs -> fmap (, p) hs) [2, 5, 10, 10] hatches
         sortedHatches = sortOn (\(Line (Vec2 _ y) _, _) -> y) $ sortOn (\(Line (Vec2 x _) _, _) -> x) hatchesWithPressure
-        settings = def { _feedrate = 30000, _zTravelHeight = 1 }
-        plotResult = runPlot settings $ for_ sortedHatches $ \(Line p q, pressure) -> do
-            withDrawingHeight (-pressure) $ do
-                repositionTo p
-                lineTo q
-    T.putStrLn (renderGCode (_plotGCode plotResult))
+    for_ sortedHatches $ \(Line p q, pressure) ->
+        withDrawingHeight (-pressure) $ do
+            repositionTo p
+            lineTo q
