b7c4d79810cf60ca802d43df20eb0d5f739d6527
Franz Thoma <franz.thoma@tngtech.com>
2022-05-17 14:27:55 +0200

    Clip lower layers

--- penplotting/metaballs/Main.hs
+++ penplotting/metaballs/Main.hs
@@ -2,6 +2,7 @@ module Main (main) where
 
 
 import Control.Monad
+import Data.List
 import qualified Graphics.Rendering.Cairo as C
 import System.Random.MWC
 
@@ -25,17 +26,17 @@ main = do
     let metaballs = vsum $ zipWith ball radii initialCenters
         slice z (Vec2 x y) = metaballs (Vec3 x y z)
         layers =
-            [ transform (isometricPerspective z) outlines
+            [ Polygon <$> transform (isometricPerspective z) outlines
             | z <- [-picHeight/2-60, -picHeight/2-55 .. picHeight/2+60]
             , let outlines = isoLines Grid { _range = (Vec2 (-60) (-60), Vec2 (picWidth+60) (picHeight+60)), _maxIndex = (picWidth `div` 5, picHeight `div` 5) } (slice z) 1
             ]
+        clippedLayers = zipWith clipWithAbove layers (drop 1 (tails layers))
 
     render "out/metaballs.png" picWidth picHeight $ do
         coordinateSystem (MathStandard_ZeroBottomLeft_XRight_YUp picHeight)
         cairoScope (setColor white >> C.paint)
-        for_ layers $ \outlines -> for_ outlines $ \outline -> do
-            sketch (Polyline outline)
-            setColor white >> C.fillPreserve
+        for_ clippedLayers $ \outlines -> for_ outlines $ \outline -> do
+            sketch outline
             setColor black >> C.stroke
 
 ball :: Double -> Vec3 -> Vec3 -> Double
@@ -48,3 +49,9 @@ isometricPerspective z
     <> rotateAround origin (deg 45)
   where origin = Vec2 (picWidth/2) (picHeight/2)
 
+clipWithAbove :: [Polygon] -> [[Polygon]] -> [Polygon]
+clipWithAbove layer above = go layer (concat above)
+  where
+    go [] _ = []
+    go xs [] = xs
+    go xs (y:ys) = go (xs >>= \x -> fst <$> (x `differencePP` y)) ys
