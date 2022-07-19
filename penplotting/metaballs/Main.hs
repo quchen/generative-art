4e364f449b30ac149ec0ff6b09628de00f3c509a
Franz Thoma <franz.thoma@tngtech.com>
2022-07-19 09:42:24 +0200

    Parameter tweaking

--- penplotting/metaballs/Main.hs
+++ penplotting/metaballs/Main.hs
@@ -20,8 +20,8 @@ picHeight = 400
 main :: IO ()
 main = do
     let count = 80
-    gen <- initializeMwc (42 :: Int)
-    initialCenters <- replicateM count (uniformRM (Vec3 100 50 (-picHeight/2+50), Vec3 (picWidth-100) (picHeight-50) (picHeight/2-100)) gen)
+    gen <- initializeMwc (27 :: Int)
+    initialCenters <- replicateM count (uniformRM (Vec3 100 100 (-picHeight/2+50), Vec3 (picWidth-100) (picWidth-100) (picHeight/2-50)) gen)
     radii <- replicateM count (uniformRM (20, 40) gen)
 
     let metaballs = vsum $ zipWith ball radii initialCenters
@@ -29,16 +29,17 @@ main = do
         layers =
             [ Polygon . toList . simplifyTrajectoryRdp 1 <$> transform (isometricPerspective z) outlines
             | z <- [-picHeight/2-60, -picHeight/2-55 .. picHeight/2+60]
-            , let outlines = isoLines Grid { _range = (Vec2 (-60) (-60), Vec2 (picWidth+60) (picHeight+60)), _maxIndex = (picWidth `div` 5, picHeight `div` 5) } (slice z) 1
+            , let outlines = isoLines Grid { _range = (Vec2 (-60) (-60), Vec2 (picWidth+60) (picWidth+60)), _maxIndex = (picWidth `div` 5, picWidth `div` 5) } (slice z) 1
             ]
         clippedLayers = zipWith clipWithAbove layers (drop 1 (tails layers))
-        penHoveringSettings = MinimizePenHoveringSettings { _getStartEndPoint = \(Polygon (p:ps)) -> (p, p), _flipObject = Nothing, _mergeObjects = Nothing }
+        penHoveringSettings = MinimizePenHoveringSettings { _getStartEndPoint = \(Polygon (p:_)) -> (p, p), _flipObject = Nothing, _mergeObjects = Nothing }
         paths = minimizePenHoveringBy penHoveringSettings $ S.fromList $ concat clippedLayers
 
     let settings = def
             { _feedrate = 3000
             , _zTravelHeight = 5
             , _zDrawingHeight = -2
+            --, _canvasBoundingBox = Just $ boundingBox [Vec2 0 0, Vec2 picWidth picHeight]
             }
         plotResult = runPlot settings $ do
             plot $ Polygon [Vec2 0 0, Vec2 picWidth 0, Vec2 picWidth picHeight, Vec2 0 picHeight]
@@ -52,10 +53,10 @@ ball radius center q = (radius^2 / normSquare (center -. q))**1.7
 
 isometricPerspective :: Double -> Transformation
 isometricPerspective z
-    =  translate (Vec2 0 (z / 1.4))
+    =  translate (Vec2 0 (z / 1.4 - 100))
     <> scaleAround' origin 1 0.35
-    <> rotateAround origin (deg 45)
-  where origin = Vec2 (picWidth/2) (picHeight/2)
+    <> rotateAround origin (deg (-45))
+  where origin = Vec2 (picWidth/2) (picWidth/2)
 
 clipWithAbove :: [Polygon] -> [[Polygon]] -> [Polygon]
 clipWithAbove layer above = go layer (concat above)
