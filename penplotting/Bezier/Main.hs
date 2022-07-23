79a5cec97580002858cc8b8fceb5e38fc1791985
Franz Thoma <franz.thoma@tngtech.com>
2022-07-23 18:03:59 +0200

    Plotting Bezier curve families

---
+++ penplotting/Bezier/Main.hs
@@ -0,0 +1,85 @@
+module Main (main) where
+
+
+
+import Control.Monad
+import Data.List
+import System.Random.MWC
+import Graphics.Rendering.Cairo         as C
+
+import Draw
+import Geometry                         as G
+import Geometry.Chaotic
+import Geometry.Algorithms.PerlinNoise
+import qualified Geometry.Processes.FlowField    as ODE
+import Numerics.DifferentialEquation
+import Numerics.VectorAnalysis
+
+
+
+picWidth, picHeight :: Num a => a
+picWidth = 500
+picHeight = 700
+
+canvas :: BoundingBox
+canvas = boundingBox [Vec2 50 50, Vec2 (picWidth - 50) (picHeight - 50)]
+
+-- Higher values yield lower-frequency noise
+noiseScale :: Double
+noiseScale = 1
+
+seed :: Int
+seed = 7
+
+main :: IO ()
+main = do
+    gen <- initializeMwc seed
+    initialPoints <- replicateM 4 $
+        uniformRM (zero, Vec2 picWidth picHeight) gen
+    let timeEvolution
+            = fitToCanvas
+            $ fmap bezierSmoothenOpen
+            $ transpose
+            $ fmap (take 50 . fmap snd . spaced 2000 . fieldLine rotationField)
+            $ initialPoints
+    render "out/bezier.png" picWidth picHeight $ do
+        cairoScope (setColor white >> C.paint)
+        for_ timeEvolution sketch
+        --sketch $ bezierSmoothenOpen initialPoints
+        C.stroke
+
+-- 2D vector potential, which in 2D is umm well a scalar potential.
+vectorPotential :: Vec2 -> Double
+vectorPotential p = noiseScale *. perlin2 params p
+  where
+    params = PerlinParameters
+        { _perlinFrequency   = 3 / (noiseScale * min picWidth picHeight)
+        , _perlinLacunarity  = 2
+        , _perlinOctaves     = 1
+        , _perlinPersistence = 0.5
+        , _perlinSeed        = seed
+        }
+
+rotationField :: Vec2 -> Vec2
+rotationField = curlZ vectorPotential
+
+fieldLine
+    :: (Vec2 -> Vec2)
+    -> Vec2
+    -> [(Double, Vec2)]
+fieldLine f p0 = rungeKuttaConstantStep (ODE.fieldLine f) p0 t0 dt
+  where
+    t0 = 0
+    dt = 10
+
+spaced :: Double -> [(Double, a)] -> [(Double, a)]
+spaced dt = go 0
+  where
+    go _ [] = []
+    go t0 ((t, a) : xs)
+        | t < t0 = go t0 xs
+        | t > t0 + dt = (t, a) : go (t0 + dt) ((t, a) : xs)
+        | otherwise = (t, a) : go (t0 + dt) xs
+
+fitToCanvas :: (HasBoundingBox geo, Transform geo) => geo -> geo
+fitToCanvas geo = G.transform (transformBoundingBox geo canvas def) geo
