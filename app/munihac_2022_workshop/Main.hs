ada12da2d33a36837f9275b960579745b9b861ed
Franz Thoma <franz.thoma@tngtech.com>
2022-10-07 23:20:22 +0200

    Simplify example

--- app/munihac_2022_workshop/Main.hs
+++ app/munihac_2022_workshop/Main.hs
@@ -8,17 +8,6 @@ import Geometry
 import Draw
 import Draw.Plotting
 
--- | Draw a 'Render' to PNG and SVG files.
-drawToFiles
-    :: FilePath -- ^ Output location, will store .svg and .png versions
-    -> Int -- ^ Width
-    -> Int -- ^ Height
-    -> Cairo.Render () -- ^ Picture, parameterized by width/height
-    -> IO ()
-drawToFiles filename w h drawing = do
-    render (filename ++ ".png") w h drawing
-    render (filename ++ ".svg") w h drawing
-
 -- | You can generate the output files using either:
 --
 -- `stack build munihac2022 --file-watch --exec munihac-2022-workshop`
@@ -27,6 +16,7 @@ drawToFiles filename w h drawing = do
 -- `ghcid --command='stack ghci --main-is munihac2022' --test=main -W`
 -- (fast compilation, slower execution time, requires `ghcid` installation)
 main :: IO ()
-main = drawToFiles "munihac2022/shatter" 1000 1000 $ do
-    setColor white
-    Cairo.paint
+main = do
+    render "munihac2022/shatter.png" 1000 1000 $ do
+        setColor white
+        Cairo.paint
