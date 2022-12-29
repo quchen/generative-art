a378c6e49e95660ccb2196bb76f483d603485b22
Franz Thoma <franz.thoma@tngtech.com>
2022-12-29 21:05:03 +0100

    Implement actual collapse for 3x3 stencils

--- src/Geometry/Algorithms/WaveFunctionCollapse.hs
+++ src/Geometry/Algorithms/WaveFunctionCollapse.hs
@@ -2,12 +2,13 @@
 module Geometry.Algorithms.WaveFunctionCollapse where
 
 import System.Random.MWC as MWC
+import Control.Monad ((>=>))
 import Control.Monad.ST
 import Control.Applicative
 import Data.List (sortOn, find, nub)
-import Data.Maybe (catMaybes)
+import Data.Maybe (maybeToList, catMaybes)
 
-data Zipper a = Zipper [a] a [a] deriving (Eq, Ord, Functor)
+data Zipper a = Zipper ![a] !a ![a] deriving (Eq, Ord, Functor)
 
 fromList :: [a] -> Zipper a
 fromList (x:xs) = Zipper [] x xs
@@ -58,6 +59,10 @@ down  (Grid zipper) = Grid <$> next zipper
 instance Foldable Grid where
     foldr plus zero (Grid zz) = foldr plus zero (toList zz >>= toList)
 
+instance Applicative Grid where
+    pure a = Grid (pure (pure a))
+    liftA2 f (Grid xs) (Grid ys) = Grid (liftA2 (liftA2 f) xs ys)
+
 extract :: Grid a -> a
 extract (Grid zipper) = extractZ (extractZ zipper)
 
@@ -86,6 +91,17 @@ data WfcSettings a = WfcSettings
 type Coordinate = (Int, Int)
 
 data WfState a = Unobserved [a] | Observed a | Contradiction deriving Eq
+
+wfState :: [a] -> WfState a
+wfState [] = Contradiction
+wfState [a] = Observed a
+wfState as = Unobserved as
+
+unWfState :: WfState a -> [a]
+unWfState = \case
+    Contradiction -> []
+    Observed x -> [x]
+    Unobserved xs -> xs
 
 instance Foldable WfState where
     foldr plus zero = \case
@@ -131,10 +147,30 @@ propagate projection = go
         let grid' = extend projection grid
         in if grid' == grid then grid else go grid'
 
-type Stencil3x3 a = (Maybe a, Maybe a, Maybe a, Maybe a, Maybe a, Maybe a, Maybe a, Maybe a, Maybe a)
+data Stencil3x3 a = Stencil3x3
+    { s11 :: !(Maybe a)
+    , s12 :: !(Maybe a)
+    , s13 :: !(Maybe a)
+    , s21 :: !(Maybe a)
+    , s22 :: !a
+    , s23 :: !(Maybe a)
+    , s31 :: !(Maybe a)
+    , s32 :: !(Maybe a)
+    , s33 :: !(Maybe a)
+    } deriving (Eq, Functor)
+
+instance Applicative Stencil3x3 where
+    pure a = Stencil3x3 (Just a) (Just a) (Just a) (Just a) a (Just a) (Just a) (Just a) (Just a)
+    liftA2 f (Stencil3x3 a1 b1 c1 d1 e1 f1 g1 h1 i1) (Stencil3x3 a2 b2 c2 d2 e2 f2 g2 h2 i2) =
+        Stencil3x3 (liftA2 f a1 a2) (liftA2 f b1 b2) (liftA2 f c1 c2)
+                   (liftA2 f d1 d2) (       f e1 e2) (liftA2 f f1 f2)
+                   (liftA2 f g1 g2) (liftA2 f h1 h2) (liftA2 f i1 i2)
+
+instance Foldable Stencil3x3 where
+    foldr plus zero Stencil3x3{..} = foldr plus zero (catMaybes [s11, s12, s13, s21, Just s22, s23, s31, s32, s33])
 
 stencil3x3 :: Grid a -> Stencil3x3 a
-stencil3x3 grid = toTuple $ fmap (fmap extract)
+stencil3x3 grid = toStencil $ fmap (fmap extract)
     [ left =<< upGrid, upGrid, right =<< upGrid
     , left grid, Just grid, right grid
     , left =<< downGrid, downGrid, right =<< downGrid
@@ -142,14 +178,77 @@ stencil3x3 grid = toTuple $ fmap (fmap extract)
   where
     upGrid = up grid
     downGrid = down grid
-    toTuple [a, b, c, d, e, f, g, h, i] = (a, b, c, d, e, f, g, h, i)
-    toTuple _ = undefined
+    toStencil [a, b, c, d, Just e, f, g, h, i] = Stencil3x3 a b c d e f g h i
+    toStencil _ = undefined
 
 stencils3x3 :: Grid a -> [Stencil3x3 a]
 stencils3x3 = foldr (:) [] . extend stencil3x3
+
+stencilToGrid :: Stencil3x3 a -> Grid a
+stencilToGrid (Stencil3x3 a b c d e f g h i) = Grid (Zipper upper middle lower)
+  where
+    upper = case liftA3 (,,) a b c of
+        Just (a', b', c') -> [Zipper [a'] b' [c']]
+        Nothing           -> []
+    middle = Zipper (maybeToList d) e (maybeToList f)
+    lower = case liftA3 (,,) g h i of
+        Just (g', h', i') -> [Zipper [g'] h' [i']]
+        Nothing           -> []
 
 settingsFromGrid :: Eq a => Grid a -> WfcSettings (Stencil3x3 a)
 settingsFromGrid grid = WfcSettings{..}
   where
     wfcTiles = nub (stencils3x3 grid)
-    wfcLocalProjection grid' = case stencil3x3 grid' of _
+    wfcLocalProjection :: Eq a => Grid (WfState (Stencil3x3 a)) -> WfState (Stencil3x3 a)
+    wfcLocalProjection = wfState . remainingEigenvalues . fmap unWfState
+
+{-
+Concept:
+* Take the middle stencil
+* Move in some direction, and take the stencils there
+* Displace the middle stencil in the same direction
+* Compare the shifted middle stencil to each of the neighbouring stencils
+* If any of them is `weakEq`, then the middle stencil is still allowed.
+
+If in one given direction none of the neighbouring stencils matches, then the middle stencil is discarded.
+
+_ _ _   _ _ _   _ _ _
+_ a b   a b c   b c _
+_ d e   d e f   e f _
+
+_ a b   a b c   b c _
+_ d e   d e f   e f _
+_ g h   g h i   h i _
+
+_ d e   d e f   e f _
+_ g h   g h i   h i _
+_ _ _   _ _ _   _ _ _
+-}
+remainingEigenvalues :: Eq a => Grid [Stencil3x3 a] -> [Stencil3x3 a]
+remainingEigenvalues grid =
+    [ this
+    | this <- extract grid
+    , (there, back) <- zip neighbouringDirections neighbouringDirections
+    , thisShifted <- maybeToList (back (stencilToGrid this))
+    , not $ null
+        [ other
+        | other <- maybeToList (there grid) >>= extract
+        , thisShifted `weakEq` stencilToGrid other
+        ]
+    ]
+  where
+    neighbouringDirections :: [Grid b -> Maybe (Grid b)]
+    neighbouringDirections =
+        [ up >=> left
+        , up
+        , up >=> right
+        , left
+        , right
+        , down >=> left
+        , down
+        , down >=> right
+        ]
+
+-- Compares the overlapping parts of the two grids, i.e. comparing the points where both sides are defined.
+weakEq :: Eq a => Grid a -> Grid a -> Bool
+weakEq as bs = and (liftA2 (==) as bs)
