3573199fa25791d0944358b870502a13d4e8df3a
Franz Thoma <franz.thoma@tngtech.com>
2023-01-15 12:17:00 +0100

    Documentation

--- src/Geometry/Algorithms/Sampling/Vogel.hs
+++ src/Geometry/Algorithms/Sampling/Vogel.hs
@@ -8,12 +8,31 @@ import Geometry.Core
 
 data VogelSamplingParams = VogelSamplingParams
     { _vogelCenter :: Vec2
-    -- ^ Center of the distribution
+    -- ^ Center of the point cloud
     , _vogelDensity :: Double
-    -- ^ Density, measured in (approximate) points per unit square
+    -- ^ Density (samples per unit area)
     , _vogelRadius :: Double
+    -- ^ Radius of the point cloud
     }
 
+-- | This sampling method produces quite evenly spaced points, but unlike
+--   randomized sampling methods such as Poisson-Disc, it also exposes a
+--   distinctive spiral structure that can result in interesting patterns.
+--
+-- The concept of using a Vogel spiral for sampling is inspired by
+-- https://bugsmapsandmath.com/2017/03/01/field-sampling-on-a-fibonacci-spiral/
+--
+-- The nomenclature is not entirely clear:
+-- * It looks like this particular spiral of points spaced by a golden angle is
+--   called a Vogel spiral, although I could not find much documentation about
+--   it.
+-- * The underlying concept seems to be a Fermat spiral, i.e. a spiral where
+--   the radius grows like the square root of the angle. So I suppose one could
+--   say, the Vogel spiral is obtained by sampling points on a Fermat spiral
+--   with an angular spacing of the golden angle.
+-- * To complicate matters further, the Vogel spiral seems to be sometimes
+--   referred to as a Fibonacci spiral (including the link above that inspired
+--   me), but a Fibonacci spiral is actually a different thing.
 vogel :: VogelSamplingParams -> [Vec2]
 vogel VogelSamplingParams{..} =
     [ _vogelCenter +. polar theta r
@@ -22,8 +41,3 @@ vogel VogelSamplingParams{..} =
     , let theta = rad (2*pi*i/phi)
     ]
   where phi = (1 + sqrt 5) / 2
-
-  
-
-  -- N = r^2 * pi * density
-  -- A = r^2 * pi
