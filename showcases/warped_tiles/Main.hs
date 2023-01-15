4621964b7a9f67ed47dc7c8f138b985614dd4bd3
Franz Thoma <franz.thoma@tngtech.com>
2023-01-15 23:35:28 +0100

    Add field lines

--- showcases/warped_tiles/Main.hs
+++ showcases/warped_tiles/Main.hs
@@ -17,12 +17,17 @@ import Geometry.Algorithms.Sampling
 import Control.Monad (replicateM)
 import Control.Applicative (Applicative(liftA2))
 import Debug.Trace
+import Numerics.VectorAnalysis (grad)
+import Numerics.DifferentialEquation (rungeKuttaAdaptiveStep)
 
 
 
 picWidth, picHeight :: Num a => a
 picWidth = 2560
 picHeight = 1440
+
+canvas :: BoundingBox
+canvas = boundingBox [zero, Vec2 picWidth picHeight]
 
 file :: FilePath
 file = "out/warped-tiles.png"
@@ -34,10 +39,35 @@ main = render file picWidth picHeight $ do
     C.setLineWidth 10
     for_ [-1, -0.9 .. 2] $ \z -> do
         for_ (isoLines GridSpec { _range = (zero, Vec2 picWidth picHeight), _maxIndex = (128, 72)} potential z) $ \isoline -> do
-            sketch (Polyline $ traceShowId isoline)
+            sketch (Polyline isoline)
             C.stroke
+    for_ [Vec2 1750 750, Vec2 1000 900] $ \p -> do
+        sketch (fieldLine p)
+        C.stroke
+    for_ [ p +. polar phi 50 | (p, _) <- charges, phi <- rad <$> [0, 0.05*pi .. 2*pi] ] $ \p -> do
+        sketch (fieldLine p)
+        C.stroke
+
+charges :: [(Vec2, Double)]
+charges = [ (Vec2 500 300, 1000), (Vec2 2000 500, -1000), (Vec2 1500 1000, 1000) ]
 
 potential :: Vec2 -> Double
 potential p = sum [ q / (picWidth / 5 + norm (p -. p')) | (p', q) <- charges ]
   where
-    charges = [ (Vec2 500 300, 1000), (Vec2 2000 500, -1000), (Vec2 1500 1000, 1000) ]
+
+vectorField :: Vec2 -> Vec2
+vectorField = grad potential
+
+fieldLine :: Vec2 -> Polyline []
+fieldLine p = Polyline (reverse trajectoryB ++ trajectoryA)
+  where
+    trajectory vf = rungeKuttaAdaptiveStep (const vf) p 0 0.1 normSquare 0.001
+    trajectoryA = takeWhile (`insideBoundingBox` canvas) $ takeUntil hitsCharge (snd <$> trajectory vectorField)
+    trajectoryB = takeWhile (`insideBoundingBox` canvas) $ takeUntil hitsCharge (snd <$> trajectory (negateV vectorField))
+
+takeUntil :: (a -> Bool) -> [a] -> [a]
+takeUntil p = takeWhile (not . p)
+
+hitsCharge :: Vec2 -> Bool
+hitsCharge p = any (\(p', _) -> norm (p -. p') < 10) charges
+
