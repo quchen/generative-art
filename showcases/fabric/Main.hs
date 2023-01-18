2c96d968fd1343314e231f02720d64b5f6672543
Franz Thoma <franz.thoma@tngtech.com>
2023-01-18 01:21:01 +0100

    Debugging, lots of fixes

--- showcases/fabric/Main.hs
+++ showcases/fabric/Main.hs
@@ -14,6 +14,7 @@ import           System.Random.MWC
 import qualified Data.Vector as V
 
 import Draw hiding (Cross)
+import Draw.Grid
 import Geometry                     as G hiding (Grid)
 import Geometry.Algorithms.WaveFunctionCollapse
 import Data.Function (on)
@@ -31,33 +32,36 @@ cellSize = 160
 
 main :: IO ()
 main = do
-    let grid = M.findMax <$> gridGenerations !! 0
+    let grid = (\[a] -> a) . M.toList <$> last gridGenerations
     render "out/fabric.png" picWidth picHeight $ do
         cairoScope (setColor white >> paint)
         setLineWidth 20
-        drawGrid cellSize grid
+        renderGrid cellSize grid
 
-gridGenerations :: [Grid (M.MultiSet (Tile ColorIndex))]
+instance DrawToSize Tile where
+    drawToSize (w, h) = drawCell (min w h)
+
+gridGenerations :: [Grid (M.MultiSet Tile)]
 gridGenerations = runST $ do
     gen <- create
     wfc wfcSettings (picWidth `div` cellSize) (picHeight `div` cellSize) gen
 
-drawGrid :: Double -> Grid (Tile ColorIndex) -> C.Render ()
-drawGrid cellSize grid@(Grid _ _ _ _ cells) = do
+renderGrid :: Double -> Grid Tile -> C.Render ()
+renderGrid cellSize grid@(Grid _ _ _ _ cells) = do
     let (w, h) = size grid
     for_ [0..w-1] $ \x -> for_ [0..h-1] $ \y -> cairoScope $ do
-        C.translate ((fromIntegral x + 0.5) * cellSize) ((fromIntegral y + 0.5) * cellSize)
+        C.translate (fromIntegral x * cellSize) (fromIntegral y * cellSize)
         drawCell cellSize (cells V.! y V.! x)
 
-drawCell :: Double -> Tile ColorIndex -> C.Render ()
+drawCell :: Double -> Tile -> C.Render ()
 drawCell cellSize = cairoScope . \case
     Empty -> pure ()
     Single Horizontal i -> do
-        sketch (Line (Vec2 (-cellSize/2) 0) (Vec2 (cellSize/2) 0))
+        sketch (Line (Vec2 0 (cellSize/2)) (Vec2 cellSize (cellSize/2)))
         setColor (mathematica97 i)
         C.stroke
     Single Vertical i -> do
-        sketch (Line (Vec2 0 (-cellSize/2)) (Vec2 0 (cellSize/2)))
+        sketch (Line (Vec2 (cellSize/2) 0) (Vec2 (cellSize/2) cellSize))
         setColor (mathematica97 i)
         C.stroke
     Cross Vertical i j -> do
@@ -67,7 +71,7 @@ drawCell cellSize = cairoScope . \case
         drawCell cellSize (Single Vertical j)
         drawCell cellSize (Single Horizontal i)
 
-wfcSettings :: WfcSettings (Tile ColorIndex)
+wfcSettings :: WfcSettings Tile
 wfcSettings = WfcSettings {..}
   where
     wfcTiles = M.fromList $ concat
@@ -88,10 +92,10 @@ wfcSettings = WfcSettings {..}
             , d <- traverse (M.toList . extract) (down  grid)
             ]
 
-data Tile a
+data Tile
     = Empty
-    | Single Direction a
-    | Cross Direction a a
+    | Single Direction ColorIndex
+    | Cross Direction ColorIndex ColorIndex
     -- ^ Direction = which one is on top
     -- Tags: Horizontal, Vertical
     deriving (Eq, Ord, Show)
@@ -100,18 +104,18 @@ data Direction = Horizontal | Vertical deriving (Eq, Ord, Show)
 
 type ColorIndex = Int
 
-component :: Direction -> Tile a -> Tile a
+component :: Direction -> Tile -> Tile
 component Horizontal (Single Horizontal a) = Single Horizontal a
 component Horizontal (Cross Horizontal a b) = Single Horizontal a
 component Horizontal (Cross Vertical a b) = Single Horizontal a
 component Horizontal _ = Empty
 component Vertical (Single Vertical a) = Single Vertical a
-component Vertical (Cross Horizontal a b) = Single Horizontal b
-component Vertical (Cross Vertical a b) = Single Horizontal b
+component Vertical (Cross Horizontal a b) = Single Vertical b
+component Vertical (Cross Vertical a b) = Single Vertical b
 component Vertical _ = Empty
 
 
-compatible :: Eq a => Stencil3x3 (Tile a) -> Bool
+compatible :: Stencil3x3 Tile -> Bool
 compatible (Stencil3x3 _ a _ b Empty c _ d _)
     | Just Single{} <- component Vertical <$> a = False
     | Just Single{} <- component Horizontal <$> b = False
@@ -124,6 +128,12 @@ compatible (Stencil3x3 _ a _ b (Single Horizontal color) c _ d _)
     | Just (Single Horizontal color1) <- component Horizontal <$> b
     , Just (Single Horizontal color2) <- component Horizontal <$> c 
     = color == color1 && color == color2
+    | Just (Single Horizontal color1) <- component Horizontal <$> b
+    , Nothing <- c
+    = color == color1
+    | Nothing <- b
+    , Just (Single Horizontal color2) <- component Horizontal <$> c 
+    = color == color2
     | otherwise = False
 compatible (Stencil3x3 _ a _ b (Single Vertical color) c _ d _)
     | Just Single{} <- component Horizontal <$> b = False
@@ -131,14 +141,21 @@ compatible (Stencil3x3 _ a _ b (Single Vertical color) c _ d _)
     | Just (Single Vertical color1) <- component Vertical <$> a
     , Just (Single Vertical color2) <- component Vertical <$> d
     = color == color1 && color == color2
+    | Just (Single Vertical color1) <- component Vertical <$> a
+    , Nothing <- d
+    = color == color1
+    | Nothing <- a
+    , Just (Single Vertical color2) <- component Vertical <$> d
+    = color == color2
     | otherwise = False
-compatible (Stencil3x3 _ a _ b (Cross _ colorH colorV) c _ d _)
+compatible s@(Stencil3x3 _ a _ b (Cross _ colorH colorV) c _ d _)
+    | Nothing <- a = compatible s { s12 = Just (Single Vertical   colorV) }
+    | Nothing <- b = compatible s { s21 = Just (Single Horizontal colorH) }
+    | Nothing <- c = compatible s { s23 = Just (Single Horizontal colorH) }
+    | Nothing <- d = compatible s { s32 = Just (Single Vertical   colorV) }
     | Just (Single Vertical colorA) <- component Vertical <$> a
     , Just (Single Horizontal colorB) <- component Horizontal <$> b
     , Just (Single Horizontal colorC) <- component Horizontal <$> c
     , Just (Single Vertical colorD) <- component Vertical <$> d
     = colorA == colorV && colorB == colorH && colorC == colorH && colorD == colorV
     | otherwise = False
-
-extractStencil :: Stencil3x3 a -> a 
-extractStencil (Stencil3x3 _ _ _ _ e _ _ _ _) = e
