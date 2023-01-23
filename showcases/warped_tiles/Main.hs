0e90a5c711eb1784f1dd93b4901dc25b8b397010
Franz Thoma <franz.thoma@tngtech.com>
2023-01-23 23:36:04 +0100

    More randomness

--- showcases/warped_tiles/Main.hs
+++ showcases/warped_tiles/Main.hs
@@ -4,12 +4,13 @@ module Main (main) where
 
 
 
+import Control.Monad.Primitive
 import Data.List ( sortOn, (\\) )
 import Data.Maybe ( fromMaybe, isJust, catMaybes )
 import Data.Ord ( comparing )
 import qualified Data.Vector as V
 import Graphics.Rendering.Cairo as C
-import System.Random.MWC ( initialize, uniformRM, GenIO )
+import System.Random.MWC ( initialize, uniformRM, GenIO, create, Gen )
 
 import Draw
 import Geometry as G
@@ -21,6 +22,8 @@ import Debug.Trace
 import Numerics.VectorAnalysis (grad, divergence)
 import Numerics.DifferentialEquation (rungeKuttaAdaptiveStep)
 import Data.List.Extended (nubOrd)
+import Control.Monad.ST
+import Data.Traversable
 
 
 
@@ -39,11 +42,12 @@ main = render file picWidth picHeight $ do
     cairoScope (setColor white >> C.paint)
     setColor black
     C.setLineWidth 1
-    let iso = isoLines Grid { _range = (zero, Vec2 picWidth picHeight), _maxIndex = (128, 72)} potential
+    let iso = isoLines Grid { _range = (zero, Vec2 picWidth picHeight), _maxIndex = (256, 144)} potential
         potentialLines = 
             [ Polyline isoline
-            | z <- [3, 3.3 .. 10]
-            , isoline <- iso z
+            | z <- [-50, -49.7 .. 20]
+            , isoline@(p:_) <- iso z
+            , and [ norm (p -. p') > 20 | (p', _) <- charges ]
             ]
         fieldLines = sampleFieldLines
         intersectionPoints = nubOrd $ do
@@ -73,7 +77,20 @@ chaikin lambda (Polygon ps@(p:_)) = Polygon $ concat
     ]
 
 charges :: [(Vec2, Double)]
-charges = [ (Vec2 500 300, 1), (Vec2 2000 500, -1), (Vec2 1500 1000, 1) ]
+charges = traceShowId $ runST $ do
+    gen <- create
+    let poissonShape = canvas
+        poissonRadius = 800
+        poissonK = 10
+    ps <- poissonDisc gen poissonShape poissonRadius poissonK
+    for ps $ \p -> do
+        q <- pick gen [-1, 1]
+        pure (p, q)
+
+pick :: PrimMonad m => Gen (PrimState m) -> [a] -> m a
+pick gen xs = do
+    i <- uniformRM (0, length xs - 1) gen
+    pure (xs !! i)
 
 potential :: Vec2 -> Double
 potential p = sum [ q * log (norm (p -. p')) | (p', q) <- charges ]
@@ -102,7 +119,7 @@ sampleFieldLines = go charges [] []
     go ((p, q):cs) sourcePoints fls' =
         go cs' (catMaybes targetPoints ++ sourcePoints) (fls' ++ fls)
       where
-        deltaPhi = 15 / abs q
+        deltaPhi = 24 / abs q
         psis =
             [ angleOfLine (Line p' (last fl'))
             | ((p', _), Polyline fl') <- sourcePoints
