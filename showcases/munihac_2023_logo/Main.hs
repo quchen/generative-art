5ccde69208a11a08f82581eb795477ee78050c01
Franz Thoma <franz.thoma@tngtech.com>
2023-04-23 20:04:52 +0200

    First draft of Haskell logo

--- showcases/munihac_2023_logo/Main.hs
+++ showcases/munihac_2023_logo/Main.hs
@@ -16,6 +16,8 @@ import           Geometry.Algorithms.Sampling
 import           Geometry.Algorithms.Voronoi
 import           Geometry.Shapes              (haskellLogo)
 import           Graphics.Rendering.Cairo     as C
+import Control.Monad (guard)
+import Graphics.Rendering.Cairo.Types (Cairo(Cairo))
 
 
 
@@ -28,7 +30,7 @@ mainHaskellLogo = do
     let picWidth, picHeight :: Num a => a
         picWidth = 1000
         picHeight = 720
-        count = 400
+        count = 200
     gen <- initialize (V.fromList (map (fromIntegral . ord) (show count)))
     let -- constructed so that we have roughly `count` points
         adaptiveRadius = sqrt (0.75 * picWidth * picHeight / fromIntegral count)
@@ -40,9 +42,17 @@ mainHaskellLogo = do
     points <- poissonDisc gen samplingProps
     print (length points)
     let voronoi = toVoronoi (bowyerWatson (BoundingBox (Vec2 0 0) (Vec2 picWidth picHeight)) points)
-    let voronoiColorized = mapWithMetadata (\seed _polygon ann -> colorizePolygon (picWidth, picHeight) seed ann) voronoi
 
-    render "munihac-2023-logo.png" picWidth picHeight $ for_ (_voronoiCells voronoiColorized) drawCell
+    let polygonsAndColors = do
+            voronoiRegion <- _voronoiRegion <$> _voronoiCells voronoi
+            (glyph, color) <- haskellLogoWithColors (picWidth, picHeight)
+            (polygon, _) <- intersectionPP voronoiRegion glyph
+            pure (chaikin 0.25 $ chaikin 0.15 $ growPolygon (-4) polygon, color)
+
+    render "munihac-2023-logo.png" picWidth picHeight $ do
+        cairoScope (setColor white >> C.paint)
+        for_ polygonsAndColors $ \(polygon, color) ->
+            drawPoly polygon color
 
 haskellLogoWithColors :: (Double, Double) -> [(Polygon, Color Double)]
 haskellLogoWithColors (picWidth, picHeight)= zip haskellLogoCentered haskellLogoColors
@@ -50,26 +60,22 @@ haskellLogoWithColors (picWidth, picHeight)= zip haskellLogoCentered haskellLogo
     haskellLogoCentered = G.transform (G.translate (Vec2 (picWidth/2 - 480) (picHeight/2 - 340)) <> G.scale 680) haskellLogo
     haskellLogoColors = [haskell 0, haskell 1, haskell 2, haskell 2]
 
-
-colorizePolygon :: (Double, Double) -> Vec2 -> () -> Color Double
-colorizePolygon (w, h) seed _ = case find (pointInPolygon seed . fst) (haskellLogoWithColors (w, h)) of
-            Just (_, c) -> c
-            Nothing     -> darkGrey
-
-drawCell :: VoronoiCell (Color Double) -> Render ()
-drawCell VoronoiCell{..} = drawPoly _voronoiRegion _voronoiProps
-
 drawPoly :: Polygon -> Color Double -> Render ()
 drawPoly (Polygon []) _ = pure ()
 drawPoly poly color = do
-    let fillColor = color
-        lineColor = blend 0.1 white color
     sketch poly
-    setColor fillColor
-    fillPreserve
-    setColor lineColor
-    setLineWidth 1
+    setColor color
+    setLineWidth 6
     stroke
 
 darkGrey :: Color Double
 darkGrey = hsv 0 0 0.1
+
+chaikin :: Double -> Polygon -> Polygon
+chaikin _ (Polygon []) = Polygon []
+chaikin lambda (Polygon ps@(p:_)) = Polygon $ concat
+    [ [c, b]
+    | (a, d) <- zip ps (tail ps ++ [p])
+    , let b = lambda *. a +. (1-lambda) *. d
+    , let c = (1-lambda) *. a +. lambda *. d
+    ]
