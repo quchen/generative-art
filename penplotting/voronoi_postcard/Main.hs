4d41ce43f2681ac61183d1751fb9cbba4cae00c1
David Luposchainsky <dluposchainsky@gmail.com>
2023-05-23 16:55:49 +0200

    commit all

--- penplotting/voronoi_postcard/Main.hs
+++ penplotting/voronoi_postcard/Main.hs
@@ -2,28 +2,15 @@ module Main (main) where
 
 
 
-import           Control.Monad
-import           Data.Coerce
-import           Data.List
-import qualified Data.Vector              as V
-import           Graphics.Rendering.Cairo as C hiding (height, width, x, y)
-import           Options.Applicative
-import  qualified          System.Random.MWC    as MWC
-
-
-import qualified Data.Set                        as S
-import           Data.Vector                     (Vector)
+import qualified Data.Set                     as S
+import           Data.Vector                  (Vector)
+import qualified Data.Vector                  as V
 import           Draw
 import           Draw.Plotting
-import           Draw.Plotting.CmdArgs
-import           Geometry                        as G
-import           Geometry.Algorithms.PerlinNoise
+import           Geometry                     as G
+import           Geometry.Algorithms.Delaunay
 import           Geometry.Algorithms.Sampling
-import qualified Geometry.Processes.FlowField    as ODE
-import           Numerics.DifferentialEquation
-import           Numerics.Functions
-import           Numerics.VectorAnalysis
-import Geometry.Algorithms.Delaunay
+import qualified System.Random.MWC            as MWC
 
 
 
@@ -58,41 +45,8 @@ main = do
 
         plottingSettings = def { _zTravelHeight = 5 }
         plotResult = runPlot plottingSettings $ do
-            -- let polylines = optimize ()
-            let hullPoints = S.fromList . toList . delaunayHull $ delaunay
-                paintUs = do
-                    line@(Line start end) <- delaunayEdges delaunay
-                    guard (all (\p -> S.notMember p hullPoints) [start, end] || True)
-                    pure line
-            for_ (optimize paintUs) plot
+            for_ (optimize (delaunayEdges delaunay)) plot
 
     writeGCodeFile outfileG plotResult
     renderPreview outfilePreview 3 plotResult
     pure ()
-
-
-widthHeightMargin :: Options -> (Double, Double, Double)
-widthHeightMargin Options{_canvas=Canvas{_canvasWidth=width, _canvasHeight=height, _canvasMargin=margin}} = (width, height, margin)
-
-data Options = Options
-    { _outputFileG :: FilePath
-    , _canvas :: Canvas
-    } deriving (Eq, Ord, Show)
-
-
-commandLineOptions :: IO Options
-commandLineOptions = execParser parserOpts
-  where
-    progOpts = Options
-        <$> strOption (mconcat
-            [ long "output"
-            , short 'o'
-            , metavar "<file>"
-            , help "Output GCode file"
-            ])
-        <*> canvasP
-
-    parserOpts = info (progOpts <**> helper)
-      ( fullDesc
-     <> progDesc "Convert SVG to GCode"
-     <> header "Not that much of SVG is supported, bear with me…" )
