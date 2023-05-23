e2ccd689f5d0c48b54565d50803a710000bd7a0a
David Luposchainsky <dluposchainsky@gmail.com>
2023-05-23 17:21:56 +0200

    Artwork: Delaunay/Voronoi postcard

--- penplotting/voronoi_postcard/Main.hs
+++ penplotting/voronoi_postcard/Main.hs
@@ -8,7 +8,7 @@ import           Data.List
 import qualified Data.Vector              as V
 import           Graphics.Rendering.Cairo as C hiding (height, width, x, y)
 import           Options.Applicative
-import           System.Random.MWC        (initialize)
+import  qualified          System.Random.MWC    as MWC
 
 
 import qualified Data.Set                        as S
@@ -23,125 +23,64 @@ import qualified Geometry.Processes.FlowField    as ODE
 import           Numerics.DifferentialEquation
 import           Numerics.Functions
 import           Numerics.VectorAnalysis
+import Geometry.Algorithms.Delaunay
 
 
-
--- Higher values yield lower-frequency noise
-noiseScale :: Double
-noiseScale = 0.5
-
-noiseSeed :: Int
-noiseSeed = 519496
 
 main :: IO ()
 main = do
-    options <- commandLineOptions
-    geometry <- mkGeometry options
+    let width = 228
+        height = 150
+        numPoints = 100
 
-    let (width, height, _) = widthHeightMargin options
-    render "out/vector_fields.svg" (round width) (round height) $ do
+        outfileG = "out/postcard-delaunay.g"
+        outfilePreview = "out/postcard.svg"
 
-        cairoScope $ do
-            setColor black
-            C.paint
+        margin = 10
+        paperBB = boundingBox [zero, Vec2 width height]
+        drawBB = shrinkBoundingBox margin paperBB
 
-        cairoScope $ do
-            setColor white
-            setLineWidth 0.3
-            for_ geometry drawFieldLine
+    points <- do
+        gen <- MWC.initialize (V.fromList [])
+        -- ps <- gaussianDistributedPoints gen drawBB (min width height / 8 *. mempty) numPoints
+        ps <- poissonDisc gen PoissonDiscParams
+            { _poissonShape = drawBB
+            , _poissonRadius = 6
+            , _poissonK = 10
+            }
+        let ps' = filter (\p -> norm (boundingBoxCenter drawBB -. p) < 59) ps
+        pure (lloydRelaxation drawBB 0.1 ps')
 
-    let drawing = sequence
-            [ plot (Polyline part)
-            | trajectory <- geometry
-            , part <- simplifyTrajectoryRadial 2 <$> splitIntoInsideParts options trajectory ]
+    let delaunay = delaunayTriangulation points
 
-        plottingSettings = def { _feedrate = 6000, _zTravelHeight = 5, _zDrawingHeight = -2 }
+        optimize :: Vector Line -> [Polyline Vector]
+        optimize lines1 =
+            let foo = S.fromList $ toList $ V.map (\(Line start end) -> V.fromList [start, end]) lines1
+            in fmap Polyline (minimizePenHovering foo)
 
-    writeGCodeFile (_outputFileG options) (runPlot plottingSettings drawing)
+        plottingSettings = def { _zTravelHeight = 5 }
+        plotResult = runPlot plottingSettings $ do
+            -- let polylines = optimize ()
+            let hullPoints = let Polygon ps = _convexHull delaunay in S.fromList ps
+                paintUs = do
+                    line@(Line start end) <- _edges delaunay
+                    guard (all (\p -> S.notMember p hullPoints) [start, end] || True)
+                    pure line
+            for_ (optimize paintUs) plot
 
-mkGeometry :: Options -> IO [Polyline Vector]
-mkGeometry options = do
-    let (width_opt, height_opt, margin_opt) = widthHeightMargin options
-        width_mm = width_opt - 2 * margin_opt
-        height_mm = height_opt - 2 * margin_opt
-    gen <- initialize (V.fromList [fromIntegral noiseSeed])
-    startPoints <- poissonDisc gen PoissonDiscParams
-        { _poissonShape = boundingBox [ Vec2 (-50) 0, Vec2 (width_mm + 50) (height_mm / 10) ]
-        , _poissonK = 3
-        , _poissonRadius = 5
-        }
-    let mkTrajectory start =
-              Polyline
-            . map (\(_t, pos) -> pos)
-            . takeWhile
-                (\(t, pos) -> t <= 200 && pos `insideBoundingBox` (Vec2 (-50) (-50), Vec2 (width_mm+50) (height_mm+50)))
-            $ fieldLine (velocityField options) (G.transform (G.scale' 1 10) start)
-    pure ((coerce . minimizePenHovering . S.fromList . concatMap (splitIntoInsideParts options . mkTrajectory)) startPoints)
+    writeGCodeFile outfileG plotResult
+    renderPreview outfilePreview 3 plotResult
+    pure ()
 
-drawFieldLine :: Polyline Vector -> Render ()
-drawFieldLine (Polyline polyLine) = cairoScope $ do
-    let simplified = simplifyTrajectoryRadial 2 polyLine
-    unless (null (drop 2 simplified)) $ do
-        sketch (PolyBezier (bezierSmoothen simplified))
-        stroke
-
-groupOn :: Eq b => (a -> b) -> [a] -> [[a]]
-groupOn f = groupBy (\x y -> f x == f y)
-
-splitIntoInsideParts :: Sequential list => Options -> Polyline list -> [[Vec2]]
-splitIntoInsideParts options (Polyline xs) = filter (\(x:_) -> x `insideBoundingBox` drawBB) . groupOn (\p -> insideBoundingBox p drawBB) . toList $ xs
-  where
-    drawBB = boundingBox (Vec2 margin margin, Vec2 (width - margin) (height - margin))
-    (width, height, margin) = widthHeightMargin options
-
-
--- 2D vector potential, which in 2D is umm well a scalar potential.
-vectorPotential :: Options -> Vec2 -> Double
-vectorPotential options p = noiseScale *. perlin2 params p
-  where
-    (width, height, _) = widthHeightMargin options
-    params = PerlinParameters
-        { _perlinFrequency   = 3 / (noiseScale * min width height)
-        , _perlinLacunarity  = 2
-        , _perlinOctaves     = 1
-        , _perlinPersistence = 0.5
-        , _perlinSeed        = noiseSeed
-        }
-
-rotationField :: Options -> Vec2 -> Vec2
-rotationField options = curlZ (vectorPotential options)
-
-velocityField :: Options -> Vec2 -> Vec2
-velocityField options p@(Vec2 x y) = Vec2 1 0 +. perturbationStrength *. rotationField options p
-  where
-    perturbationStrength =
-        0.8
-        * logisticRamp (0.6*width_mm) (width_mm/6) x
-        * gaussianFalloff (0.5*height_mm) (0.4*height_mm) y
-
-    (width, height, margin) = widthHeightMargin options
-    width_mm = width - 2 * margin
-    height_mm = height - 2 * margin
 
 widthHeightMargin :: Options -> (Double, Double, Double)
 widthHeightMargin Options{_canvas=Canvas{_canvasWidth=width, _canvasHeight=height, _canvasMargin=margin}} = (width, height, margin)
-
-fieldLine
-    :: (Vec2 -> Vec2)
-    -> Vec2
-    -> [(Double, Vec2)]
-fieldLine f p0 = rungeKuttaAdaptiveStep (ODE.fieldLine f) p0 t0 dt0 tolNorm tol
-  where
-    t0 = 0
-    dt0 = 1
-    -- Decrease exponent for more accurate results
-    tol = 1e-4
-    tolNorm = norm
 
 data Options = Options
     { _outputFileG :: FilePath
     , _canvas :: Canvas
     } deriving (Eq, Ord, Show)
+
 
 commandLineOptions :: IO Options
 commandLineOptions = execParser parserOpts
