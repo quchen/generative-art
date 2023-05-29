6bcd39ac9ffc00d22e9d49808a20e0ff687eca18
David Luposchainsky <dluposchainsky@gmail.com>
2023-05-29 08:22:10 +0200

    Add Poisson disc forest

--- src/Geometry/Algorithms/Sampling/PoissonDisc.hs
+++ src/Geometry/Algorithms/Sampling/PoissonDisc.hs
@@ -1,5 +1,6 @@
 module Geometry.Algorithms.Sampling.PoissonDisc (
-    poissonDisc
+      poissonDisc
+    , poissonDiscForest
 ) where
 
 
@@ -20,6 +21,7 @@ import Geometry
 -- $setup
 -- >>> import           Draw
 -- >>> import           Control.Monad.ST
+-- >>> import           Numerics.Interpolation
 -- >>> import           Graphics.Rendering.Cairo as C
 -- >>> import qualified System.Random.MWC        as MWC
 
@@ -50,46 +52,119 @@ poissonDisc
     => Gen (PrimState m) -- ^ RNG from mwc-random. 'create' yields the default (static) RNG.
     -> boundingBox -- ^ Region to generate points in
     -> Double      -- ^ Radius around each point no other points are genereted. Smaller values yield more points.
-    -> Int         -- ^ Per point, how many attempts should be made to find an empty spot?
+    -> Int         -- ^ \(k\) parameter: per point, how many attempts should be made to find an empty spot?
                    --   Typical value: 3. Higher values are slower, but increase result quality.
     -> m [Vec2]
 poissonDisc gen bb' radius k = do
     let bb@(BoundingBox minV maxV) = boundingBox bb'
         cellSize = CellSize (radius/sqrt 2)
     initialPoint <- uniformRM (minV, maxV) gen
-    let initialState = PoissonDiscState
-            { _grid   = M.singleton (gridCell cellSize initialPoint) initialPoint
-            , _active = S.singleton initialPoint
-            }
+    let initialGrid = M.singleton (gridCell cellSize initialPoint) initialPoint
+        initialActive = S.singleton initialPoint
 
-        sampleLoop state
-            | S.null (_active state) = pure []
-        sampleLoop state = do
-            activeSample <- randomSetElement gen (_active state)
+        sampleLoop _grid active
+            | S.null active = pure []
+        sampleLoop grid active = do
+            activeSample <- randomSetElement gen active
             candidates <- candidatesAroundSample gen k bb radius activeSample
 
-            let grid = _grid state
-                validPoint candidate =
+            let validPoint candidate =
                     let neighbours = neighbouringPoints cellSize grid candidate
                         tooClose neighbour = normSquare (candidate -. neighbour) <= radius^2
                     in not (any tooClose neighbours)
 
             case find validPoint candidates of
                 Nothing -> do
-                    rest <- sampleLoop state
-                        { _active = S.delete activeSample (_active state) }
+                    rest <- sampleLoop grid (S.delete activeSample active)
                     pure (activeSample : rest)
-                Just sample -> sampleLoop state
-                    { _grid = M.insert (gridCell cellSize sample) sample (_grid state)
-                    , _active = S.insert sample (_active state) }
+                Just sample -> sampleLoop
+                    (M.insert (gridCell cellSize sample) sample grid)
+                    (S.insert sample active)
 
     -- TODO remove after refactoring! Old alg was backwards :-)
-    fmap reverse $ sampleLoop initialState
+    fmap reverse $ sampleLoop initialGrid initialActive
 
-data PoissonDiscState = PoissonDiscState
-    { _grid   :: Map (Int, Int) Vec2
-    , _active :: Set Vec2
-    }
+-- | 'poissonDisc', but keeps track of which parent spawned which children. While
+-- this algorithm does a bit more processing to reassemble the trees, it allows
+-- specifying the starting points explicitly. ('poissonDisc' would also allow this,
+-- but since it’s mostly used for simply sampling, this additional config option
+-- would just clutter the API.)
+--
+-- <<docs/haddock/Geometry/Algorithms/Sampling/PoissonDisc/poisson_disc_forest.svg>>
+--
+-- === __(image code)__
+-- >>> :{
+-- haddockRender "Geometry/Algorithms/Sampling/PoissonDisc/poisson_disc_forest.svg" 300 300 $ do
+--     let initialPoints = [Vec2 50 50, Vec2 150 150, Vec2 250 250]
+--         forest = runST $ do
+--             gen <- MWC.create
+--             poissonDiscForest gen (shrinkBoundingBox 30 [zero, Vec2 300 300]) 7 10 initialPoints
+--     C.setLineWidth 1
+--     let paint color i parent = do
+--             let parentRadius = 2.5
+--             cairoScope $ do
+--                 setColor (color (sin (lerpID (0,20) (0,pi) i)^2))
+--                 sketch (Circle parent parentRadius)
+--                 fill
+--             case M.lookup parent forest of
+--                 Nothing -> pure () -- Should never happen: only the roots have no parents
+--                 Just children -> for_ children $ \child -> do
+--                     cairoScope $ do
+--                         setColor black
+--                         sketch (resizeLineSymmetric (\l -> l-2*parentRadius) (Line parent child))
+--                         stroke
+--                     paint color (i+1) child
+--     for_ (zip [flare, crest, flare] initialPoints) $ \(color, p0) -> paint color 0 p0
+-- :}
+-- docs/haddock/Geometry/Algorithms/Sampling/PoissonDisc/poisson_disc_forest.svg
+poissonDiscForest
+    :: (PrimMonad m, HasBoundingBox boundingBox)
+    => Gen (PrimState m)
+    -> boundingBox
+    -> Double
+    -> Int
+    -> [Vec2] -- ^ Initial points to start sampling from.
+    -> m (Map Vec2 (Set Vec2)) -- ^ Map of parent to children.
+poissonDiscForest gen bb' radius k initialPoints = do
+    let bb = boundingBox bb'
+        cellSize = CellSize (radius/sqrt 2)
+    let initialGrid = M.fromList [(gridCell cellSize initialPoint, initialPoint) | initialPoint <- initialPoints]
+        initialActive = S.fromList [ChildParent initialPoint Nothing | initialPoint <- initialPoints]
+
+        sampleLoop _grid active
+            | S.null active = pure []
+        sampleLoop grid active = do
+            ChildParent activeSample parent <- randomSetElement gen active
+            candidates <- candidatesAroundSample gen k bb radius activeSample
+
+            let validPoint candidate =
+                    let neighbours = neighbouringPoints cellSize grid candidate
+                        tooClose neighbour = normSquare (candidate -. neighbour) <= radius^2
+                    in not (any tooClose neighbours)
+
+            case find validPoint candidates of
+                Nothing -> do
+                    rest <- sampleLoop grid (S.delete (ChildParent activeSample parent) active)
+                    pure (ChildParent activeSample parent : rest)
+                Just sample -> sampleLoop
+                    (M.insert (gridCell cellSize sample) sample grid)
+                    (S.insert (ChildParent sample (Just activeSample)) active)
+
+    edges <- sampleLoop initialGrid initialActive
+    let families = mapMaybe
+            (\(ChildParent child maybeParent) -> case maybeParent of
+                Nothing -> Nothing
+                Just parent -> Just (parent, S.singleton child))
+            edges
+    pure (M.fromListWith S.union families)
+
+data ChildParent = ChildParent Vec2 (Maybe Vec2)
+
+instance Eq ChildParent where
+    ChildParent child1 _parent1 == ChildParent child2 _parent2 = child1 == child2
+
+instance Ord ChildParent where
+    ChildParent child1 _parent1 `compare` ChildParent child2 _parent2 = child1 `compare` child2
 
 randomSetElement :: PrimMonad m => Gen (PrimState m) -> Set a -> m a
 randomSetElement gen set = do
