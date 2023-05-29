c4b4d6c61e70d9bef5731a54b3bf13cfd1b0d5b1
David Luposchainsky <dluposchainsky@gmail.com>
2023-05-29 17:02:30 +0200

    Add doctest for Cohen-Sutherland

--- src/Geometry/Algorithms/Clipping/CohenSutherland.hs
+++ src/Geometry/Algorithms/Clipping/CohenSutherland.hs
@@ -2,12 +2,51 @@ module Geometry.Algorithms.Clipping.CohenSutherland (cohenSutherland) where
 
 
 
-import Geometry.Core
 import Data.Bits
+import Geometry.Core
+import Util
 
+
+
+-- $setup
+-- >>> import Geometry.Algorithms.Sampling
+-- >>> import Control.Monad
+-- >>> import Control.Monad.ST
+-- >>> import Draw
+-- >>> import Data.Maybe
+-- >>> import Data.Traversable
+-- >>> import qualified Graphics.Rendering.Cairo as C
+-- >>> import qualified System.Random.MWC as MWC
 
 
 -- | Constrain a line to the inside of a box with the Cohen-Sutherland clipping algorithm.
+--
+-- <<docs/haddock/Geometry/Algorithms/Clipping/CohenSutherland/cohenSutherland.svg>>
+--
+-- === __(image code)__
+-- >>> :{
+-- haddockRender "Geometry/Algorithms/Clipping/CohenSutherland/cohenSutherland.svg" 300 200 $ do
+--     let viewport = boundingBox [zero, Vec2 300 200]
+--         mask = shrinkBoundingBox 20 viewport
+--         lines = runST $ do
+--             gen <- MWC.create
+--             ps <- poissonDisc gen mask 20 4
+--             for ps $ \p -> do
+--                 angle <- MWC.uniformM gen
+--                 let makeLooong = resizeLineSymmetric (const 500)
+--                 pure (makeLooong (Line p (p +. polar angle 1)))
+--         clippedLines = mapMaybe (cohenSutherland mask) lines
+--     for_ clippedLines $ \line -> cairoScope $ do
+--         setColor (mathematica97 1)
+--         sketch line
+--         C.stroke
+--     cairoScope $ do
+--         C.setLineWidth 2
+--         setColor (mathematica97 0)
+--         sketch (boundingBoxPolygon mask)
+--         C.stroke
+-- :}
+-- docs/haddock/Geometry/Algorithms/Clipping/CohenSutherland/cohenSutherland.svg
 cohenSutherland :: BoundingBox -> Line -> Maybe Line
 cohenSutherland bb = \line -> let Line start end = line in loop line (outCode start) (outCode end)
   where
@@ -45,7 +84,7 @@ cohenSutherland bb = \line -> let Line start end = line in loop line (outCode st
                   | 0 /= outcode' .&. bit_TOP    = Vec2 (x0 + (x1-x0) * (yMin-y0) / (y1-y0)) yMin
                   | 0 /= outcode' .&. bit_RIGHT  = Vec2 xMax (y0 + (y1-y0) * (xMax-x0) / (x1-x0))
                   | 0 /= outcode' .&. bit_LEFT   = Vec2 xMin (y0 + (y1-y0) * (xMin-x0) / (x1-x0))
-                  | otherwise = error "Impossible!"
+                  | otherwise = bugError "cohenSutherland" "Impossible!"
 
             in if outcode' == outcode0
                 then loop (Line p p1) (outCode p) outcode1
