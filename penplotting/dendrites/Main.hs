bd593edc430e3016ab4a0eb36b5efe2912b4bf38
Franz Thoma <franz.thoma@tngtech.com>
2024-12-31 17:36:39 +0100

    Fit plot onto plotter

--- penplotting/dendrites/Main.hs
+++ penplotting/dendrites/Main.hs
@@ -19,8 +19,8 @@ import Geometry.Shapes
 
 
 picWidth, picHeight :: Num a => a
-picWidth = 500
-picHeight = 500
+picWidth = 470
+picHeight = 470
 
 canvas :: Polygon
 canvas = transform (translate (Vec2 (picWidth/2) (picHeight/2)) <> scale' (picWidth/2) (picHeight/2)) (regularPolygon 32)
