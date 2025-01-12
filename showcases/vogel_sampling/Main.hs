0d1d561b85cdd0619433ce9bce82e6485de9553e
Franz Thoma <franz.thoma@tngtech.com>
2025-01-13 00:57:06 +0100

    Plotting

--- showcases/vogel_sampling/Main.hs
+++ showcases/vogel_sampling/Main.hs
@@ -7,9 +7,11 @@ import qualified Data.Vector as V
 import Graphics.Rendering.Cairo as C
 
 import Draw
+import Draw.Plotting
 import Geometry as G
 import Geometry.Algorithms.Delaunay
 import Geometry.Algorithms.Sampling.Vogel
+import Data.List (sortOn)
 
 
 
@@ -18,8 +20,8 @@ picWidth = 1440
 picHeight = 1440
 squareSize = min picWidth picHeight
 
-file :: FilePath
-file = "out/vogel_sampling.png"
+plotSize :: Num a => a
+plotSize = 440
 
 main :: IO ()
 main = do
@@ -40,6 +42,9 @@ main = do
         triangles = filter
             (\(Polygon ps) -> all (\p -> norm (p -. center) < cutoff) ps)
             (V.toList $ delaunayTriangles delaunay)
+        edges = filter
+            (\(Line a b) -> all (\p -> norm (p -. center) < cutoff) [a, b])
+            (V.toList $ delaunayEdges delaunay)
         voronoi = filter
             (\(seed, _) -> norm (seed -. center) < cutoff)
             (zip seeds cells)
@@ -48,21 +53,9 @@ main = do
             in  (seed, shrinkPolygon cellGutter poly)
         voronoiSmoothed = fmap (snd . fmap (chaikin 0.25 . chaikin 0.25 . chaikin 0.15) . addCellGutter) voronoi
 
-    render file picWidth picHeight $ do
-        cairoScope (setColor white >> paint)
-        C.setLineJoin C.LineJoinBevel
-        cairoScope $ do 
-            setColor black
-            setLineWidth 0.25
-            for_ triangles $ \poly -> do
-                sketch poly
-                C.stroke
-        cairoScope $ do
-            setColor black
-            setLineWidth 1
-            for_ voronoiSmoothed $ \cell -> do
-                sketch cell
-                C.stroke
+    render "out/vogel_sampling.png" picWidth picHeight $ drawPic edges voronoiSmoothed
+    let plotResult = plotPic edges voronoiSmoothed
+    renderPreview "out/vogel_sampling_preview.png" 5 plotResult
 
 chaikin :: Double -> Polygon -> Polygon
 chaikin _ (Polygon []) = Polygon []
@@ -72,3 +65,47 @@ chaikin lambda (Polygon ps@(p:_)) = Polygon $ concat
     , let b = lambda *. a +. (1-lambda) *. d
     , let c = (1-lambda) *. a +. lambda *. d
     ]
+
+drawPic :: [Line] -> [Polygon] -> C.Render ()
+drawPic delaunay voronoi = do
+    cairoScope (setColor white >> paint)
+    C.setLineJoin C.LineJoinBevel
+    cairoScope $ do 
+        setColor black
+        setLineWidth 0.5
+        for_ delaunay $ \poly -> do
+            sketch poly
+            C.stroke
+    cairoScope $ do
+        setColor black
+        setLineWidth 1
+        for_ voronoi $ \cell -> do
+            sketch cell
+            C.stroke
+
+plotPic :: [Line] -> [Polygon] -> RunPlotResult
+plotPic delaunay voronoi =
+    let plottingSettings = def
+            { _feedrate = 6000
+            , _zTravelHeight = 5
+            , _zDrawingHeight = -2
+            , _repositionThreshold = 0.05
+            , _canvasBoundingBox = Just $ boundingBox [zero, Vec2 plotSize plotSize]
+            , _previewPenWidth = 0.2
+            }
+        scaleFactor = plotSize / squareSize
+        resize :: Transform a => a -> a
+        resize = G.transform (G.scale scaleFactor)
+
+    in runPlot plottingSettings $ do
+        penChange
+        for_ (resize voronoi) plot
+        penChange
+        for_ (resize delaunay) plot
+
+penChange :: Plot ()
+penChange = withDrawingHeight 0 $ do
+    repositionTo zero
+    penDown
+    pause PauseUserConfirm
+    penUp
