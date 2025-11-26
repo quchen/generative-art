2fa549546d473762bf8f512e56d0128263aa387e
Franz Thoma <franz.thoma@tngtech.com>
2025-11-26 20:21:36 +0100

    Prepare for plotting

--- showcases/warped_tiles/Main.hs
+++ showcases/warped_tiles/Main.hs
@@ -64,13 +64,13 @@ main = render file picWidth picHeight $ do
     --    setColor (black `withOpacity` 0.3)
     --    sketch l
     --    stroke
-    for_ triangles $ \poly -> do
-        sketch poly
+    --for_ triangles $ \poly -> do
+    --    sketch poly
+    --    C.stroke
+    for_ cells $ \cell -> do
+        let area = polygonArea cell
+        sketch $ chaikin 0.25 $ chaikin 0.25 $ chaikin 0.1 $ growPolygon (-0.1 * sqrt area) cell
         C.stroke
-    -- for_ cells $ \cell -> do
-    --     let area = polygonArea cell
-    --     sketch $ chaikin 0.25 $ chaikin 0.25 $ chaikin 0.1 $ growPolygon (-0.1 * sqrt area) cell
-    --     C.fill
 
 chaikin :: Double -> Polygon -> Polygon
 chaikin _ (Polygon []) = Polygon []
